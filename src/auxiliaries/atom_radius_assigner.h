0213d127a7acca3c951e4c081f1ec42144d59f0c
kliment <kliment@localhost>
2013-07-05 14:40:45 +0300

    Finished adding balls collection function.

---
+++ src/auxiliaries/atom_radius_assigner.h
@@ -0,0 +1,78 @@
+#ifndef AUXILIARIES_ATOM_RADIUS_ASSIGNER_H_
+#define AUXILIARIES_ATOM_RADIUS_ASSIGNER_H_
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace auxiliaries
+{
+
+class AtomRadiusAssigner
+{
+public:
+	AtomRadiusAssigner(const double default_radius) : default_radius(default_radius)
+	{
+	}
+
+	bool add_radius_by_descriptor(const std::string& resName_pattern, const std::string& name_pattern, const double radius)
+	{
+		if(!resName_pattern.empty() && !name_pattern.empty())
+		{
+			radii_by_descriptors[Descriptor(resName_pattern, name_pattern)]=radius;
+			return true;
+		}
+		return false;
+	}
+
+	double get_atom_radius(const std::string& resName, const std::string& name) const
+	{
+		std::vector<std::string> ordered_resName_patterns;
+		ordered_resName_patterns.push_back(resName);
+		ordered_resName_patterns.push_back("*");
+
+		std::vector<std::string> ordered_name_patterns;
+		ordered_name_patterns.push_back(name);
+		for(std::size_t i=0;i<name.size();i++)
+		{
+			ordered_name_patterns.push_back(name.substr(0, name.size()-i)+"*");
+		}
+		ordered_name_patterns.push_back("*");
+
+		for(std::size_t i=0;i<ordered_resName_patterns.size();i++)
+		{
+			for(std::size_t j=0;j<ordered_name_patterns.size();j++)
+			{
+				std::map<Descriptor, double>::const_iterator it=radii_by_descriptors.find(Descriptor(ordered_resName_patterns[i], ordered_name_patterns[j]));
+				if(it!=radii_by_descriptors.end())
+				{
+					return it->second;
+				}
+			}
+		}
+		return default_radius;
+	}
+
+private:
+	struct Descriptor
+	{
+		std::string resName_pattern;
+		std::string name_pattern;
+
+		Descriptor(const std::string& resName_pattern, const std::string& name_pattern) : resName_pattern(resName_pattern), name_pattern(name_pattern)
+		{
+		}
+
+		bool operator<(const Descriptor& d) const
+		{
+			return ( resName_pattern<d.resName_pattern || (resName_pattern==d.resName_pattern && (name_pattern<d.name_pattern)) );
+		}
+	};
+
+	double default_radius;
+	std::map<Descriptor, double> radii_by_descriptors;
+};
+
+}
+
+#endif /* AUXILIARIES_ATOM_RADIUS_ASSIGNER_H_ */
--- src/get_balls_from_pdb_file.cpp
+++ src/get_balls_from_pdb_file.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "auxiliaries/pdb_file_parsing.h"
+#include "auxiliaries/atom_radius_assigner.h"
 #include "auxiliaries/command_line_options.h"
 
 void get_balls_from_pdb_file(const auxiliaries::CommandLineOptions& clo)
 {
-	clo.check_allowed_options("--include-heteroatoms");
+	clo.check_allowed_options("--include-heteroatoms --default-radius: --only-default-radius --radii-file: --output-serial-numbers");
 
 	const bool include_heteroatoms=clo.isopt("--include-heteroatoms");
+	const double default_radius=clo.isarg("--default-radius") ? clo.arg<double>("--default-radius") : 1.70;
+	const bool only_default_radius=clo.isopt("--only-default-radius");
+	const std::string radii_file=clo.isarg("--radii-file") ? clo.arg<std::string>("--radii-file") : std::string();
+	const bool output_serial_numbers=clo.isopt("--output-serial-numbers");
 
-	std::vector<auxiliaries::PDBFileParsing::AtomRecord> atoms=auxiliaries::PDBFileParsing::read_atom_records_from_pdb_file_stream(std::cin, include_heteroatoms);
+	const std::vector<auxiliaries::PDBFileParsing::AtomRecord> atoms=auxiliaries::PDBFileParsing::read_atom_records_from_pdb_file_stream(std::cin, include_heteroatoms);
+
+	auxiliaries::AtomRadiusAssigner atom_radius_assigner(default_radius);
+	if(!only_default_radius)
+	{
+		if(radii_file.empty())
+		{
+			atom_radius_assigner.add_radius_by_descriptor("*", "C*", 1.70);
+			atom_radius_assigner.add_radius_by_descriptor("*", "N*", 1.55);
+			atom_radius_assigner.add_radius_by_descriptor("*", "O*", 1.52);
+			atom_radius_assigner.add_radius_by_descriptor("*", "P*", 1.80);
+		}
+		else
+		{
+			std::ifstream input(radii_file.c_str(), std::ios::in);
+			while(input.good())
+			{
+				std::string line;
+				std::getline(input, line);
+				if(!line.empty())
+				{
+					std::istringstream line_input(line);
+					std::string resName;
+					std::string name;
+					double radius=default_radius;
+					line_input >> resName >> name >> radius;
+					if(!line_input.fail())
+					{
+						atom_radius_assigner.add_radius_by_descriptor(resName, name, radius);
+					}
+				}
+			}
+		}
+	}
+
+	for(std::size_t i=0;i<atoms.size();i++)
+	{
+		const auxiliaries::PDBFileParsing::AtomRecord& atom=atoms[i];
+		std::cout << atom.x << " " << atom.y << " " << atom.z << " " << atom_radius_assigner.get_atom_radius(atom.resName, atom.name);
+		if(output_serial_numbers)
+		{
+			std::cout << " " << atom.serial;
+		}
+		std::cout << "\n";
+	}
 }
