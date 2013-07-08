183b58cf6f6ff7ce7e44fa81265abc0d461872e5
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-07-08 16:58:49 +0300

    Added improved command line option handler.

--- src/calculate_triangulation.cpp
+++ src/calculate_triangulation.cpp
@@ -3,7 +3,7 @@
 #include "apollota/triangulation.h"
 
 #include "auxiliaries/io_utilities.h"
-#include "auxiliaries/command_line_options.h"
+#include "auxiliaries/command_line_options_handler.h"
 
 namespace
 {
@@ -20,20 +20,26 @@ void add_sphere_from_stream_to_vector(std::istream& input, std::vector<apollota:
 
 }
 
-void calculate_triangulation(const auxiliaries::CommandLineOptions& clo)
+void calculate_triangulation(const auxiliaries::CommandLineOptionsHandler& clo)
 {
-	clo.check_allowed_options("--bounding-spheres-hierarchy-first-radius: --allow-redundant-quadruples --skip-output --print-log --check");
+	auxiliaries::CommandLineOptionsHandler::MapOfOptionDescriptions map_of_option_descriptions;
+	map_of_option_descriptions["--bounding-spheres-hierarchy-first-radius"]=auxiliaries::CommandLineOptionsHandler::OptionDescription(true, "initial radius for bounding sphere hierarchy");
+	map_of_option_descriptions["--allow-redundant-quadruples"]=auxiliaries::CommandLineOptionsHandler::OptionDescription(false, "flag to include redundant quadruples");
+	map_of_option_descriptions["--skip-output"]=auxiliaries::CommandLineOptionsHandler::OptionDescription(false, "flag to disable output of the resulting triangulation");
+	map_of_option_descriptions["--print-log"]=auxiliaries::CommandLineOptionsHandler::OptionDescription(false, "flag to print log of calculations");
+	map_of_option_descriptions["--check"]=auxiliaries::CommandLineOptionsHandler::OptionDescription(false, "flag to explicitely check the resulting triangulation (takes time, used only for testing)");
+	clo.compare_with_map_of_option_descriptions(map_of_option_descriptions);
 
-	const double bounding_spheres_hierarchy_first_radius=clo.isopt("--bounding-spheres-hierarchy-first-radius") ? clo.arg<double>("--bounding-spheres-hierarchy-first-radius") : 3.5;
+	const double bounding_spheres_hierarchy_first_radius=clo.argument<double>("--bounding-spheres-hierarchy-first-radius", 3.5);
 	if(bounding_spheres_hierarchy_first_radius<=1.0)
 	{
 		throw std::runtime_error("Bounding spheres hierarchy first radius is not greater than 1.");
 	}
 
-	const bool allow_redundant_quadruples=clo.isopt("--allow-redundant-quadruples");
-	const bool skip_output=clo.isopt("--skip-output");
-	const bool print_log=clo.isopt("--print-log");
-	const bool check=clo.isopt("--check");
+	const bool allow_redundant_quadruples=clo.contains_option("--allow-redundant-quadruples");
+	const bool skip_output=clo.contains_option("--skip-output");
+	const bool print_log=clo.contains_option("--print-log");
+	const bool check=clo.contains_option("--check");
 
 	std::vector<apollota::SimpleSphere> spheres;
 	auxiliaries::read_lines_to_container(std::cin, "#", add_sphere_from_stream_to_vector, spheres);
