6b2ac5748b057546e9ccddbb68fdf855364426fe
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-08-07 15:29:40 +0300

    Added admittance infrastructure.

--- src/apollota/triangulation.h
+++ src/apollota/triangulation.h
@@ -110,7 +110,7 @@ public:
 				}
 			}
 
-			result.quadruples_search_log=find_valid_quadruples(bsh, result.quadruples_map);
+			result.quadruples_search_log=find_valid_quadruples(bsh, std::vector<int>(bsh.leaves_spheres().size(), 1), result.quadruples_map);
 			if(include_surplus_valid_quadruples)
 			{
 				result.surplus_quadruples_search_log=find_surplus_valid_quadruples(bsh, result.quadruples_map);
@@ -122,21 +122,45 @@ public:
 			}
 		}
 
-		result.ignored_spheres_ids=collect_ignored_spheres_ids(spheres.size(), result.quadruples_map);
+		result.ignored_spheres_ids=collect_ignored_spheres_ids(std::vector<int>(spheres.size(), 1), result.quadruples_map);
 
 		return result;
 	}
 
-	static Result construct_result(const BoundingSpheresHierarchy& bsh, const bool include_surplus_valid_quadruples)
+	template<typename AdmittanceSet>
+	static Result construct_result_for_admittance_set(const BoundingSpheresHierarchy& bsh, const AdmittanceSet& admittance_set)
 	{
 		Result result;
-		result.quadruples_search_log=find_valid_quadruples(bsh, result.quadruples_map);
-		if(include_surplus_valid_quadruples)
+		std::vector<int> admittance(bsh.leaves_spheres().size(), 0);
+		bool admittance_filled=false;
+		for(typename AdmittanceSet::const_iterator it=admittance_set.begin();it!=admittance_set.end();++it)
 		{
-			result.surplus_quadruples_search_log=find_surplus_valid_quadruples(bsh, result.quadruples_map);
+			const std::size_t id=(*it);
+			if(id<admittance.size())
+			{
+				admittance[id]=1;
+				admittance_filled=true;
+			}
 		}
-		result.ignored_spheres_ids=collect_ignored_spheres_ids(bsh.leaves_spheres().size(), result.quadruples_map);
+		if(admittance_filled)
+		{
+			result.quadruples_search_log=find_valid_quadruples(bsh, admittance, result.quadruples_map);
+			result.ignored_spheres_ids=collect_ignored_spheres_ids(admittance, result.quadruples_map);
+		}
 		return result;
+	}
+
+	static void merge_quadruples_maps(const QuadruplesMap& source_quadruples_map, QuadruplesMap& destination_quadruples_map)
+	{
+		for(QuadruplesMap::const_iterator it=source_quadruples_map.begin();it!=source_quadruples_map.end();++it)
+		{
+			const Quadruple& quadruple=it->first;
+			const std::vector<SimpleSphere>& tangent_spheres=it->second;
+			for(std::size_t i=0;i<tangent_spheres.size();i++)
+			{
+				augment_quadruples_map(quadruple, tangent_spheres[i], destination_quadruples_map);
+			}
+		}
 	}
 
 	static QuadruplesOrderedMap collect_ordered_map_of_quadruples(const QuadruplesMap& quadruples_map)
@@ -830,28 +854,40 @@ private:
 		};
 	};
 
-	static std::size_t select_starting_sphere_for_finding_first_valid_faces(const BoundingSpheresHierarchy& bsh)
+	static std::size_t select_starting_sphere_for_finding_first_valid_faces(const BoundingSpheresHierarchy& bsh, const std::vector<int>& admittance)
 	{
 		const std::vector<SimpleSphere>& spheres=bsh.leaves_spheres();
-		if(spheres.empty())
+		if(spheres.empty() || admittance.size()!=spheres.size())
 		{
 			return 0;
 		}
 		SimplePoint center(0.0, 0.0, 0.0);
+		int count=0;
 		for(std::size_t i=0;i<spheres.size();i++)
 		{
-			center=center+SimplePoint(spheres[i]);
-		}
-		center=center*(1/static_cast<double>(spheres.size()));
-		std::size_t result=0;
-		double result_distance=distance_from_point_to_point(center, spheres[result]);
-		for(std::size_t i=1;i<spheres.size();i++)
-		{
-			const double i_distance=distance_from_point_to_point(center, spheres[i]);
-			if(i_distance<result_distance)
+			if(admittance[i]>0)
 			{
-				result=i;
-				result_distance=i_distance;
+				center=center+spheres[i];
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			return 0;
+		}
+		center=center*(1/static_cast<double>(count));
+		std::size_t result=0;
+		double result_distance=std::numeric_limits<double>::max();
+		for(std::size_t i=0;i<spheres.size();i++)
+		{
+			if(admittance[i]>0)
+			{
+				const double i_distance=distance_from_point_to_point(center, spheres[i]);
+				if(i_distance<result_distance)
+				{
+					result=i;
+					result_distance=i_distance;
+				}
 			}
 		}
 		return result;
@@ -859,6 +895,7 @@ private:
 
 	static std::vector<Face> find_first_valid_faces(
 			const BoundingSpheresHierarchy& bsh,
+			const std::vector<int>& admittance,
 			const std::size_t starting_sphere_id,
 			std::size_t& iterations_count,
 			const bool fix_starting_sphere_id,
@@ -867,7 +904,7 @@ private:
 	{
 		const std::vector<SimpleSphere>& spheres=bsh.leaves_spheres();
 		std::vector<Face> result;
-		if(spheres.size()>=4 && starting_sphere_id<spheres.size())
+		if(spheres.size()>=4 && starting_sphere_id<spheres.size() && admittance.size()==spheres.size())
 		{
 			const std::vector<std::size_t> traversal=BoundingSpheresHierarchy::sort_objects_by_distance_to_one_of_them(spheres, starting_sphere_id, minimal_distance_from_sphere_to_sphere<SimpleSphere, SimpleSphere>);
 			for(std::size_t d=3;d<std::min(traversal.size(), max_size_of_traversal);d++)
@@ -879,16 +916,19 @@ private:
 						for(std::size_t c=b+1;c<d;c++)
 						{
 							iterations_count++;
-							const Triple triple(traversal[a], traversal[b], traversal[c]);
-							const Quadruple quadruple(triple, traversal[d]);
-							const std::vector<SimpleSphere> tangents=TangentSphereOfFourSpheres::calculate(spheres[quadruple.get(0)], spheres[quadruple.get(1)], spheres[quadruple.get(2)], spheres[quadruple.get(3)]);
-							if(
-									(tangents.size()==1 && SearchForSphericalCollisions::find_any_collision(bsh, tangents.front()).empty())
-									|| (allow_quadruples_with_two_tangent_spheres && tangents.size()==2 && (SearchForSphericalCollisions::find_any_collision(bsh, tangents.front()).empty() || SearchForSphericalCollisions::find_any_collision(bsh, tangents.back()).empty()))
-								)
+							if(admittance[traversal[a]]>0 || admittance[traversal[b]]>0 || admittance[traversal[c]]>0)
 							{
-								result.push_back(Face(bsh.leaves_spheres(), triple, bsh.min_input_radius()));
-								return result;
+								const Triple triple(traversal[a], traversal[b], traversal[c]);
+								const Quadruple quadruple(triple, traversal[d]);
+								const std::vector<SimpleSphere> tangents=TangentSphereOfFourSpheres::calculate(spheres[quadruple.get(0)], spheres[quadruple.get(1)], spheres[quadruple.get(2)], spheres[quadruple.get(3)]);
+								if(
+										(tangents.size()==1 && SearchForSphericalCollisions::find_any_collision(bsh, tangents.front()).empty())
+										|| (allow_quadruples_with_two_tangent_spheres && tangents.size()==2 && (SearchForSphericalCollisions::find_any_collision(bsh, tangents.front()).empty() || SearchForSphericalCollisions::find_any_collision(bsh, tangents.back()).empty()))
+									)
+								{
+									result.push_back(Face(bsh.leaves_spheres(), triple, bsh.min_input_radius()));
+									return result;
+								}
 							}
 						}
 					}
@@ -921,13 +961,13 @@ private:
 		return std::make_pair(quadruple_added, quadruple_tangent_sphere_added);
 	}
 
-	static QuadruplesSearchLog find_valid_quadruples(const BoundingSpheresHierarchy& bsh, QuadruplesMap& quadruples_map)
+	static QuadruplesSearchLog find_valid_quadruples(const BoundingSpheresHierarchy& bsh, const std::vector<int>& admittance, QuadruplesMap& quadruples_map)
 	{
 		typedef std::tr1::unordered_map<Triple, std::size_t, Triple::HashFunctor> TriplesMap;
 
 		QuadruplesSearchLog log=QuadruplesSearchLog();
 
-		std::vector<Face> stack=find_first_valid_faces(bsh, select_starting_sphere_for_finding_first_valid_faces(bsh), log.performed_iterations_for_finding_first_faces, false, true);
+		std::vector<Face> stack=find_first_valid_faces(bsh, admittance, select_starting_sphere_for_finding_first_valid_faces(bsh, admittance), log.performed_iterations_for_finding_first_faces, false, true);
 		std::tr1::unordered_set<Triple, Triple::HashFunctor> processed_triples_set;
 		std::vector<int> spheres_usage_mapping(bsh.leaves_spheres().size(), 0);
 		std::set<std::size_t> ignorable_spheres_ids;
@@ -969,25 +1009,28 @@ private:
 						for(std::size_t i=0;i<produced_prefaces.size();i++)
 						{
 							const std::pair<Triple, std::pair<std::size_t, SimpleSphere> >& produced_preface=produced_prefaces[i];
-							if(processed_triples_set.count(produced_preface.first)==0)
+							if(admittance[produced_preface.first.get(0)]>0 || admittance[produced_preface.first.get(1)]>0 || admittance[produced_preface.first.get(2)]>0)
 							{
-								TriplesMap::const_iterator sm_it=stack_map.find(produced_preface.first);
-								if(sm_it==stack_map.end())
+								if(processed_triples_set.count(produced_preface.first)==0)
 								{
-									stack_map[produced_preface.first]=stack.size();
-									stack.push_back(Face(bsh.leaves_spheres(), produced_preface.first, bsh.min_input_radius()));
-									stack.back().set_d_with_d_number_selection(produced_preface.second.first, produced_preface.second.second);
-									log.produced_faces++;
+									TriplesMap::const_iterator sm_it=stack_map.find(produced_preface.first);
+									if(sm_it==stack_map.end())
+									{
+										stack_map[produced_preface.first]=stack.size();
+										stack.push_back(Face(bsh.leaves_spheres(), produced_preface.first, bsh.min_input_radius()));
+										stack.back().set_d_with_d_number_selection(produced_preface.second.first, produced_preface.second.second);
+										log.produced_faces++;
+									}
+									else
+									{
+										stack.at(sm_it->second).set_d_with_d_number_selection(produced_preface.second.first, produced_preface.second.second);
+										log.updated_faces++;
+									}
 								}
 								else
 								{
-									stack.at(sm_it->second).set_d_with_d_number_selection(produced_preface.second.first, produced_preface.second.second);
-									log.updated_faces++;
+									log.encountered_triples_repetitions++;
 								}
-							}
-							else
-							{
-								log.encountered_triples_repetitions++;
 							}
 						}
 					}
@@ -1005,9 +1048,9 @@ private:
 			}
 			for(std::size_t i=0;i<spheres_usage_mapping.size() && stack.empty();i++)
 			{
-				if(spheres_usage_mapping[i]==0 && ignorable_spheres_ids.count(i)==0)
+				if(spheres_usage_mapping[i]==0 && admittance[i]>0 && ignorable_spheres_ids.count(i)==0)
 				{
-					stack=find_first_valid_faces(bsh, i, log.performed_iterations_for_finding_first_faces, true, true, 25);
+					stack=find_first_valid_faces(bsh, admittance, i, log.performed_iterations_for_finding_first_faces, true, true, 25);
 					ignorable_spheres_ids.insert(i);
 				}
 			}
@@ -1082,21 +1125,24 @@ private:
 		return renumbered_quadruples_map;
 	}
 
-	static std::set<std::size_t> collect_ignored_spheres_ids(const std::size_t input_spheres_count, const QuadruplesMap& quadruples_map)
+	static std::set<std::size_t> collect_ignored_spheres_ids(const std::vector<int>& admittance, const QuadruplesMap& quadruples_map)
 	{
 		std::set<std::size_t> ignored_spheres_ids;
-		std::vector<int> spheres_inclusion_map(input_spheres_count, 0);
+		std::vector<int> spheres_inclusion_map(admittance.size(), 0);
 		for(QuadruplesMap::const_iterator it=quadruples_map.begin();it!=quadruples_map.end();++it)
 		{
 			const Quadruple& q=it->first;
 			for(int i=0;i<4;i++)
 			{
-				spheres_inclusion_map[q.get(i)]=1;
+				if(q.get(i)<spheres_inclusion_map.size())
+				{
+					spheres_inclusion_map[q.get(i)]=1;
+				}
 			}
 		}
 		for(std::size_t i=0;i<spheres_inclusion_map.size();i++)
 		{
-			if(spheres_inclusion_map[i]==0)
+			if(spheres_inclusion_map[i]==0 && admittance[i]>0)
 			{
 				ignored_spheres_ids.insert(i);
 			}
