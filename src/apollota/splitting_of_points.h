7134374f48d7958d6f778f2b859b679b5c32a5d8
kliment <kliment@localhost>
2013-08-10 12:22:30 +0300

    Added splitting improvement function.

--- src/apollota/splitting_of_points.h
+++ src/apollota/splitting_of_points.h
@@ -32,6 +32,37 @@ public:
 		return result;
 	}
 
+	template<typename SphereType>
+	static std::vector< std::vector<std::size_t> > improve_splitting(const std::vector<SphereType>& spheres, const std::vector< std::vector<std::size_t> >& current_splitting)
+	{
+		std::vector<SimplePoint> centers(current_splitting.size());
+		for(std::size_t i=0;i<current_splitting.size();i++)
+		{
+			centers[i]=calc_mass_center(spheres, current_splitting[i]);
+		}
+		std::vector< std::vector<std::size_t> > improved_splitting(centers.size());
+		for(std::size_t i=0;i<improved_splitting.size();i++)
+		{
+			improved_splitting[i].reserve(spheres.size()/centers.size()*2);
+		}
+		for(std::size_t i=0;i<spheres.size();i++)
+		{
+			std::size_t best_center_id=0;
+			double best_distance=maximal_distance_from_point_to_sphere(centers[0], spheres[i]);
+			for(std::size_t j=1;j<centers.size();j++)
+			{
+				const double distance=maximal_distance_from_point_to_sphere(centers[j], spheres[i]);
+				if(distance<best_distance)
+				{
+					best_distance=distance;
+					best_center_id=j;
+				}
+			}
+			improved_splitting[best_center_id].push_back(i);
+		}
+		return improved_splitting;
+	}
+
 private:
 	template<typename PointType>
 	static double coord(const PointType& p, const unsigned int k)
@@ -89,6 +120,21 @@ private:
 			result[1].insert(result[1].end(), middle_it, ids.end());
 			return result;
 		}
+	}
+
+	template<typename PointType>
+	static SimplePoint calc_mass_center(const std::vector<PointType>& points, const std::vector<std::size_t>& ids)
+	{
+		SimplePoint center(0.0, 0.0, 0.0);
+		if(!ids.empty())
+		{
+			for(std::size_t i=0;i<ids.size();i++)
+			{
+				center=center+SimplePoint(points[ids[i]]);
+			}
+			center=center*(1/static_cast<double>(ids.size()));
+		}
+		return center;
 	}
 };
 
