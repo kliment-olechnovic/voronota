a47c0336dea41d8c2af2b8c037f4013736433865
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-08-16 16:32:21 +0300

    Refactored parallel processing code.

--- src/mode_calculate_triangulation_in_parallel.cpp
+++ src/mode_calculate_triangulation_in_parallel.cpp
@@ -24,291 +24,296 @@ struct ParallelComputationResult
 	apollota::Triangulation::QuadruplesMap merged_quadruples_map;
 };
 
+class ParallelComputationProcessingSimulated
+{
+public:
+	static void process(
+			const std::size_t parts,
+			const double init_radius_for_BSH,
+			ParallelComputationResult& result)
+	{
+		std::vector<apollota::SimpleSphere> spheres;
+		auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
+		result.number_of_input_spheres=spheres.size();
+
+		const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
+		result.number_of_initialized_parts=distributed_ids.size();
+
+		const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
+
+		for(std::size_t i=0;i<distributed_ids.size();i++)
+		{
+			const apollota::Triangulation::QuadruplesMap partial_quadruples_map=apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map;
+			result.number_of_produced_quadruples+=partial_quadruples_map.size();
+			apollota::Triangulation::merge_quadruples_maps(partial_quadruples_map, result.merged_quadruples_map);
+		}
+	}
+};
+
+#ifdef _OPENMP
+class ParallelComputationProcessingWithOpenMP
+{
+public:
+	static void process(
+			const std::size_t parts,
+			const double init_radius_for_BSH,
+			ParallelComputationResult& result)
+	{
+		std::vector<apollota::SimpleSphere> spheres;
+		auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
+		result.number_of_input_spheres=spheres.size();
+
+		const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
+		result.number_of_initialized_parts=distributed_ids.size();
+
+		const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
+
+		std::vector<apollota::Triangulation::QuadruplesMap> distributed_quadruples_maps(distributed_ids.size());
+		std::vector<int> distributed_errors(distributed_ids.size(), 0);
+
+		{
+	#pragma omp parallel for
+			for(std::size_t i=0;i<distributed_ids.size();i++)
+			{
+				try
+				{
+					distributed_quadruples_maps[i]=apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map;
+				}
+				catch(...)
+				{
+					distributed_errors[i]=1;
+				}
+			}
+		}
+
+		{
+			std::ostringstream errors_summary_stream;
+			for(std::size_t i=0;i<distributed_errors.size();i++)
+			{
+				if(distributed_errors[i]!=0)
+				{
+					errors_summary_stream << " " << i;
+				}
+			}
+			const std::string errors_summary=errors_summary_stream.str();
+			if(!errors_summary.empty())
+			{
+				throw std::runtime_error("Parallel processing failed because of exceptions in parts:"+errors_summary+".");
+			}
+		}
+
+		for(std::size_t i=0;i<distributed_quadruples_maps.size();i++)
+		{
+			result.number_of_produced_quadruples+=distributed_quadruples_maps[i].size();
+			apollota::Triangulation::merge_quadruples_maps(distributed_quadruples_maps[i], result.merged_quadruples_map);
+		}
+	}
+};
+#endif
+
+#ifdef ENABLE_MPI
+class ParallelComputationProcessingWithMPI
+{
+public:
+	static bool process(
+			const std::vector<std::string>& argv,
+			const std::size_t parts,
+			const double init_radius_for_BSH,
+			ParallelComputationResult& result)
+	{
+		const int quadruples_map_data_tag=1;
+
+		MPIWrapper mpi_wrapper(argv);
+
+		if(mpi_wrapper.size<=2)
+		{
+			if(mpi_wrapper.rank==0)
+			{
+				ParallelComputationProcessingSimulated::process(parts, init_radius_for_BSH, result);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		std::vector<apollota::SimpleSphere> spheres;
+		{
+			std::vector<double> spheres_plain_vector;
+			int spheres_plain_vector_length=0;
+			if(mpi_wrapper.rank==0)
+			{
+				auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
+				fill_plain_vector_from_spheres(spheres, spheres_plain_vector);
+				spheres_plain_vector_length=static_cast<int>(spheres_plain_vector.size());
+			}
+			MPI_Bcast(&spheres_plain_vector_length, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			if(mpi_wrapper.rank!=0)
+			{
+				spheres_plain_vector.resize(static_cast<std::size_t>(spheres_plain_vector_length));
+			}
+			MPI_Bcast(spheres_plain_vector.data(), spheres_plain_vector_length, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			if(mpi_wrapper.rank!=0)
+			{
+				fill_spheres_from_plain_vector(spheres_plain_vector, spheres);
+			}
+		}
+		result.number_of_input_spheres=spheres.size();
+
+		const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
+		result.number_of_initialized_parts=distributed_ids.size();
+
+		if(mpi_wrapper.rank==0)
+		{
+			for(std::size_t i=0;i<distributed_ids.size();i++)
+			{
+				std::vector<double> plain_vector;
+				{
+					MPI_Status status;
+					MPI_Probe(MPI_ANY_SOURCE, quadruples_map_data_tag, MPI_COMM_WORLD, &status);
+					const int source_rank=status.MPI_SOURCE;
+					int plain_vector_size=0;
+					MPI_Get_count(&status, MPI_DOUBLE, &plain_vector_size);
+					if(plain_vector_size>0)
+					{
+						plain_vector.resize(static_cast<std::size_t>(plain_vector_size));
+						MPI_Recv(plain_vector.data(), plain_vector_size, MPI_DOUBLE, source_rank, quadruples_map_data_tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+					}
+				}
+				if(!plain_vector.empty())
+				{
+					apollota::Triangulation::QuadruplesMap partial_quadruples_map;
+					fill_quadruples_map_from_plain_vector(plain_vector, partial_quadruples_map);
+					result.number_of_produced_quadruples+=partial_quadruples_map.size();
+					apollota::Triangulation::merge_quadruples_maps(partial_quadruples_map, result.merged_quadruples_map);
+				}
+			}
+		}
+		else
+		{
+			const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
+			for(std::size_t i=0;i<distributed_ids.size();i++)
+			{
+				const int source_rank=static_cast<int>(i)%(mpi_wrapper.size-1)+1;
+				if(mpi_wrapper.rank==source_rank)
+				{
+					std::vector<double> plain_vector;
+					fill_plain_vector_from_quadruples_map(apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map, plain_vector);
+					MPI_Send(plain_vector.data(), static_cast<int>(plain_vector.size()), MPI_DOUBLE, 0, quadruples_map_data_tag, MPI_COMM_WORLD);
+				}
+			}
+		}
+
+		return (mpi_wrapper.rank==0);
+	}
+private:
+	class MPIWrapper
+	{
+	public:
+		MPIWrapper(const std::vector<std::string>& argv) : argc_(static_cast<int>(argv.size()))
+		{
+			argv_=new char*[argc_];
+			for(int i=0;i<argc_;i++)
+			{
+				argv_[i]=new char[argv[i].size()+1];
+				strcpy(argv_[i], argv[i].c_str());
+			}
+			MPI_Init(&argc_, &argv_);
+	        MPI_Comm_size(MPI_COMM_WORLD, &size);
+	        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		}
+
+		~MPIWrapper()
+		{
+			MPI_Finalize();
+			for(int i=0;i<argc_;i++)
+			{
+				delete[] argv_[i];
+			}
+			delete argv_;
+		}
+
+		int size;
+		int rank;
+
+	private:
+		int argc_;
+		char** argv_;
+
+	};
+
+	static void fill_plain_vector_from_spheres(const std::vector<apollota::SimpleSphere>& spheres, std::vector<double>& plain_vector)
+	{
+		plain_vector.resize(spheres.size()*4);
+		for(std::size_t i=0;i<spheres.size();i++)
+		{
+			plain_vector[i*4+0]=spheres[i].x;
+			plain_vector[i*4+1]=spheres[i].y;
+			plain_vector[i*4+2]=spheres[i].z;
+			plain_vector[i*4+3]=spheres[i].r;
+		}
+	}
+
+	static void fill_spheres_from_plain_vector(const std::vector<double>& plain_vector, std::vector<apollota::SimpleSphere>& spheres)
+	{
+		spheres.resize(plain_vector.size()/4);
+		for(std::size_t i=0;i<spheres.size();i++)
+		{
+			spheres[i].x=plain_vector[i*4+0];
+			spheres[i].y=plain_vector[i*4+1];
+			spheres[i].z=plain_vector[i*4+2];
+			spheres[i].r=plain_vector[i*4+3];
+		}
+	}
+
+	static void fill_plain_vector_from_quadruples_map(const apollota::Triangulation::QuadruplesMap& quadruples_map, std::vector<double>& plain_vector)
+	{
+		plain_vector.resize(apollota::Triangulation::count_tangent_spheres_in_quadruples_map(quadruples_map)*8);
+		std::size_t i=0;
+		for(apollota::Triangulation::QuadruplesMap::const_iterator it=quadruples_map.begin();it!=quadruples_map.end();++it)
+		{
+			const apollota::Quadruple& q=it->first;
+			const std::vector<apollota::SimpleSphere>& ts=it->second;
+			for(std::size_t j=0;j<ts.size();j++)
+			{
+				plain_vector[i*8+0]=q.get(0);
+				plain_vector[i*8+1]=q.get(1);
+				plain_vector[i*8+2]=q.get(2);
+				plain_vector[i*8+3]=q.get(3);
+				plain_vector[i*8+4]=ts[j].x;
+				plain_vector[i*8+5]=ts[j].y;
+				plain_vector[i*8+6]=ts[j].z;
+				plain_vector[i*8+7]=ts[j].r;
+				i++;
+			}
+		}
+	}
+
+	static void fill_quadruples_map_from_plain_vector(const std::vector<double>& plain_vector, apollota::Triangulation::QuadruplesMap& quadruples_map)
+	{
+		quadruples_map.clear();
+		const std::size_t n=(plain_vector.size()/8);
+		for(std::size_t i=0;i<n;i++)
+		{
+			quadruples_map[apollota::Quadruple(
+					static_cast<std::size_t>(plain_vector[i*8+0]+0.5),
+					static_cast<std::size_t>(plain_vector[i*8+1]+0.5),
+					static_cast<std::size_t>(plain_vector[i*8+2]+0.5),
+					static_cast<std::size_t>(plain_vector[i*8+3]+0.5))].push_back(apollota::SimpleSphere(
+							plain_vector[i*8+4],
+							plain_vector[i*8+5],
+							plain_vector[i*8+6],
+							plain_vector[i*8+7]));
+		}
+	}
+};
+#endif
+
 inline bool number_is_power_of_two(const unsigned long x)
 {
 	return ( (x>0) && ((x & (x-1))==0) );
 }
-
-void calculate_triangulation_in_parallel_simulated(
-		const std::size_t parts,
-		const double init_radius_for_BSH,
-		ParallelComputationResult& result)
-{
-	std::vector<apollota::SimpleSphere> spheres;
-	auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
-	result.number_of_input_spheres=spheres.size();
-
-	const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
-	result.number_of_initialized_parts=distributed_ids.size();
-
-	const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
-
-	for(std::size_t i=0;i<distributed_ids.size();i++)
-	{
-		const apollota::Triangulation::QuadruplesMap partial_quadruples_map=apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map;
-		result.number_of_produced_quadruples+=partial_quadruples_map.size();
-		apollota::Triangulation::merge_quadruples_maps(partial_quadruples_map, result.merged_quadruples_map);
-	}
-}
-
-#ifdef _OPENMP
-void calculate_triangulation_in_parallel_with_openmp(
-		const std::size_t parts,
-		const double init_radius_for_BSH,
-		ParallelComputationResult& result)
-{
-	std::vector<apollota::SimpleSphere> spheres;
-	auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
-	result.number_of_input_spheres=spheres.size();
-
-	const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
-	result.number_of_initialized_parts=distributed_ids.size();
-
-	const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
-
-	std::vector<apollota::Triangulation::QuadruplesMap> distributed_quadruples_maps(distributed_ids.size());
-	std::vector<int> distributed_errors(distributed_ids.size(), 0);
-
-	{
-#pragma omp parallel for
-		for(std::size_t i=0;i<distributed_ids.size();i++)
-		{
-			try
-			{
-				distributed_quadruples_maps[i]=apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map;
-			}
-			catch(...)
-			{
-				distributed_errors[i]=1;
-			}
-		}
-	}
-
-	{
-		std::ostringstream errors_summary_stream;
-		for(std::size_t i=0;i<distributed_errors.size();i++)
-		{
-			if(distributed_errors[i]!=0)
-			{
-				errors_summary_stream << " " << i;
-			}
-		}
-		const std::string errors_summary=errors_summary_stream.str();
-		if(!errors_summary.empty())
-		{
-			throw std::runtime_error("Parallel processing failed because of exceptions in parts:"+errors_summary+".");
-		}
-	}
-
-	for(std::size_t i=0;i<distributed_quadruples_maps.size();i++)
-	{
-		result.number_of_produced_quadruples+=distributed_quadruples_maps[i].size();
-		apollota::Triangulation::merge_quadruples_maps(distributed_quadruples_maps[i], result.merged_quadruples_map);
-	}
-}
-#endif
-
-#ifdef ENABLE_MPI
-
-namespace mpi_utilities
-{
-
-class MPIWrapper
-{
-public:
-	MPIWrapper(const std::vector<std::string>& argv) : argc_(static_cast<int>(argv.size()))
-	{
-		argv_=new char*[argc_];
-		for(int i=0;i<argc_;i++)
-		{
-			argv_[i]=new char[argv[i].size()+1];
-			strcpy(argv_[i], argv[i].c_str());
-		}
-		MPI_Init(&argc_, &argv_);
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	}
-
-	~MPIWrapper()
-	{
-		MPI_Finalize();
-		for(int i=0;i<argc_;i++)
-		{
-			delete[] argv_[i];
-		}
-		delete argv_;
-	}
-
-	int size;
-	int rank;
-
-private:
-	int argc_;
-	char** argv_;
-
-};
-
-void fill_plain_vector_from_spheres(const std::vector<apollota::SimpleSphere>& spheres, std::vector<double>& plain_vector)
-{
-	plain_vector.resize(spheres.size()*4);
-	for(std::size_t i=0;i<spheres.size();i++)
-	{
-		plain_vector[i*4+0]=spheres[i].x;
-		plain_vector[i*4+1]=spheres[i].y;
-		plain_vector[i*4+2]=spheres[i].z;
-		plain_vector[i*4+3]=spheres[i].r;
-	}
-}
-
-void fill_spheres_from_plain_vector(const std::vector<double>& plain_vector, std::vector<apollota::SimpleSphere>& spheres)
-{
-	spheres.resize(plain_vector.size()/4);
-	for(std::size_t i=0;i<spheres.size();i++)
-	{
-		spheres[i].x=plain_vector[i*4+0];
-		spheres[i].y=plain_vector[i*4+1];
-		spheres[i].z=plain_vector[i*4+2];
-		spheres[i].r=plain_vector[i*4+3];
-	}
-}
-
-void fill_plain_vector_from_quadruples_map(const apollota::Triangulation::QuadruplesMap& quadruples_map, std::vector<double>& plain_vector)
-{
-	plain_vector.resize(apollota::Triangulation::count_tangent_spheres_in_quadruples_map(quadruples_map)*8);
-	std::size_t i=0;
-	for(apollota::Triangulation::QuadruplesMap::const_iterator it=quadruples_map.begin();it!=quadruples_map.end();++it)
-	{
-		const apollota::Quadruple& q=it->first;
-		const std::vector<apollota::SimpleSphere>& ts=it->second;
-		for(std::size_t j=0;j<ts.size();j++)
-		{
-			plain_vector[i*8+0]=q.get(0);
-			plain_vector[i*8+1]=q.get(1);
-			plain_vector[i*8+2]=q.get(2);
-			plain_vector[i*8+3]=q.get(3);
-			plain_vector[i*8+4]=ts[j].x;
-			plain_vector[i*8+5]=ts[j].y;
-			plain_vector[i*8+6]=ts[j].z;
-			plain_vector[i*8+7]=ts[j].r;
-			i++;
-		}
-	}
-}
-
-void fill_quadruples_map_from_plain_vector(const std::vector<double>& plain_vector, apollota::Triangulation::QuadruplesMap& quadruples_map)
-{
-	quadruples_map.clear();
-	const std::size_t n=(plain_vector.size()/8);
-	for(std::size_t i=0;i<n;i++)
-	{
-		quadruples_map[apollota::Quadruple(
-				static_cast<std::size_t>(plain_vector[i*8+0]+0.5),
-				static_cast<std::size_t>(plain_vector[i*8+1]+0.5),
-				static_cast<std::size_t>(plain_vector[i*8+2]+0.5),
-				static_cast<std::size_t>(plain_vector[i*8+3]+0.5))].push_back(apollota::SimpleSphere(
-						plain_vector[i*8+4],
-						plain_vector[i*8+5],
-						plain_vector[i*8+6],
-						plain_vector[i*8+7]));
-	}
-}
-
-}
-
-bool calculate_triangulation_in_parallel_with_mpi(
-		const std::vector<std::string>& argv,
-		const std::size_t parts,
-		const double init_radius_for_BSH,
-		ParallelComputationResult& result)
-{
-	const int quadruples_map_data_tag=1;
-
-	mpi_utilities::MPIWrapper mpi_wrapper(argv);
-
-	if(mpi_wrapper.size<=2)
-	{
-		if(mpi_wrapper.rank==0)
-		{
-			calculate_triangulation_in_parallel_simulated(parts, init_radius_for_BSH, result);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	std::vector<apollota::SimpleSphere> spheres;
-	{
-		std::vector<double> spheres_plain_vector;
-		int spheres_plain_vector_length=0;
-		if(mpi_wrapper.rank==0)
-		{
-			auxiliaries::read_lines_to_container(std::cin, "#", modes_commons::add_sphere_from_stream_to_vector<apollota::SimpleSphere>, spheres);
-			mpi_utilities::fill_plain_vector_from_spheres(spheres, spheres_plain_vector);
-			spheres_plain_vector_length=static_cast<int>(spheres_plain_vector.size());
-		}
-		MPI_Bcast(&spheres_plain_vector_length, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		if(mpi_wrapper.rank!=0)
-		{
-			spheres_plain_vector.resize(static_cast<std::size_t>(spheres_plain_vector_length));
-		}
-		MPI_Bcast(spheres_plain_vector.data(), spheres_plain_vector_length, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		if(mpi_wrapper.rank!=0)
-		{
-			mpi_utilities::fill_spheres_from_plain_vector(spheres_plain_vector, spheres);
-		}
-	}
-	result.number_of_input_spheres=spheres.size();
-
-	const std::vector< std::vector<std::size_t> > distributed_ids=apollota::SplittingOfSpheres::split_for_number_of_parts(spheres, parts);
-	result.number_of_initialized_parts=distributed_ids.size();
-
-	if(mpi_wrapper.rank==0)
-	{
-		for(std::size_t i=0;i<distributed_ids.size();i++)
-		{
-			std::vector<double> plain_vector;
-			{
-				MPI_Status status;
-				MPI_Probe(MPI_ANY_SOURCE, quadruples_map_data_tag, MPI_COMM_WORLD, &status);
-				const int source_rank=status.MPI_SOURCE;
-				int plain_vector_size=0;
-				MPI_Get_count(&status, MPI_DOUBLE, &plain_vector_size);
-				if(plain_vector_size>0)
-				{
-					plain_vector.resize(static_cast<std::size_t>(plain_vector_size));
-					MPI_Recv(plain_vector.data(), plain_vector_size, MPI_DOUBLE, source_rank, quadruples_map_data_tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				}
-			}
-			if(!plain_vector.empty())
-			{
-				apollota::Triangulation::QuadruplesMap partial_quadruples_map;
-				mpi_utilities::fill_quadruples_map_from_plain_vector(plain_vector, partial_quadruples_map);
-				result.number_of_produced_quadruples+=partial_quadruples_map.size();
-				apollota::Triangulation::merge_quadruples_maps(partial_quadruples_map, result.merged_quadruples_map);
-			}
-		}
-	}
-	else
-	{
-		const apollota::BoundingSpheresHierarchy bsh(spheres, init_radius_for_BSH, 1);
-		for(std::size_t i=0;i<distributed_ids.size();i++)
-		{
-			const int source_rank=static_cast<int>(i)%(mpi_wrapper.size-1)+1;
-			if(mpi_wrapper.rank==source_rank)
-			{
-				std::vector<double> plain_vector;
-				mpi_utilities::fill_plain_vector_from_quadruples_map(apollota::Triangulation::construct_result_for_admittance_set(bsh, distributed_ids[i]).quadruples_map, plain_vector);
-				MPI_Send(plain_vector.data(), static_cast<int>(plain_vector.size()), MPI_DOUBLE, 0, quadruples_map_data_tag, MPI_COMM_WORLD);
-			}
-		}
-	}
-
-	return (mpi_wrapper.rank==0);
-}
-
-#endif
 
 }
 
@@ -384,18 +389,18 @@ void calculate_triangulation_in_parallel(const auxiliaries::ProgramOptionsHandle
 
 	if(method=="simulated")
 	{
-		calculate_triangulation_in_parallel_simulated(parts, init_radius_for_BSH, result);
+		ParallelComputationProcessingSimulated::process(parts, init_radius_for_BSH, result);
 	}
 #ifdef _OPENMP
 	else if(method=="openmp")
 	{
-		calculate_triangulation_in_parallel_with_openmp(parts, init_radius_for_BSH, result);
+		ParallelComputationProcessingWithOpenMP::process(parts, init_radius_for_BSH, result);
 	}
 #endif
 #ifdef ENABLE_MPI
 	else if(method=="mpi")
 	{
-		master_finished=calculate_triangulation_in_parallel_with_mpi(poh.unused_argv(), parts, init_radius_for_BSH, result);
+		master_finished=ParallelComputationProcessingWithMPI::process(poh.unused_argv(), parts, init_radius_for_BSH, result);
 	}
 #endif
 	else
