eced3bef93225e15ead9f9346d9c69c0cba5ac83
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2013-08-31 17:01:28 +0300

    Improved middle region approximation construction. Remove central sphere usage.

--- src/apollota/triangulation.h
+++ src/apollota/triangulation.h
@@ -354,7 +354,6 @@ private:
 			{
 				d_ids_and_tangent_spheres_.resize(2, std::pair<std::size_t, SimpleSphere>(npos, SimpleSphere()));
 				init_central_planes();
-				init_central_sphere();
 				init_middle_region_approximation_sphere();
 			}
 			else
@@ -484,7 +483,7 @@ private:
 
 		void update_middle_region_approximation()
 		{
-			if(can_have_e_ && central_sphere_.first && d_ids_and_tangent_spheres_[0].first!=npos && d_ids_and_tangent_spheres_[1].first!=npos)
+			if(can_have_e_ && can_have_d_ && d_ids_and_tangent_spheres_[0].first!=npos && d_ids_and_tangent_spheres_[1].first!=npos)
 			{
 				const std::vector<SimpleSphere> disk0=TangentSphereOfThreeSpheres::calculate(
 						SimpleSphere(SimplePoint(*a_sphere_)+(sub_of_points<SimplePoint>(d_ids_and_tangent_spheres_[0].second, *a_sphere_).unit()*a_sphere_->r), 0),
@@ -498,9 +497,9 @@ private:
 							SimpleSphere(SimplePoint(*c_sphere_)+(sub_of_points<SimplePoint>(d_ids_and_tangent_spheres_[1].second, *c_sphere_).unit()*c_sphere_->r), 0));
 					if(disk1.size()==1)
 					{
+						const SimplePoint center(sum_of_points<SimplePoint>(disk0.front(), disk1.front())*0.5);
 						middle_region_approximation_sphere_.first=true;
-						middle_region_approximation_sphere_.second=SimpleSphere(central_sphere_.second,
-								std::max(distance_from_point_to_point(central_sphere_.second, disk0.front())+disk0.front().r, distance_from_point_to_point(central_sphere_.second, disk1.front())+disk1.front().r));
+						middle_region_approximation_sphere_.second=SimpleSphere(center, std::max(distance_from_point_to_point(center, disk0.front())+disk0.front().r, distance_from_point_to_point(center, disk1.front())+disk1.front().r));
 					}
 				}
 			}
@@ -608,33 +607,27 @@ private:
 			}
 		}
 
-		void init_central_sphere()
-		{
-			central_sphere_.first=false;
-			if(can_have_d_)
-			{
-				const std::vector<SimpleSphere> minimal_tangent_sphere=TangentSphereOfThreeSpheres::calculate((*a_sphere_), (*b_sphere_), (*c_sphere_));
-				if(minimal_tangent_sphere.size()==1)
-				{
-					central_sphere_.first=true;
-					central_sphere_.second=minimal_tangent_sphere.front();
-				}
-			}
-		}
-
 		void init_middle_region_approximation_sphere()
 		{
 			middle_region_approximation_sphere_.first=false;
-			if(central_sphere_.first)
+			if(can_have_d_)
 			{
-				const std::vector<SimpleSphere> disk=TangentSphereOfThreeSpheres::calculate(
+				const std::vector<SimpleSphere> disk0=TangentSphereOfThreeSpheres::calculate(
 						SimpleSphere(SimplePoint(*a_sphere_)+(tangent_planes_[0].second*(a_sphere_->r)), 0),
 						SimpleSphere(SimplePoint(*b_sphere_)+(tangent_planes_[0].second*(b_sphere_->r)), 0),
 						SimpleSphere(SimplePoint(*c_sphere_)+(tangent_planes_[0].second*(c_sphere_->r)), 0));
-				if(disk.size()==1)
+				if(disk0.size()==1)
 				{
-					middle_region_approximation_sphere_.first=true;
-					middle_region_approximation_sphere_.second=SimpleSphere(central_sphere_.second, distance_from_point_to_point(central_sphere_.second, disk.front())+disk.front().r);
+					const std::vector<SimpleSphere> disk1=TangentSphereOfThreeSpheres::calculate(
+							SimpleSphere(SimplePoint(*a_sphere_)+(tangent_planes_[1].second*(a_sphere_->r)), 0),
+							SimpleSphere(SimplePoint(*b_sphere_)+(tangent_planes_[1].second*(b_sphere_->r)), 0),
+							SimpleSphere(SimplePoint(*c_sphere_)+(tangent_planes_[1].second*(c_sphere_->r)), 0));
+					if(disk1.size()==1)
+					{
+						const SimplePoint center(sum_of_points<SimplePoint>(disk0.front(), disk1.front())*0.5);
+						middle_region_approximation_sphere_.first=true;
+						middle_region_approximation_sphere_.second=SimpleSphere(center, std::max(distance_from_point_to_point(center, disk0.front())+disk0.front().r, distance_from_point_to_point(center, disk1.front())+disk1.front().r));
+					}
 				}
 			}
 		}
@@ -682,7 +675,6 @@ private:
 		bool can_have_d_;
 		bool can_have_e_;
 		std::vector< std::pair<SimplePoint, SimplePoint> > central_planes_;
-		std::pair<bool, SimpleSphere> central_sphere_;
 		std::pair<bool, SimpleSphere> middle_region_approximation_sphere_;
 	};
 
