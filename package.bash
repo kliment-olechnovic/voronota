0a204b65bbedd961ae0267ad57d021c14fcf8bd5
kliment <kliment@localhost>
2013-09-30 15:50:00 +0300

    Added packaging script.

---
+++ package.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PACKAGE_NAME=$1
+
+cd $(dirname "$0")
+
+VERSION_MAJOR_STRING=$(./Release/apollota --version 2>&1 | awk '{print $3}')
+VERSION_MINOR_STRING=$(hg branches | egrep '^default' | tr ':' ' ' | awk '{print $2}')
+VERSION_STRING="apollota_$VERSION_MAJOR_STRING.$VERSION_MINOR_STRING"
+
+if [ -z "$PACKAGE_NAME" ]
+then
+  PACKAGE_NAME=$VERSION_STRING
+fi
+
+rm -f $PACKAGE_NAME.tar.gz
+rm -r -f $PACKAGE_NAME
+
+mkdir -p $PACKAGE_NAME
+cp -r src $PACKAGE_NAME/src
+cp README $PACKAGE_NAME/README
+cp LICENSE $PACKAGE_NAME/LICENSE
+cp CMakeLists.txt $PACKAGE_NAME/CMakeLists.txt
+cp resources/radii $PACKAGE_NAME/radii
+cp Release/apollota $PACKAGE_NAME/apollota
+
+tar -czf "$PACKAGE_NAME.tar.gz" $PACKAGE_NAME
+rm -r $PACKAGE_NAME
