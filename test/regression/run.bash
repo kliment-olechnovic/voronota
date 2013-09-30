2114ab92ef0f602e6aa6a73059fad40c17c95297
kliment <kliment@localhost>
2013-09-30 15:56:29 +0300

    Incorporated packaging into regression testing.

--- test/regression/run.bash
+++ test/regression/run.bash
@@ -2,8 +2,14 @@
 
 cd $(dirname "$0")
 
-TEST_SUBJECT=../../Release/apollota
-RADII_FILE=../../resources/radii
+../../package.bash apollota_package
+mv ../../apollota_package.tar.gz ./apollota_package.tar.gz
+tar -xf ./apollota_package.tar.gz
+rm ./apollota_package.tar.gz
+
+TEST_SUBJECT=./apollota_package/apollota
+RADII_FILE=./apollota_package/radii
+
 INPUT_FILE=./input.pdb
 INPUT_FILE_MMCIF=./input.cif
 OUTPUT_DIR=./output/
@@ -13,8 +19,6 @@ mkdir $OUTPUT_DIR
 
 $TEST_SUBJECT --help 2> $OUTPUT_DIR/help_message
 $TEST_SUBJECT --help-full 2> $OUTPUT_DIR/help_full_message
-
-set -e
 
 $TEST_SUBJECT --mode get-balls-from-atoms-file --radii-file $RADII_FILE < $INPUT_FILE > $OUTPUT_DIR/balls1
 $TEST_SUBJECT --mode get-balls-from-atoms-file --radii-file $RADII_FILE --include-heteroatoms < $INPUT_FILE > $OUTPUT_DIR/balls2
@@ -33,5 +37,7 @@ $TEST_SUBJECT --mode compare-triangulations --first-triangulation-file $OUTPUT_D
 $TEST_SUBJECT --mode compare-triangulations --first-triangulation-file $OUTPUT_DIR/triangulation1 --second-triangulation-file $OUTPUT_DIR/triangulation3 < $OUTPUT_DIR/balls3 > $OUTPUT_DIR/comparison_1_to_3_on_3
 $TEST_SUBJECT --mode compare-triangulations --first-triangulation-file $OUTPUT_DIR/triangulation1 --second-triangulation-file $OUTPUT_DIR/triangulation1p < $OUTPUT_DIR/balls1 > $OUTPUT_DIR/comparison_1_to_1p
 $TEST_SUBJECT --mode compare-triangulations --first-triangulation-file $OUTPUT_DIR/triangulation2 --second-triangulation-file $OUTPUT_DIR/triangulation2p < $OUTPUT_DIR/balls1 > $OUTPUT_DIR/comparison_2_to_2p
+
+rm -r ./apollota_package
 
 hg status ./
