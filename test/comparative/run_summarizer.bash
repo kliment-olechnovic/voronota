6b8f314631be392c88b357b08f5acba721c283ca
kliment <kliment@localhost>
2013-11-29 13:46:14 +0200

    Restored comparative testing scripts.

---
+++ test/comparative/run_summarizer.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+WORKING_DIR=$1
+PDB_FILE=$2
+
+PDB_FILE_BASENAME=$(basename $PDB_FILE .ent.gz)
+PDB_FILE_DOMAIN=$(echo $PDB_FILE_BASENAME | sed 's/pdb.\(..\)./\1/')
+WORKING_DIR="$WORKING_DIR/$PDB_FILE_DOMAIN"
+
+FIRST_GAUGE_LOG_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.first_gauge.log"
+SECOND_GAUGE_LOG_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.second_gauge.log"
+TEST_SUBJECT_LOG_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.test_subject.log"
+FIRST_COMPARISON_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.comparison.first"
+SECOND_COMPARISON_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.comparison.second"
+
+if [ ! -s "$FIRST_GAUGE_LOG_FILE" ] ; then exit 1 ; fi
+if [ ! -s "$SECOND_GAUGE_LOG_FILE" ] ; then exit 1 ; fi
+if [ ! -s "$TEST_SUBJECT_LOG_FILE" ] ; then exit 1 ; fi
+if [ ! -s "$FIRST_COMPARISON_FILE" ] ; then exit 1 ; fi
+if [ ! -s "$SECOND_COMPARISON_FILE" ] ; then exit 1 ; fi
+
+SUMMARY_LIST_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.summary.list"
+SUMMARY_TABLE_FILE="$WORKING_DIR/$PDB_FILE_BASENAME.summary.table"
+
+echo "input $PDB_FILE_BASENAME" > $SUMMARY_LIST_FILE
+cat $FIRST_GAUGE_LOG_FILE | sed 's/^/first_gauge_/' >> $SUMMARY_LIST_FILE
+cat $SECOND_GAUGE_LOG_FILE | sed 's/^/second_gauge_/' >> $SUMMARY_LIST_FILE
+cat $TEST_SUBJECT_LOG_FILE | sed 's/^/test_subject_/' >> $SUMMARY_LIST_FILE
+cat $FIRST_COMPARISON_FILE | sed 's/^/first_/' >> $SUMMARY_LIST_FILE
+cat $SECOND_COMPARISON_FILE | sed 's/^/second_/' >> $SUMMARY_LIST_FILE
+
+( cat $SUMMARY_LIST_FILE | cut --delimiter " " --fields 1 | paste -s ; cat $SUMMARY_LIST_FILE | cut --delimiter " " --fields 2 | paste -s ) | column -t > $SUMMARY_TABLE_FILE
---
+++ test/comparative/separately_run_test_subject_in_parallel.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+TEST_SUBJECT=$1
+EPSILON=$2
+INPUT_DIR=$3
+OUTPUT_DIR=$4
+PDB_FILE=$5
+
+PDB_FILE_BASENAME=$(basename $PDB_FILE .ent.gz)
+PDB_FILE_DOMAIN=$(echo $PDB_FILE_BASENAME | sed 's/pdb.\(..\)./\1/')
+INPUT_DIR="$INPUT_DIR/$PDB_FILE_DOMAIN"
+
+mkdir -p $OUTPUT_DIR
+
+TMP_DIR=$(mktemp -d)
+
+ZIPPED_INPUT_FILE="$INPUT_DIR/$PDB_FILE_BASENAME.first_gauge.balls.gz"
+SEQUENTIAL_LOG_FILE="$INPUT_DIR/$PDB_FILE_BASENAME.test_subject.log"
+INPUT_FILE="$TMP_DIR/input.balls"
+RAW_LOG_FILE="$TMP_DIR/raw_log"
+RAW_TIME_FILE="$TMP_DIR/raw_time"
+LOG_LIST_FILE="$OUTPUT_DIR/$PDB_FILE_BASENAME.parallel.log.list"
+LOG_TABLE_FILE="$OUTPUT_DIR/$PDB_FILE_BASENAME.parallel.log.table"
+
+zcat $ZIPPED_INPUT_FILE > $INPUT_FILE
+
+if [ ! -s "$INPUT_FILE" ]
+then
+	exit 1
+fi
+
+( time -p ($TEST_SUBJECT --mode calculate-triangulation-in-parallel --method openmp --parts 4 --clog-file $RAW_LOG_FILE --epsilon $EPSILON --print-log --skip-output < $INPUT_FILE > /dev/null 2> /dev/null) ) 2> $RAW_TIME_FILE
+
+echo "input $PDB_FILE_BASENAME" > $LOG_LIST_FILE
+cat $SEQUENTIAL_LOG_FILE | sed 's/^/sequential_/' >> $LOG_LIST_FILE
+cat $RAW_LOG_FILE | sed 's/^/parallel_/' >> $LOG_LIST_FILE
+cat $RAW_TIME_FILE | sed 's/^/parallel_time_/' >> $LOG_LIST_FILE
+
+( cat $LOG_LIST_FILE | cut --delimiter " " --fields 1 | paste -s ; cat $LOG_LIST_FILE | cut --delimiter " " --fields 2 | paste -s ) | column -t > $LOG_TABLE_FILE
+
+rm -r "$TMP_DIR"
