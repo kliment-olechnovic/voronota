b7e4267170b16b6229c46c79ab5a7ab7a722b2dd
kliment <kliment@localhost>
2014-01-31 13:43:24 +0200

    Refactored.

--- src/apollota/opengl_printer.h
+++ src/apollota/opengl_printer.h
@@ -13,7 +13,11 @@ namespace apollota
 class OpenGLPrinter
 {
 public:
-	OpenGLPrinter(std::ostream& output_stream, const std::string& obj_name, const std::string& cgo_name) : output_stream_(output_stream), obj_name_(obj_name), cgo_name_(cgo_name)
+	OpenGLPrinter(std::ostream& output_stream, const std::string& obj_name, const std::string& cgo_name) :
+		output_stream_(output_stream),
+		indent_(4, ' '),
+		obj_name_(obj_name),
+		cgo_name_(cgo_name)
 	{
 		string_stream_ << obj_name_ << " = [\n";
 	}
@@ -32,23 +36,23 @@ public:
 
 	void print_alpha(const double alpha)
 	{
-		string_stream_ << "    ALPHA, " << alpha << ",\n";
+		string_stream_ << indent_ << "ALPHA, " << alpha << ",\n";
 	}
 
 	void print_color(const double r, const double g, const double b)
 	{
-		string_stream_ << "    COLOR, " << rgb_to_string(r, g, b) << ",\n";
+		string_stream_ << indent_ << "COLOR, " << rgb_to_string(r, g, b) << ",\n";
 	}
 
 	void print_color(const unsigned int rgb)
 	{
-		string_stream_ << "    COLOR, " << rgb_to_string(rgb) << ",\n";
+		string_stream_ << indent_ << "COLOR, " << rgb_to_string(rgb) << ",\n";
 	}
 
 	template<typename SphereType>
 	void print_sphere(const SphereType& sphere)
 	{
-		string_stream_ << "    SPHERE, " << point_to_string(sphere) << ", " << sphere.r << ",\n";
+		string_stream_ << indent_ << "SPHERE, " << point_to_string(sphere) << ", " << sphere.r << ",\n";
 	}
 
 	template<typename PointType>
@@ -58,17 +62,17 @@ public:
 		{
 			if(loop)
 			{
-				string_stream_ << "    BEGIN, LINE_LOOP,\n";
+				string_stream_ << indent_ << "BEGIN, LINE_LOOP,\n";
 			}
 			else
 			{
-				string_stream_ << "    BEGIN, LINE_STRIP,\n";
+				string_stream_ << indent_ << "BEGIN, LINE_STRIP,\n";
 			}
 			for(std::size_t i=0;i<vertices.size();i++)
 			{
-				string_stream_ << "    VERTEX, " << point_to_string(vertices[i]) << ",\n";
+				string_stream_ << indent_ << "VERTEX, " << point_to_string(vertices[i]) << ",\n";
 			}
-			string_stream_ << "    END,\n";
+			string_stream_ << indent_ << "END,\n";
 		}
 	}
 
@@ -77,13 +81,13 @@ public:
 	{
 		if(!vertices.empty() && vertices.size()==normals.size())
 		{
-			string_stream_ << "    BEGIN, TRIANGLE_STRIP,\n";
+			string_stream_ << indent_ << "BEGIN, TRIANGLE_STRIP,\n";
 			for(std::size_t i=0;i<vertices.size();i++)
 			{
-				string_stream_ << "    NORMAL, " << point_to_string(normals[i]) << ",\n";
-				string_stream_ << "    VERTEX, " << point_to_string(vertices[i]) << ",\n";
+				string_stream_ << indent_ << "NORMAL, " << point_to_string(normals[i]) << ",\n";
+				string_stream_ << indent_ << "VERTEX, " << point_to_string(vertices[i]) << ",\n";
 			}
-			string_stream_ << "    END,\n";
+			string_stream_ << indent_ << "END,\n";
 		}
 	}
 
@@ -92,13 +96,13 @@ public:
 	{
 		if(!vertices.empty() && vertices.size()==normals.size())
 		{
-			string_stream_ << "    BEGIN, TRIANGLE_FAN,\n";
+			string_stream_ << indent_ << "BEGIN, TRIANGLE_FAN,\n";
 			for(std::size_t i=0;i<vertices.size();i++)
 			{
-				string_stream_ << "    NORMAL, " << point_to_string(normals[i]) << ",\n";
-				string_stream_ << "    VERTEX, " << point_to_string(vertices[i]) << ",\n";
+				string_stream_ << indent_ << "NORMAL, " << point_to_string(normals[i]) << ",\n";
+				string_stream_ << indent_ << "VERTEX, " << point_to_string(vertices[i]) << ",\n";
 			}
-			string_stream_ << "    END,\n";
+			string_stream_ << indent_ << "END,\n";
 		}
 	}
 
@@ -107,24 +111,24 @@ public:
 	{
 		if(!vertices.empty())
 		{
-			string_stream_ << "    BEGIN, TRIANGLE_FAN,\n";
-			string_stream_ << "    NORMAL, " << point_to_string(normal) << ",\n";
-			string_stream_ << "    VERTEX, " << point_to_string(center) << ",\n";
+			string_stream_ << indent_ << "BEGIN, TRIANGLE_FAN,\n";
+			string_stream_ << indent_ << "NORMAL, " << point_to_string(normal) << ",\n";
+			string_stream_ << indent_ << "VERTEX, " << point_to_string(center) << ",\n";
 			for(std::size_t i=0;i<vertices.size();i++)
 			{
-				string_stream_ << "    NORMAL, " << point_to_string(normal) << ",\n";
-				string_stream_ << "    VERTEX, " << point_to_string(vertices[i]) << ",\n";
+				string_stream_ << indent_ << "NORMAL, " << point_to_string(normal) << ",\n";
+				string_stream_ << indent_ << "VERTEX, " << point_to_string(vertices[i]) << ",\n";
 			}
-			string_stream_ << "    NORMAL, " << point_to_string(normal) << ",\n";
-			string_stream_ << "    VERTEX, " << point_to_string(vertices.front()) << ",\n";
-			string_stream_ << "    END,\n";
+			string_stream_ << indent_ << "NORMAL, " << point_to_string(normal) << ",\n";
+			string_stream_ << indent_ << "VERTEX, " << point_to_string(vertices.front()) << ",\n";
+			string_stream_ << indent_ << "END,\n";
 		}
 	}
 
 	template<typename PointType>
 	void print_cylinder(const PointType& p1, const PointType& p2, const double radius, const unsigned int rgb1, const unsigned int rgb2)
 	{
-		string_stream_ << "    CYLINDER, " << point_to_string(p1) << ", " << point_to_string(p2) << ", " << radius << ", " << rgb_to_string(rgb1) << ", " << rgb_to_string(rgb2) << ",\n";
+		string_stream_ << indent_ << "CYLINDER, " << point_to_string(p1) << ", " << point_to_string(p2) << ", " << radius << ", " << rgb_to_string(rgb1) << ", " << rgb_to_string(rgb2) << ",\n";
 	}
 
 private:
@@ -153,6 +157,7 @@ private:
 	}
 
 	std::ostream& output_stream_;
+	std::string indent_;
 	const std::string obj_name_;
 	const std::string cgo_name_;
 	std::ostringstream string_stream_;
