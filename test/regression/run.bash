08348eb6513e39cd6a3aad2c66678e5076cdbffc
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-07-23 15:58:30 +0300

    Added regression test for matching with external list of contact names.

--- test/regression/run.bash
+++ test/regression/run.bash
@@ -47,6 +47,7 @@ $TEST_SUBJECT --mode calculate-contacts --print-log --clog-file $OUTPUT_DIR/log_
 ($TEST_SUBJECT --mode calculate-contacts-query --inter-residue --match-first 'rn[THR]' --match-second 'rn[ARG]' | column -t) < $OUTPUT_DIR/contacts4 > $OUTPUT_DIR/contacts4_query2
 cat $OUTPUT_DIR/balls4 | $TEST_SUBJECT --mode calculate-contacts --annotate --draw | $TEST_SUBJECT --mode calculate-contacts-query --match-first 'rn[CRO]' --preserve-graphics --drawing-for-pymol $OUTPUT_DIR/contacts4_query3_drawing_for_pymol --drawing-for-jmol $OUTPUT_DIR/contacts4_query3_drawing_for_jmol > $OUTPUT_DIR/contacts4_query3
 ($TEST_SUBJECT --mode calculate-contacts-query --no-solvent --match-min-area 10.0 | column -t) < $OUTPUT_DIR/contacts4 > $OUTPUT_DIR/contacts4_query4
+($TEST_SUBJECT --mode calculate-contacts-query --match-external-annotations $OUTPUT_DIR/contacts4_query2 | column -t) < $OUTPUT_DIR/contacts4 > $OUTPUT_DIR/contacts4_query5
 
 rm -r ./voronota_package
 
