751e7a33e9560d41005365630dbb8581301bac42
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-08-05 10:22:52 +0300

    Removed non-utilized parts.

--- test/comparative/prepare_for_retest.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WORKING_DIR=$1
-
-find $WORKING_DIR -type f -name "*.summary.*" |  while read DFILE; do rm $DFILE ; done
-find $WORKING_DIR -type f -name "*.test_subject.*" |  while read DFILE; do rm $DFILE ; done
-find $WORKING_DIR -type f -name "*.comparison.*" |  while read DFILE; do rm $DFILE ; done
