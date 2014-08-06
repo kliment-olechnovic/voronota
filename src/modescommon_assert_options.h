041cec68e783e2b4d3ab02cda6d17f0a272f79b8
kliment <kliment@localhost>
2014-08-06 16:24:48 +0300

    Improved running modes system.

--- src/modescommon_assert_options.h
+++ src/modescommon_assert_options.h
@@ -7,10 +7,12 @@ namespace modescommon
 {
 
 inline bool assert_options(
-		const std::vector<auxiliaries::ProgramOptionsHandler::OptionDescription>& basic_list_of_option_descriptions,
+		const std::vector<auxiliaries::ProgramOptionsHandler::OptionDescription>& list_of_option_descriptions,
 		const auxiliaries::ProgramOptionsHandler& poh,
 		const bool allow_unrecognized_options)
 {
+	std::vector<auxiliaries::ProgramOptionsHandler::OptionDescription> basic_list_of_option_descriptions=list_of_option_descriptions;
+	basic_list_of_option_descriptions.push_back(auxiliaries::ProgramOptionsHandler::OptionDescription("--help", "", "flag to print usage help to stderr and exit"));
 	if(poh.contains_option("--help"))
 	{
 		if(!basic_list_of_option_descriptions.empty())
