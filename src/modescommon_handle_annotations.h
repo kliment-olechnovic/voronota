8f530028bb2ee1fc7221d700e8f3b2fd641f958a
kliment <kliment@localhost>
2014-09-02 14:49:30 +0300

    Refactored.

---
+++ src/modescommon_handle_annotations.h
@@ -0,0 +1,52 @@
+#ifndef MODESCOMMON_HANDLE_ANNOTATIONS_H_
+#define MODESCOMMON_HANDLE_ANNOTATIONS_H_
+
+#include <set>
+#include <vector>
+
+#include "auxiliaries/chain_residue_atom_descriptor.h"
+
+namespace modescommon
+{
+
+inline bool match_chain_residue_atom_descriptor(const auxiliaries::ChainResidueAtomDescriptor& full_descriptor, const std::vector<std::string>& positive_descriptors, const std::vector<std::string>& negative_descriptors)
+{
+	for(std::size_t i=0;i<positive_descriptors.size();i++)
+	{
+		if(!auxiliaries::ChainResidueAtomDescriptor::match_with_member_descriptor(full_descriptor, positive_descriptors[i]))
+		{
+			return false;
+		}
+	}
+	for(std::size_t i=0;i<negative_descriptors.size();i++)
+	{
+		if(auxiliaries::ChainResidueAtomDescriptor::match_with_member_descriptor(full_descriptor, negative_descriptors[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+inline bool match_set_of_tags(const std::set<std::string>& tags, const std::vector<std::string>& positive_values, const std::vector<std::string>& negative_values)
+{
+	for(std::size_t i=0;i<positive_values.size();i++)
+	{
+		if(tags.count(positive_values[i])==0)
+		{
+			return false;
+		}
+	}
+	for(std::size_t i=0;i<negative_values.size();i++)
+	{
+		if(tags.count(negative_values[i])>0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+}
+
+#endif /* MODESCOMMON_HANDLE_ANNOTATIONS_H_ */
