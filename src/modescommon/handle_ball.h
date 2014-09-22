936749a935435f1250990ad6c4f9b5ba80ab5ef1
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-09-22 13:39:14 +0300

    Refactored.

--- src/modescommon/handle_ball.h
+++ src/modescommon/handle_ball.h
@@ -20,8 +20,7 @@ struct BallValue
 	}
 };
 
-template<typename T>
-inline void print_ball_record(const T& name, const BallValue& value, std::ostream& output)
+inline void print_ball_record(const auxiliaries::ChainResidueAtomDescriptor& name, const BallValue& value, std::ostream& output)
 {
 	output << name.str() << " " << value.x << " " << value.y << " " << value.z << " " << value.r;
 	output << " " << (value.tags.empty() ? std::string(".") : auxiliaries::print_set_to_string(value.tags, ";"));
@@ -29,8 +28,7 @@ inline void print_ball_record(const T& name, const BallValue& value, std::ostrea
 	output << "\n";
 }
 
-template<typename T>
-inline bool add_ball_record_from_stream_to_vector(std::istream& input, std::vector< std::pair<T, BallValue> >& vector_of_records)
+inline bool add_ball_record_from_stream_to_vector(std::istream& input, std::vector< std::pair<auxiliaries::ChainResidueAtomDescriptor, BallValue> >& vector_of_records)
 {
 	std::string name_string;
 	BallValue value;
@@ -47,7 +45,7 @@ inline bool add_ball_record_from_stream_to_vector(std::istream& input, std::vect
 	}
 	if(!input.fail() && !name_string.empty())
 	{
-		const T name=T::from_str(name_string);
+		const auxiliaries::ChainResidueAtomDescriptor name=auxiliaries::ChainResidueAtomDescriptor::from_str(name_string);
 		if(name.valid())
 		{
 			vector_of_records.push_back(std::make_pair(name, value));
@@ -57,8 +55,8 @@ inline bool add_ball_record_from_stream_to_vector(std::istream& input, std::vect
 	return false;
 }
 
-template<typename T, typename SphereType>
-inline void collect_spheres_from_vector_of_ball_records(const std::vector< std::pair<T, BallValue> >& vector_of_records, std::vector<SphereType>& spheres)
+template<typename SphereType>
+inline void collect_spheres_from_vector_of_ball_records(const std::vector< std::pair<auxiliaries::ChainResidueAtomDescriptor, BallValue> >& vector_of_records, std::vector<SphereType>& spheres)
 {
 	spheres.resize(vector_of_records.size());
 	for(std::size_t i=0;i<vector_of_records.size();i++)
