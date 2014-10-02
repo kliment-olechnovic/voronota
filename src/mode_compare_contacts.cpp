fcaa50a361e4f4592358250d52f93803df22535a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-10-02 13:05:11 +0300

    Started adding comparison functionality.

--- src/mode_compare_contacts.cpp
+++ src/mode_compare_contacts.cpp
@@ -45,6 +45,51 @@ struct CADDescriptor
 	}
 };
 
+std::map< std::pair<CRAD, CRAD>, double > summarize_pair_mapping_of_values(const std::map< std::pair<CRAD, CRAD>, double >& map)
+{
+	std::map< std::pair<CRAD, CRAD>, double > result;
+	for(std::map< std::pair<CRAD, CRAD>, double >::const_iterator it=map.begin();it!=map.end();++it)
+	{
+		const std::pair<CRAD, CRAD>& crads=it->first;
+		result[modescommon::refine_pair_by_ordering(std::make_pair(crads.first.without_atom(), crads.second.without_atom()))]+=it->second;
+	}
+	return result;
+}
+
+std::map< std::pair<CRAD, CRAD>, std::pair<double, double> > combine_two_pair_mappings_of_values(const std::map< std::pair<CRAD, CRAD>, double >& map1, const std::map< std::pair<CRAD, CRAD>, double >& map2)
+{
+	std::map< std::pair<CRAD, CRAD>, std::pair<double, double> > result;
+	for(std::map< std::pair<CRAD, CRAD>, double >::const_iterator it=map1.begin();it!=map1.end();++it)
+	{
+		result[it->first].first=it->second;
+	}
+	for(std::map< std::pair<CRAD, CRAD>, double >::const_iterator it=map2.begin();it!=map2.end();++it)
+	{
+		result[it->first].second=it->second;
+	}
+	return result;
+}
+
+std::map< std::pair<CRAD, CRAD>, CADDescriptor > construct_map_of_cad_descriptors(const std::map< std::pair<CRAD, CRAD>, std::pair<double, double> >& map_of_value_pairs)
+{
+	std::map< std::pair<CRAD, CRAD>, CADDescriptor > result;
+	for(std::map< std::pair<CRAD, CRAD>, std::pair<double, double> >::const_iterator it=map_of_value_pairs.begin();it!=map_of_value_pairs.end();++it)
+	{
+		result[it->first].add(it->second.first, it->second.second);
+	}
+	return result;
+}
+
+CADDescriptor construct_global_cad_descriptor(const std::map< std::pair<CRAD, CRAD>, CADDescriptor >& map_of_descriptors)
+{
+	CADDescriptor result;
+	for(std::map< std::pair<CRAD, CRAD>, CADDescriptor >::const_iterator it=map_of_descriptors.begin();it!=map_of_descriptors.end();++it)
+	{
+		result.add(it->second);
+	}
+	return result;
+}
+
 inline void print_score(const std::string& name, const CADDescriptor& cadd, std::ostream& output)
 {
 	output << name << " ";
@@ -107,5 +152,42 @@ void compare_contacts(const auxiliaries::ProgramOptionsHandler& poh)
 	const std::string inter_residue_scores_file=poh.argument<std::string>("--inter-residue-scores-file", "");
 	const std::string atom_scores_file=poh.argument<std::string>("--atom-scores-file", "");
 	const std::string residue_scores_file=poh.argument<std::string>("--residue-scores-file", "");
-//	const int depth=poh.argument<int>("--depth", 0);
+	const int depth=poh.argument<int>("--depth", 0);
+
+	std::map< std::pair<CRAD, CRAD>, double > map_of_contacts;
+	{
+		auxiliaries::read_lines_to_container(std::cin, modescommon::add_chain_residue_atom_descriptors_pair_value_from_stream_to_map<false>, map_of_contacts);
+		if(map_of_contacts.empty())
+		{
+			throw std::runtime_error("No contacts input.");
+		}
+	}
+
+	std::map< std::pair<CRAD, CRAD>, double > map_of_target_contacts;
+	{
+		auxiliaries::read_lines_to_container(std::cin, modescommon::add_chain_residue_atom_descriptors_pair_value_from_stream_to_map<false>, map_of_target_contacts);
+		if(map_of_target_contacts.empty())
+		{
+			throw std::runtime_error("No target contacts input.");
+		}
+	}
+
+	const std::map< std::pair<CRAD, CRAD>, CADDescriptor > map_of_inter_atom_cad_descriptors=construct_map_of_cad_descriptors(combine_two_pair_mappings_of_values(map_of_target_contacts, map_of_contacts));
+	print_pair_scores_to_file(map_of_inter_atom_cad_descriptors, inter_atom_scores_file);
+
+	const std::map< std::pair<CRAD, CRAD>, CADDescriptor > map_of_inter_residue_cad_descriptors=construct_map_of_cad_descriptors(combine_two_pair_mappings_of_values(summarize_pair_mapping_of_values(map_of_target_contacts), summarize_pair_mapping_of_values(map_of_contacts)));
+	print_pair_scores_to_file(map_of_inter_residue_cad_descriptors, inter_residue_scores_file);
+
+	if(!atom_scores_file.empty())
+	{
+		print_single_scores_to_file(modescommon::construct_single_mapping_of_descriptors_from_pair_mapping_of_descriptors(map_of_inter_atom_cad_descriptors, depth), atom_scores_file);
+	}
+
+	if(!residue_scores_file.empty())
+	{
+		print_single_scores_to_file(modescommon::construct_single_mapping_of_descriptors_from_pair_mapping_of_descriptors(map_of_inter_residue_cad_descriptors, depth), residue_scores_file);
+	}
+
+	print_score("atom_level_global", construct_global_cad_descriptor(map_of_inter_atom_cad_descriptors), std::cout);
+	print_score("residue_level_global", construct_global_cad_descriptor(map_of_inter_residue_cad_descriptors), std::cout);
 }
