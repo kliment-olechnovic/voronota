972d469c73dcc71ce00508c35eedb4567538372e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-11-10 13:35:38 +0200

    Enhanced pipeline test script.

--- test/pipelines/run_scoring_on_casp_archives.bash
+++ test/pipelines/run_scoring_on_casp_archives.bash
@@ -37,6 +37,8 @@ mkdir -p sequences
 mkdir -p alignments
 mkdir -p contacts
 mkdir -p qscores
+mkdir -p qscores_atom
+mkdir -p qscores_residue
 mkdir -p cadscores
 
 (echo $DOWNLOADS_DIR/$TARGET_NAME.pdb ; find $DOWNLOADS_DIR/$TARGET_NAME -type f) | while read MODEL_FILE
@@ -49,7 +51,7 @@ do
 		$BIN_DIR/voronota query-balls --set-ref-seq-num-adjunct sequences/$TARGET_NAME --ref-seq-alignment alignments/$MODEL_NAME < balls/raw_$MODEL_NAME | $BIN_DIR/voronota query-balls --renumber-from-adjunct refseq > balls/$MODEL_NAME ; rm balls/raw_$MODEL_NAME
 		$BIN_DIR/voronota calculate-contacts --annotated < balls/$MODEL_NAME > contacts/$MODEL_NAME
 	fi
-	cat contacts/$MODEL_NAME | $BIN_DIR/voronota score-contacts --potential-file $BIN_DIR/potential | sed "s/^/$TARGET_NAME $MODEL_NAME /" > qscores/$MODEL_NAME
+	cat contacts/$MODEL_NAME | $BIN_DIR/voronota score-contacts --erf-mean 0.4 --erf-sd 0.2 --potential-file $BIN_DIR/potential --atom-scores-file qscores_atom/$MODEL_NAME --residue-scores-file qscores_residue/$MODEL_NAME | sed "s/^/$TARGET_NAME $MODEL_NAME /" > qscores/$MODEL_NAME
 	$BIN_DIR/voronota query-contacts --match-min-seq-sep 1 --no-solvent < contacts/$MODEL_NAME | $BIN_DIR/voronota compare-contacts --target-contacts-file <($BIN_DIR/voronota query-contacts --match-min-seq-sep 1 --no-solvent < contacts/$TARGET_NAME) | sed "s/^/$TARGET_NAME $MODEL_NAME /" > cadscores/$MODEL_NAME
 done
 
