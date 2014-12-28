1f5e5ecbd8a2d7c9961bd028a1f3c49e211580d4
kliment <kliment@localhost>
2014-12-28 10:32:17 +0200

    Enhanced preparatory pipeline scripts for potentials collection.

--- analysis/potentials/scripts/prepare_bin_dir.bash
+++ analysis/potentials/scripts/prepare_bin_dir.bash
@@ -6,3 +6,4 @@ mkdir -p $BIN_DIR
 cp ~/workspace/voronota/Release/voronota $BIN_DIR/voronota
 cp ~/workspace/voronota/resources/radii $BIN_DIR/radii
 cp ~/workspace/voronota/analysis/potentials/scripts/prepare_enhanced_raw_contacts.bash $BIN_DIR/prepare_enhanced_raw_contacts.bash
+cp ~/workspace/voronota/analysis/potentials/scripts/prepare_contacts_summaries.bash $BIN_DIR/prepare_contacts_summaries.bash
