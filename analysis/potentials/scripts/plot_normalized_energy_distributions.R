307a110ea1cf79689995b24a6ebaab4f7319ebfb
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2014-12-30 16:28:24 +0200

    Improved script to plot atomic normalized energies distribution.

--- analysis/potentials/scripts/plot_normalized_energy_distributions.R
+++ analysis/potentials/scripts/plot_normalized_energy_distributions.R
@@ -30,3 +30,30 @@ quantile(mt$means);
 
 sd(t$V2);
 quantile(mt$sds);
+
+#####################################
+
+mean_addition=0.3;
+
+mean_homo=mean(t$V2);
+sd_homo=sd(t$V2);
+
+scores_homo=rep(0, length(t$V2));
+scores_hetero=scores_homo;
+for(name in names)
+{
+	sel=which(t$V1==name);
+	scores_homo[sel]=(1-pnorm(t$V2[sel], mean=(mean_homo+mean_addition), sd=sd_homo));
+	mt_hetero=mt[which(mt$names==name),];
+	scores_hetero[sel]=(1-pnorm(t$V2[sel], mean=(mt_hetero$means[1]+mean_addition), sd=mt_hetero$sds[1]));
+}
+
+quantile(scores_homo);
+quantile(scores_hetero);
+
+histbreaks=seq(min(c(scores_homo, scores_hetero))-0.05, max(c(scores_homo, scores_hetero))+0.05, 0.02);
+hist(scores_hetero, breaks=histbreaks, col=rgb(0, 0, 1, 0.5), freq=FALSE);
+hist(scores_homo, breaks=histbreaks, col=rgb(1, 0, 0, 0.5), freq=FALSE, add=TRUE);
+
+sample_sel=sample(1:length(scores_homo), 10000);
+plot(x=scores_homo[sample_sel], y=scores_hetero[sample_sel], cex=0.2, xlim=c(0, 1), ylim=c(0, 1));
