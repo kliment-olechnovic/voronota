34989e61d4ea402df5b492e2bec38c588675e6ef
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-02-10 11:34:44 +0200

    Added residue frequency adjustment for scoring potentials.

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ void calculate_contacts(const auxiliaries::ProgramOptionsHandler&);
 void query_balls(const auxiliaries::ProgramOptionsHandler&);
 void query_contacts(const auxiliaries::ProgramOptionsHandler&);
 void count_residue_types(const auxiliaries::ProgramOptionsHandler&);
+void count_residue_types_coefficients(const auxiliaries::ProgramOptionsHandler&);
 void score_contacts_potential(const auxiliaries::ProgramOptionsHandler&);
 void score_contacts_energy(const auxiliaries::ProgramOptionsHandler&);
 void score_contacts_quality(const auxiliaries::ProgramOptionsHandler&);
@@ -49,6 +50,7 @@ std::vector<ModeDescriptor> get_list_of_modes()
 	list_of_modes.push_back(ModeDescriptor("query-balls", ModeDescriptor::FunctionPtr(query_balls)));
 	list_of_modes.push_back(ModeDescriptor("query-contacts", ModeDescriptor::FunctionPtr(query_contacts)));
 	list_of_modes.push_back(ModeDescriptor("count-residue-types", ModeDescriptor::FunctionPtr(count_residue_types)));
+	list_of_modes.push_back(ModeDescriptor("count-residue-types-coefficients", ModeDescriptor::FunctionPtr(count_residue_types_coefficients)));
 	list_of_modes.push_back(ModeDescriptor("score-contacts-potential", ModeDescriptor::FunctionPtr(score_contacts_potential)));
 	list_of_modes.push_back(ModeDescriptor("score-contacts-energy", ModeDescriptor::FunctionPtr(score_contacts_energy)));
 	list_of_modes.push_back(ModeDescriptor("score-contacts-quality", ModeDescriptor::FunctionPtr(score_contacts_quality)));
--- src/mode_count_residue_types.cpp
+++ src/mode_count_residue_types.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "auxiliaries/program_options_handler.h"
 #include "auxiliaries/io_utilities.h"
@@ -16,12 +17,23 @@ inline bool read_and_accumulate_to_map_of_counts(std::istream& input, std::map<T
 	T crad;
 	long count;
 	input >> crad >> count;
-	if(!input.fail())
+	if(!input.fail() && count>0)
 	{
 		map_of_counts[crad]+=count;
 		return true;
 	}
 	return false;
+}
+
+template<typename T>
+long sum_counts(const std::map<T, long>& map_of_counts)
+{
+	long sum=0;
+	for(typename std::map<T, long>::const_iterator it=map_of_counts.begin();it!=map_of_counts.end();++it)
+	{
+		sum+=it->second;
+	}
+	return sum;
 }
 
 }
@@ -37,8 +49,7 @@ void count_residue_types(const auxiliaries::ProgramOptionsHandler& poh)
 		{
 			poh.print_io_description("stdin", true, false,
 					"list of ids, or list of types with counts, or sequence\n(default mode line format: 'id')\n(pre-counted mode line format: 'type count')\n(sequence mode format: plain or FASTA)");
-			poh.print_io_description("stdout", false, true,
-					"list of types with counts (line format: 'type count')");
+			poh.print_io_description("stdout", false, true, "list of types with counts (line format: 'type count')");
 			return;
 		}
 	}
@@ -76,6 +87,48 @@ void count_residue_types(const auxiliaries::ProgramOptionsHandler& poh)
 			crad.resName=it->resName;
 			result[crad]++;
 		}
+	}
+
+	auxiliaries::IOUtilities().write_map(result, std::cout);
+}
+
+void count_residue_types_coefficients(const auxiliaries::ProgramOptionsHandler& poh)
+{
+	{
+		typedef auxiliaries::ProgramOptionsHandler::OptionDescription OD;
+		std::vector<OD> ods;
+		ods.push_back(OD("--first-counts-file", "string", "file to input first set of counts of residue types", true));
+		ods.push_back(OD("--second-counts-file", "string", "file to input second set of counts of residue types", true));
+		if(!poh.assert(ods, false))
+		{
+			poh.print_io_description("stdout", false, true, "list of types with coefficients (line format: 'type coefficient')");
+			return;
+		}
+	}
+
+	const std::string first_counts_file=poh.argument<std::string>("--first-counts-file");
+	const std::string second_counts_file=poh.argument<std::string>("--second-counts-file");
+
+	std::map<CRAD, long> first_counts;
+	std::map<CRAD, long> second_counts;
+
+	auxiliaries::IOUtilities().read_file_lines_to_container(first_counts_file, read_and_accumulate_to_map_of_counts<CRAD>, first_counts);
+	auxiliaries::IOUtilities().read_file_lines_to_container(second_counts_file, read_and_accumulate_to_map_of_counts<CRAD>, second_counts);
+
+	const long first_sum=sum_counts(first_counts);
+	const long second_sum=sum_counts(second_counts);
+
+	if(first_sum<=0 || second_sum<=0)
+	{
+		throw std::runtime_error("Invalid input.");
+	}
+
+	std::map<CRAD, double> result;
+	for(std::map<CRAD, long>::const_iterator it=first_counts.begin();it!=first_counts.end();++it)
+	{
+		double first_r=static_cast<double>(it->second)/static_cast<double>(first_sum);
+		double second_r=static_cast<double>(second_counts[it->first])/static_cast<double>(second_sum);
+		result[it->first]=(second_r/first_r);
 	}
 
 	auxiliaries::IOUtilities().write_map(result, std::cout);
