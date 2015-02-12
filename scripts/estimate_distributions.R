2b745d063c22de335f35d1869b9a5d6f2b79ad2a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-02-12 10:07:27 +0200

    Added hist calc script.

---
+++ scripts/estimate_distributions.R
@@ -0,0 +1,22 @@
+args=commandArgs(TRUE);
+
+t=read.table(args[1], header=FALSE, stringsAsFactors=FALSE);
+names=sort(union(t$V1, t$V1));
+
+pdf(args[2]);
+
+histbreaks=seq(min(t$V2)-0.05, max(t$V2)+0.05, 0.01);
+hist(t$V2, breaks=histbreaks, col=rgb(1, 0, 0, 0.5), freq=FALSE, main="All atoms");
+
+means=c();
+sds=c();
+for(name in names)
+{
+	st=t[which(t$V1==name),];
+	means=c(means, mean(st$V2));
+	sds=c(sds, sd(st$V2));
+	hist(st$V2, breaks=histbreaks, col=rgb(0, 0, 1, 0.5), freq=FALSE, main=name);
+}
+
+mt=data.frame(names=names, means=means, sds=sds);
+write.table(mt, args[3], col.names=FALSE, row.names=FALSE, quote=FALSE);
