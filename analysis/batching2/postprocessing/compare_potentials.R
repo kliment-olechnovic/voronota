3ebee7a54f68297ad1e7f55accdb1d78e78024fc
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-02-14 11:33:54 +0200

    Improved batching results postprocessing script.

--- analysis/batching2/postprocessing/compare_potentials.R
+++ analysis/batching2/postprocessing/compare_potentials.R
@@ -6,9 +6,13 @@ t=merge(t1, t2, by=c("V1", "V2", "V3"));
 tp=merge(t1[which(t1$V3=="."),], t2[which(t2$V3=="."),], by=c("V1", "V2", "V3"));
 tw=merge(t1[which(t1$V2=="c<solvent>"),], t2[which(t2$V2=="c<solvent>"),], by=c("V1", "V2", "V3"));
 
+png(args[3],  height=10, width=10, units="in", res=300);
+
 plot(t$V4.x, t$V4.y);
 points(tp$V4.x, tp$V4.y, col="blue");
 points(tw$V4.x, tw$V4.y, col="red");
 points(c(-100, 100), c(-100, 100), type="l", col="yellow");
 points(c(-100, 100), c(0, 0), type="l", col="yellow");
 points(c(0, 0), c(-100, 100), type="l", col="yellow");
+
+dev.off();
