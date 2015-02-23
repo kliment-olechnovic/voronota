09431d995fdd8ea9418a2c5b0c0a5a85ee4ee090
kliment <kliment@localhost>
2015-02-23 15:01:10 +0200

    Added better contacts calc script.

---
+++ analysis/scripts/calc_contacts.bash
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+set +e
+
+TMPDIR=$(mktemp -d)
+trap "rm -r $TMPDIR" EXIT
+
+BINDIR=""
+INPUT_FILE_PATH=""
+INPUT_URL=false
+INPUT_PRINT_COMMAND="cat"
+OUTPUTDIR=""
+RUN_HBPLUS=false
+CREATE_SUMMARIES=false
+
+while getopts "b:i:uzo:ps" OPTION
+do
+	case $OPTION in
+	h)
+		echo "No help available, read sript file." 1>&2
+		exit 0
+		;;
+	b)
+		BINDIR=$OPTARG
+		;;
+	i)
+		INPUT_FILE_PATH=$OPTARG
+		;;
+	u)
+		INPUT_URL=true
+		;;
+	z)
+		INPUT_PRINT_COMMAND="zcat"
+		;;
+    o)
+		OUTPUTDIR=$OPTARG
+		;;
+	p)
+		RUN_HBPLUS=true
+		;;
+	s)
+		CREATE_SUMMARIES=true
+		;;
+	?)
+		echo "Unrecognized option." 1>&2
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "$BINDIR" ]
+then
+	echo "Missing binaries directory." 1>&2
+	exit 1
+fi
+
+if [ -z "$INPUT_FILE_PATH" ]
+then
+	echo "Missing input file path." 1>&2
+	exit 1
+fi
+
+if [ -z "$OUTPUTDIR" ]
+then
+	echo "Missing output directory." 1>&2
+	exit 1
+fi
+
+if [ ! -s "$BINDIR/voronota" ]
+then
+	echo "Invalid binaries directory." 1>&2
+	exit 1
+fi
+
+if $INPUT_URL
+then
+	wget -O $TMPDIR/download "$INPUT_FILE_PATH"
+	$INPUT_PRINT_COMMAND $TMPDIR/download > $TMPDIR/input.pdb
+else
+	$INPUT_PRINT_COMMAND $INPUT_FILE_PATH > $TMPDIR/input.pdb
+fi
+
+if [ ! -s "$TMPDIR/input.pdb" ]
+then
+	echo "Failed to extract input file." 1>&2
+	exit 1
+fi
+
+mkdir -p "$OUTPUTDIR"
+
+if [ ! -d "$OUTPUTDIR" ]
+then
+	echo "Could not find or create output directory." 1>&2
+fi
+
+cat $TMPDIR/input.pdb \
+| $BINDIR/voronota get-balls-from-atoms-file --radii-file $BINDIR/radii --annotated \
+| $BINDIR/voronota query-balls --drop-altloc-indicators --drop-atom-serials --drop-adjuncts --drop-tags \
+| sed 's/A<OXT>/A<O>/g' \
+| grep -f $BINDIR/standard_atom_names \
+> $OUTPUTDIR/balls
+
+if [ ! -s "$OUTPUTDIR/balls" ]
+then
+	echo "Input set of atoms is empty." 1>&2
+	exit 1
+fi
+
+cat $OUTPUTDIR/balls \
+| $BINDIR/voronota calculate-contacts --annotated \
+> $OUTPUTDIR/contacts
+
+if $RUN_HBPLUS
+then
+	cd $TMPDIR
+	$BINDIR/hbplus ./input.pdb > /dev/null 2> /dev/null
+	cd -
+
+	cat $OUTPUTDIR/contacts \
+	| $BINDIR/voronota query-contacts --set-hbplus-tags $TMPDIR/input.hb2 --inter-residue-hbplus-tags \
+	> $OUTPUTDIR/contacts_whb
+
+	mv $OUTPUTDIR/contacts_whb $OUTPUTDIR/contacts
+fi
+
+mkdir -p $OUTPUTDIR/contacts_summaries
+
+if $CREATE_SUMMARIES
+then
+	for SEQSEP in {1..10}
+	do
+		cat $OUTPUTDIR/contacts \
+		| $BINDIR/voronota query-contacts --no-solvent --match-min-seq-sep $SEQSEP --match-max-seq-sep $SEQSEP \
+		| awk '{print $1 " " $2 " " $5 " " $3}' \
+		| $BINDIR/voronota score-contacts-potential \
+		> "$OUTPUTDIR/contacts_summaries/seqsep_$SEQSEP"
+	done
+	
+	cat $OUTPUTDIR/contacts \
+	| $BINDIR/voronota query-contacts --no-solvent --match-min-seq-sep 11 \
+	| awk '{print $1 " " $2 " " $5 " " $3}' \
+	| $BINDIR/voronota score-contacts-potential \
+	> "$OUTPUTDIR/contacts_summaries/seqsep_11"
+	
+	cat $OUTPUTDIR/contacts \
+	| $BINDIR/voronota query-contacts --match-second "c<solvent>" \
+	| awk '{print $1 " " $2 " " $5 " " $3}' \
+	| $BINDIR/voronota score-contacts-potential \
+	> "$OUTPUTDIR/contacts_summaries/solvent"
+fi
