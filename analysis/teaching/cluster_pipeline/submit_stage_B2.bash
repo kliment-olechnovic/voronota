633b9ae627fc9b2e4364703765cf2591a9c0e3af
kliment <kliment@localhost>
2015-03-10 11:15:41 +0200

    Enhanced CASP pipeline.

---
+++ analysis/teaching/cluster_pipeline/submit_stage_B2.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find /scratch/lustre/kliment/voromqa/output/casp/contacts/ -type f -name contacts -not -empty | while read FNAME
+do
+	echo $(dirname $FNAME)
+done > /scratch/lustre/kliment/voromqa/output/casp/contacts_list
+
+cat ./output/casp/contacts_list \
+| xargs -L 200 -P 1 \
+sbatch /scratch/lustre/kliment/voromqa/bin/run_args_loop.bash \
+"/scratch/lustre/kliment/voromqa/bin/calc_contacts_tags.bash -b /scratch/lustre/kliment/voromqa/bin -p -s -d"
---
+++ analysis/teaching/cluster_pipeline/submit_stage_B4.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for i in {1..6}
+do
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-e energy_without_tags_iss${i}_global \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_without_tags_iss${i}_global
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-e energy_with_tags_iss${i}_global \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_with_tags_iss${i}_global
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-e energy_without_tags_iss${i}_atoms \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_without_tags_iss${i}_atoms
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-e energy_with_tags_iss${i}_atoms \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_with_tags_iss${i}_atoms
+done
