cef2a5f62fe657a481529e1132813e8fc68ab6ac
kliment <kliment@localhost>
2015-03-10 12:04:08 +0200

    Enhanced CASP pipeline.

--- analysis/teaching/cluster_pipeline/submit_stage_B4.bash
+++ analysis/teaching/cluster_pipeline/submit_stage_B4.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cat /scratch/lustre/kliment/voromqa/output/casp/contacts_list | grep models > /scratch/lustre/kliment/voromqa/output/casp/contacts_list_models
+cat /scratch/lustre/kliment/voromqa/output/casp/contacts_list | grep target > /scratch/lustre/kliment/voromqa/output/casp/contacts_list_targets
+
 for i in {1..6}
 do
 	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
@@ -13,12 +16,22 @@ do
 	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_with_tags_iss${i}_global
 	
 	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
-	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list_models \
 	-e energy_without_tags_iss${i}_atoms \
-	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_without_tags_iss${i}_atoms
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/models/energy_without_tags_iss${i}_atoms
 	
 	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
-	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list_models \
 	-e energy_with_tags_iss${i}_atoms \
-	-o /scratch/lustre/kliment/voromqa/output/casp/energy/energy_with_tags_iss${i}_atoms
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/models/energy_with_tags_iss${i}_atoms
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list_targets \
+	-e energy_without_tags_iss${i}_atoms \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/targets/energy_without_tags_iss${i}_atoms
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/casp/contacts_list_targets \
+	-e energy_with_tags_iss${i}_atoms \
+	-o /scratch/lustre/kliment/voromqa/output/casp/energy/targets/energy_with_tags_iss${i}_atoms
 done
