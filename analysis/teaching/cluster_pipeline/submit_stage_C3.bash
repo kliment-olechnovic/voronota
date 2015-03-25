3e2d3a8e8bc3bab873666aa7fb69ab8f98ba49f7
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-03-25 09:14:17 +0200

    Updated decoys pipeline scripts.

--- analysis/teaching/cluster_pipeline/submit_stage_C3.bash
+++ analysis/teaching/cluster_pipeline/submit_stage_C3.bash
@@ -10,5 +10,5 @@ do
 	cat /scratch/lustre/kliment/voromqa/output/decoys/contacts_list \
 	| xargs -L 200 -P 1 \
 	sbatch /scratch/lustre/kliment/voromqa/bin/run_args_loop.bash \
-	"/scratch/lustre/kliment/voromqa/bin/calc_contacts_energy.bash -b /scratch/lustre/kliment/voromqa/bin -s -e 2 -y $i -p /scratch/lustre/kliment/voromqa/output/complexes/potential/full/potential_with_tags -t -n energy_with_tags_iss$i -d"
+	"/scratch/lustre/kliment/voromqa/bin/calc_contacts_energy.bash -b /scratch/lustre/kliment/voromqa/bin -s -e 2 -y $i -p /scratch/lustre/kliment/voromqa/output/complexes/potential/full/potential_with_tags -t -n energy_with_tags_iss$i -q /scratch/lustre/kliment/voromqa/output/complexes/energy/energy_with_tags_iss${i}_atoms_means_and_sds -d"
 done
--- analysis/teaching/cluster_pipeline/submit_stage_C4.bash
+++ analysis/teaching/cluster_pipeline/submit_stage_C4.bash
@@ -6,4 +6,9 @@ do
 	-l /scratch/lustre/kliment/voromqa/output/decoys/contacts_list \
 	-e energy_with_tags_iss${i}_global \
 	-o /scratch/lustre/kliment/voromqa/output/decoys/energy/energy_with_tags_iss${i}_global
+	
+	sbatch /scratch/lustre/kliment/voromqa/bin/cat_list_of_files.bash \
+	-l /scratch/lustre/kliment/voromqa/output/decoys/contacts_list \
+	-e energy_with_tags_iss${i}_global_quality \
+	-o /scratch/lustre/kliment/voromqa/output/decoys/energy/energy_with_tags_iss${i}_global_quality
 done
