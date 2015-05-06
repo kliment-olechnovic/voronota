4ccfae61eee61929559888ab9ac601506b637a64
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-06 10:43:02 +0300

    Enhanced global qualities analysis script.

--- analysis/teaching/analysis_scripts/analyze_global_qualities.R
+++ analysis/teaching/analysis_scripts/analyze_global_qualities.R
@@ -7,6 +7,11 @@ v=values$V1
 
 targets=sort(union(names$V2, names$V2));
 
+nonempty_targets=c();
+max_scores=c();
+target_scores=c();
+mean_scores=c();
+sd_of_scores=c();
 sizes=c();
 jumpers=c();
 jumpernames=c();
@@ -23,7 +28,14 @@ for(target in targets)
 		stargetsel=which(sn=="target");
 		if(length(stargetsel)>0)
 		{
+			nonempty_targets=c(nonempty_targets, target);
+			
 			jumpersel=which(sv>sv[stargetsel]);
+			
+			max_scores=c(max_scores, max(sv));
+			target_scores=c(target_scores, sv[stargetsel]);
+			mean_scores=c(mean_scores, mean(sv));
+			sd_of_scores=c(sd_of_scores, sd(sv));
 			
 			sizes=c(sizes, length(sv));
 			jumpers=c(jumpers, length(jumpersel));
@@ -54,3 +66,6 @@ jumpertargetnames;
 length(which(jumpers>0));
 mean(jumpers/sizes);
 sum(jumpers)/sum(sizes);
+
+result_table=data.frame(target=nonempty_targets, N=(sizes+1), rank=(jumpers+1), Z_score=format(((target_scores-mean_scores)/sd_of_scores), digits=2));
+write.table(result_table, "./target_selection_results", row.names=FALSE, quote=FALSE);
