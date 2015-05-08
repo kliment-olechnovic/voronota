87a0fe0b9eec63376148a0c92d41ab4038995bbf
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-08 14:01:58 +0300

    Added tests covering more options for basic modes.

--- tests/scripts/do_basic_triangulation_and_contacts.bash
+++ tests/scripts/do_basic_triangulation_and_contacts.bash
@@ -4,30 +4,127 @@ set +e
 
 DIR=$OUTPUTDIR/basic_triangulation_and_contacts
 
+##############################################################
+
 SUBDIR=$DIR/default_radii
 mkdir -p $SUBDIR
+
 cat $INPUTDIR/single/structure.pdb | $VORONOTA get-balls-from-atoms-file > $SUBDIR/balls
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices --print-log --check > $SUBDIR/triangulation 2> $SUBDIR/triangulation_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices-in-parallel --print-log --method simulated --parts 8 > $SUBDIR/triangulation_simulated_parallel 2> $SUBDIR/triangulation_simulated_parallel_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-contacts > $SUBDIR/contacts
+
+##############################################################
 
 SUBDIR=$DIR/custom_radii
 mkdir -p $SUBDIR
+
 cat $INPUTDIR/single/structure.pdb | $VORONOTA get-balls-from-atoms-file --radii-file $VORONOTA_RADII_FILE > $SUBDIR/balls
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices --print-log --check > $SUBDIR/triangulation 2> $SUBDIR/triangulation_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices-in-parallel --print-log --method simulated --parts 8 > $SUBDIR/triangulation_simulated_parallel 2> $SUBDIR/triangulation_simulated_parallel_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-contacts > $SUBDIR/contacts
+
+##############################################################
 
 SUBDIR=$DIR/custom_radii_with_heteroatoms
 mkdir -p $SUBDIR
+
 cat $INPUTDIR/single/structure.pdb | $VORONOTA get-balls-from-atoms-file --radii-file $VORONOTA_RADII_FILE --include-heteroatoms > $SUBDIR/balls
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices --print-log --check > $SUBDIR/triangulation 2> $SUBDIR/triangulation_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-vertices-in-parallel --print-log --method simulated --parts 8 > $SUBDIR/triangulation_simulated_parallel 2> $SUBDIR/triangulation_simulated_parallel_log
+
 cat $SUBDIR/balls | $VORONOTA calculate-contacts > $SUBDIR/contacts
 
-SUBDIR=$DIR/mmcif_custom_radii_with_heteroatoms
+##############################################################
+
+SUBDIR=$DIR/mmcif_custom_radii_with_heteroatoms_with_calculation_enhancements
 mkdir -p $SUBDIR
-cat $INPUTDIR/single/structure.cif | $VORONOTA get-balls-from-atoms-file --mmcif --radii-file $VORONOTA_RADII_FILE --include-heteroatoms > $SUBDIR/balls
-cat $SUBDIR/balls | $VORONOTA calculate-vertices --print-log --check > $SUBDIR/triangulation 2> $SUBDIR/triangulation_log
-cat $SUBDIR/balls | $VORONOTA calculate-vertices-in-parallel --print-log --method simulated --parts 8 > $SUBDIR/triangulation_simulated_parallel 2> $SUBDIR/triangulation_simulated_parallel_log
-cat $SUBDIR/balls | $VORONOTA calculate-contacts > $SUBDIR/contacts
+
+cat $INPUTDIR/single/structure.cif \
+| $VORONOTA get-balls-from-atoms-file \
+  --mmcif \
+  --radii-file $VORONOTA_RADII_FILE \
+  --include-heteroatoms \
+  --hull-offset 3.0 \
+> $SUBDIR/balls
+
+cat $SUBDIR/balls \
+| $VORONOTA calculate-vertices \
+  --exclude-hidden-balls \
+  --include-surplus-quadruples \
+  --link \
+  --print-log \
+  --check \
+> $SUBDIR/triangulation \
+2> $SUBDIR/triangulation_log
+
+cat $SUBDIR/balls \
+| $VORONOTA calculate-vertices-in-parallel \
+  --include-surplus-quadruples \
+  --link \
+  --print-log \
+  --method simulated \
+  --parts 8 \
+> $SUBDIR/triangulation_simulated_parallel \
+2> $SUBDIR/triangulation_simulated_parallel_log
+
+cat $SUBDIR/balls \
+| $VORONOTA calculate-contacts \
+  --exclude-hidden-balls \
+> $SUBDIR/contacts
+
+##############################################################
+
+SUBDIR=$DIR/with_secondary_options
+mkdir -p $SUBDIR
+
+cat $INPUTDIR/single/structure.pdb \
+| $VORONOTA get-balls-from-atoms-file \
+  --radii-file $VORONOTA_RADII_FILE \
+  --include-heteroatoms \
+  --include-hydrogens \
+  --multimodel-chains \
+  --default-radius 1.70 \
+  --only-default-radius \
+  --hull-offset 3.0 \
+> $SUBDIR/balls
+
+cat $SUBDIR/balls \
+| $VORONOTA calculate-vertices \
+  --exclude-hidden-balls \
+  --include-surplus-quadruples \
+  --link \
+  --init-radius-for-BSH 3.5 \
+  --print-log \
+  --check \
+ > $SUBDIR/triangulation \
+ 2> $SUBDIR/triangulation_log
+ 
+cat $SUBDIR/balls \
+| $VORONOTA calculate-vertices-in-parallel \
+  --include-surplus-quadruples \
+  --init-radius-for-BSH 3.5 \
+  --link \
+  --print-log \
+  --method simulated \
+  --parts 8 \
+> $SUBDIR/triangulation_simulated_parallel \
+2> $SUBDIR/triangulation_simulated_parallel_log
+
+cat $SUBDIR/balls \
+| $VORONOTA calculate-contacts \
+  --exclude-hidden-balls \
+  --add-mirrored \
+  --probe 1.4 \
+  --step 0.2 \
+  --projections 5 \
+  --sih-depth 3 \
+> $SUBDIR/contacts
