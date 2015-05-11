5bdd27faa16d7f1dc41a5eb707b764402f32ac15
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-11 11:11:12 +0300

    Added simplified parallel processing tests.

---
+++ tests/jobs_scripts/do_parallel_vertices_calculations_using_mpi.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set +e
+
+TMPDIR=$(mktemp -d)
+trap "rm -r $TMPDIR" EXIT
+
+SUBDIR=$OUTPUTDIR/parallel_vertices_calculations_using_mpi
+mkdir -p $SUBDIR
+
+MPI_BIN_DIR="/usr/lib64/openmpi/bin/"
+
+$MPI_BIN_DIR/mpic++ -O3 -DENABLE_MPI -o $TMPDIR/voronota_mpi $VORONOTADIR/src/*.cpp
+
+cat $INPUTDIR/single/structure.pdb \
+| $VORONOTA get-balls-from-atoms-file --radii-file $VORONOTADIR/radii --include-heteroatoms \
+> $TMPDIR/balls
+
+$MPI_BIN_DIR/mpirun -n 5 $TMPDIR/voronota_mpi calculate-vertices-in-parallel --method mpi --parts 4 --print-log < $TMPDIR/balls > /dev/null 2> $SUBDIR/mpi_4p_log
+$MPI_BIN_DIR/mpirun -n 5 $TMPDIR/voronota_mpi calculate-vertices-in-parallel --method mpi --parts 8 --print-log < $TMPDIR/balls > /dev/null 2> $SUBDIR/mpi_8p_log
+$MPI_BIN_DIR/mpirun -n 5 $TMPDIR/voronota_mpi calculate-vertices-in-parallel --method mpi --parts 16 --print-log < $TMPDIR/balls > /dev/null 2> $SUBDIR/mpi_16p_log
+$MPI_BIN_DIR/mpirun -n 5 $TMPDIR/voronota_mpi calculate-vertices-in-parallel --method mpi --parts 32 --print-log < $TMPDIR/balls > /dev/null 2> $SUBDIR/mpi_32p_log
+
+find $SUBDIR/ -type f -name "mpi_*p_log" | while read MPI_LOG_FILE
+do
+	cat $MPI_LOG_FILE | grep -v seconds | sponge $MPI_LOG_FILE
+done
---
+++ tests/jobs_scripts/do_parallel_vertices_calculations_using_openmp.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set +e
+
+TMPDIR=$(mktemp -d)
+trap "rm -r $TMPDIR" EXIT
+
+SUBDIR=$OUTPUTDIR/parallel_vertices_calculations_using_openmp
+mkdir -p $SUBDIR
+
+g++ -O3 -fopenmp -o $TMPDIR/voronota_openmp $VORONOTADIR/src/*.cpp
+
+cat $INPUTDIR/single/structure.pdb \
+| $TMPDIR/voronota_openmp get-balls-from-atoms-file --radii-file $VORONOTADIR/radii --include-heteroatoms \
+> $TMPDIR/balls
+
+cat $TMPDIR/balls | $TMPDIR/voronota_openmp calculate-vertices-in-parallel --method openmp --parts 4 --print-log > /dev/null 2> $SUBDIR/openmp_4p_log
+cat $TMPDIR/balls | $TMPDIR/voronota_openmp calculate-vertices-in-parallel --method openmp --parts 8 --print-log > /dev/null 2> $SUBDIR/openmp_8p_log
+cat $TMPDIR/balls | $TMPDIR/voronota_openmp calculate-vertices-in-parallel --method openmp --parts 16 --print-log > /dev/null 2> $SUBDIR/openmp_16p_log
+cat $TMPDIR/balls | $TMPDIR/voronota_openmp calculate-vertices-in-parallel --method openmp --parts 32 --print-log > /dev/null 2> $SUBDIR/openmp_32p_log
