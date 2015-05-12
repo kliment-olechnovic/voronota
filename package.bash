1284703b301c82d9e485953f360451448a030027
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-12 15:40:57 +0300

    Improved contacts scoring testing by incorporating packaged potential data.

--- package.bash
+++ package.bash
@@ -22,6 +22,8 @@ cp README.markdown $PACKAGE_NAME/README.txt
 cp LICENSE.txt $PACKAGE_NAME/LICENSE.txt
 cp CMakeLists.txt $PACKAGE_NAME/CMakeLists.txt
 cp resources/radii $PACKAGE_NAME/radii
+cp resources/energy_potential $PACKAGE_NAME/energy_potential
+cp resources/energy_means_and_sds $PACKAGE_NAME/energy_means_and_sds
 cp Release/voronota $PACKAGE_NAME/voronota
 pandoc README.markdown -f markdown -t html -s -o $PACKAGE_NAME/README.html
 
