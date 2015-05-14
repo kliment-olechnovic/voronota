9b6588dd3844bec0e2c23cc36cda5b7fcfbc9b28
kliment <kliment@localhost>
2015-05-14 11:51:02 +0300

    Added valgrind memcheck memory test.

---
+++ tests/jobs_scripts/do_memory_check.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set +e
+
+TMPDIR=$(mktemp -d)
+trap "rm -r $TMPDIR" EXIT
+
+SUBDIR=$OUTPUTDIR/memory_check
+mkdir -p $SUBDIR
+
+g++ -g -O1 -o $TMPDIR/voronota_raw $VORONOTADIR/src/*.cpp
+
+cat $INPUTDIR/single/structure.pdb \
+| $VORONOTA get-balls-from-atoms-file --radii-file $VORONOTADIR/radii --include-heteroatoms \
+> $TMPDIR/balls
+
+valgrind --tool=memcheck $TMPDIR/voronota_raw calculate-vertices < $TMPDIR/balls > $SUBDIR/triangulation 2> $TMPDIR/log
+
+(cat $TMPDIR/log | grep -A 3 "LEAK SUMMARY" ; cat $TMPDIR/log | grep "ERROR SUMMARY") \
+| sed 's/^==\S\+== //' \
+> $SUBDIR/valgrind_memcheck_log
