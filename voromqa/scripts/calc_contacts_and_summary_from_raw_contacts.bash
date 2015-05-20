32c14fa386a4fdf1c0c6571907f3c1b492474abf
kliment <kliment@localhost>
2015-05-20 14:22:32 +0300

    Improved voromqa teaching pipeline scripts.

--- voromqa/scripts/calc_contacts_and_summary_from_raw_contacts.bash
+++ voromqa/scripts/calc_contacts_and_summary_from_raw_contacts.bash
@@ -3,15 +3,28 @@
 set +e
 
 WORKDIR=""
+MULTAREAS=false
 
-while getopts "d:" OPTION
+while getopts "d:m" OPTION
 do
 	case $OPTION in
 	d)
 		WORKDIR=$OPTARG
 		;;
+	m)
+		MULTAREAS=true
+		;;
 	esac
 done
+
+MULTAREAS_OPTION=""
+if $MULTAREAS
+then
+	cat $WORKDIR/balls \
+	| $BINDIR/voronota query-balls --chains-summary-output $WORKDIR/chains_counts \
+	> /dev/null
+	MULTAREAS_OPTION="--multiply-areas $(cat $WORKDIR/chains_counts | head -1 | awk '{print (24.0/$1*$2)}')"
+fi
 
 cat $WORKDIR/raw_contacts \
 | $BINDIR/voronota query-contacts --match-min-seq-sep 1 \
@@ -32,8 +45,7 @@ cat $WORKDIR/raw_contacts \
   --set-tags 'sep2' \
 | awk '{print $1 " " $2 " " $5 " " $3}' \
 | tee $WORKDIR/contacts \
-| $BINDIR/voronota score-contacts-potential \
-  --multiply-areas $(cat $WORKDIR/chains_counts | head -1 | awk '{print (24.0/$1*$2)}') \
+| $BINDIR/voronota score-contacts-potential $MULTAREAS_OPTION \
   --contributions-file $WORKDIR/contributions \
   --single-areas-file $WORKDIR/single_areas \
 > $WORKDIR/summary
