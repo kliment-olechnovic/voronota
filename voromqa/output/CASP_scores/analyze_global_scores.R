ac135090f8e1766f195bea33efe6248b0611099e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-21 13:28:45 +0300

    Improved CASP global scores output analysis script.

--- voromqa/output/CASP_scores/analyze_global_scores.R
+++ voromqa/output/CASP_scores/analyze_global_scores.R
@@ -13,18 +13,36 @@ for(target in targets)
 	{
 		model_best_qscore=max(st$qscore_atom[sel_models]);
 		sel_model_with_best_qscore=sel_models[which(st$qscore_atom[sel_models]==model_best_qscore)][1];
+		
+		model_best_cadscore=max(st$cadscore_residue[sel_models]);
+		sel_model_with_best_cadscore=sel_models[which(st$cadscore_residue[sel_models]==model_best_cadscore)][1];
+		
+		cor_qscore_vs_cadscore=cor(st$qscore_atom[sel_models], st$cadscore_residue[sel_models]);
+		
 		sr=data.frame(
 				target=target,
 				target_qscore=st$qscore_atom[sel_target],
 				model_with_best_qscore=st$model[sel_model_with_best_qscore],
-				model_qscore=model_best_qscore);
+				model_best_qscore=model_best_qscore,
+				model_with_best_cadscore=st$model[sel_model_with_best_cadscore],
+				model_best_cadscore=model_best_cadscore,
+				cor_qscore_vs_cadscore=cor_qscore_vs_cadscore,
+				model_cadscore_of_best_qscore=st$cadscore_residue[sel_model_with_best_qscore]);
 		if(length(r)>0) { r=rbind(r, sr); } else { r=sr; }
 	}
 }
 
 length(r$target);
-length(which(r$target_qscore<=r$model_qscore));
+length(which(r$target_qscore<=r$model_best_qscore));
 
-r[which(r$target_qscore<=r$model_qscore),];
+quantile(r$cor_qscore_vs_cadscore);
+mean(r$cor_qscore_vs_cadscore);
+cor(t$cadscore_residue, t$qscore_atom);
+
+r[which(r$target_qscore<=r$model_best_qscore),];
 
 plot(c(0, 1), c(0, 1), type="l", xlab="Target quality score", ylab="Highest model quality score", main=""); points(r$target_qscore, r$model_qscore);
+
+plot(c(0, 1), c(0, 1), type="l", xlab="Best CAD-score", ylab="CAD-score corresponding to the best quality score", main=""); points(r$model_best_cadscore, r$model_cadscore_of_best_qscore);
+
+plot(t$cadscore_residue, t$qscore_atom, xlab="CAD-score", ylab="Quality score", main="");
