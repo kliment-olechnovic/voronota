07ed00cdaf4e995d93ff3072874a7da5f1d773b7
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-22 12:32:52 +0300

    Added atoms pdb file generation for CASP scoring pipeline.

--- voromqa/pipelines/perform_CASP_scoring_distributed.bash
+++ voromqa/pipelines/perform_CASP_scoring_distributed.bash
@@ -81,12 +81,24 @@ then
 	exit 0
 fi
 
-if [[ $STEPNAMES == *"[goap_scores]"* ]]
+if [[ $STEPNAMES == *"[atoms]"* ]]
 then
 	find $OUTPUTDIR/entries/ -type f -name balls -not -empty | sed 's/balls$//' > $OUTPUTDIR/list_of_entries_with_balls
 	INCOUNT=$(cat $OUTPUTDIR/list_of_entries_with_balls | wc -l)
 	
 	cat $OUTPUTDIR/list_of_entries_with_balls \
+	| xargs -L $(echo "$INCOUNT/$CPUCOUNT" | bc) \
+	$SCHEDULER $BINDIR/run_calc_script.bash $BINDIR "$BINDIR/collect_atoms_from_balls.bash -d"
+	
+	exit 0
+fi
+
+if [[ $STEPNAMES == *"[goap_scores]"* ]]
+then
+	find $OUTPUTDIR/entries/ -type f -name atoms.pdb -not -empty | sed 's/atoms.pdb$//' > $OUTPUTDIR/list_of_entries_with_atoms
+	INCOUNT=$(cat $OUTPUTDIR/list_of_entries_with_atoms | wc -l)
+	
+	cat $OUTPUTDIR/list_of_entries_with_atoms \
 	| xargs -L $(echo "$INCOUNT/$CPUCOUNT" | bc) \
 	$SCHEDULER $BINDIR/run_calc_script.bash $BINDIR "$BINDIR/calc_goap_scores_from_atoms.bash -d"
 	
