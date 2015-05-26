80a24701c5d2d00945d0548809c20a68476b4d16
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-26 18:52:37 +0300

    Improved partial potentials statistics plotting script.

--- voromqa/output/postprocessing/plot_comparison_of_potential_and_partial_potentials_stats.bash
+++ voromqa/output/postprocessing/plot_comparison_of_potential_and_partial_potentials_stats.bash
@@ -6,7 +6,7 @@ OUTDIR=$3
 
 mkdir -p $OUTDIR
 
-R --vanilla --args $POTENTIAL_FILE $STATS_FILE $OUTDIR/comparison_of_potential_and_partial_potentials_stats.png << 'EOF'
+R --vanilla --args $POTENTIAL_FILE $STATS_FILE $OUTDIR/plot_for_tag_ << 'EOF'
 
 args=commandArgs(TRUE);
 
@@ -14,21 +14,27 @@ a=read.table(args[1], header=FALSE, stringsAsFactors=FALSE);
 b=read.table(args[2], header=FALSE, stringsAsFactors=FALSE);
 
 t=merge(a, b, by=c("V1", "V2", "V3"));
-t=t[which(t$V3!="sep1"),];
 
-x=t$V4.x;
-y=t$V4.y;
-sds=t$V5;
+tags=sort(union(t$V3, t$V3));
 
-png(args[3],  height=10, width=10, units="in", res=300);
-
-plot(x, y, type="n");
-segments(x, y-sds, x, y+sds, col="red");
-points(x, y, col="black");
-points(c(-100, 100), c(-100, 100), type="l", col="yellow");
-points(c(-100, 100), c(0, 0), type="l", col="yellow");
-points(c(0, 0), c(-100, 100), type="l", col="yellow");
-
-dev.off();
+for(tag in tags)
+{
+	st=t[which(t$V3==tag),];
+	
+	x=st$V4.x;
+	y=st$V4.y;
+	sds=st$V5;
+	
+	png(paste(args[3], tag, ".png", sep=""),  height=10, width=10, units="in", res=300);
+	
+	plot(x, y, type="n", main=tag);
+	segments(x, y-sds, x, y+sds, col="red");
+	points(x, y, col="black");
+	points(c(-100, 100), c(-100, 100), type="l", col="yellow");
+	points(c(-100, 100), c(0, 0), type="l", col="yellow");
+	points(c(0, 0), c(-100, 100), type="l", col="yellow");
+	
+	dev.off();
+}
 
 EOF
