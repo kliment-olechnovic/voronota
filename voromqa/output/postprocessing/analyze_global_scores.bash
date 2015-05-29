633e6f7e2887ad761b8e2b537b6eae9fbfbcb2a8
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-05-29 14:08:44 +0300

    Enhanced global scores analysis.

--- voromqa/output/postprocessing/analyze_global_scores.bash
+++ voromqa/output/postprocessing/analyze_global_scores.bash
@@ -4,6 +4,7 @@ SCRIPTDIR=$(dirname $0)
 INFILE=$1
 OUTDIR=$2
 
+
 SUBDIR=$OUTDIR/qscore_vs_cadscore
 mkdir -p $SUBDIR
 R --vanilla --args \
@@ -27,6 +28,7 @@ R --vanilla --args \
 < $SCRIPTDIR/analyze_global_scores.R \
 | grep 'Results statistics output' -A 9999 \
 > $SUBDIR/log
+
 
 SUBDIR=$OUTDIR/goap_vs_cadscore
 mkdir -p $SUBDIR
@@ -52,6 +54,19 @@ R --vanilla --args \
 | grep 'Results statistics output' -A 9999 \
 > $SUBDIR/log
 
+SUBDIR=$OUTDIR/goap_vs_qscore
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name goap F-invert-testscore F-normalize-testscore \
+  V-refscore-name qscore_atom \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+
 SUBDIR=$OUTDIR/dfire_vs_cadscore
 mkdir -p $SUBDIR
 R --vanilla --args \
@@ -75,6 +90,31 @@ R --vanilla --args \
 < $SCRIPTDIR/analyze_global_scores.R \
 | grep 'Results statistics output' -A 9999 \
 > $SUBDIR/log
+
+SUBDIR=$OUTDIR/dfire_vs_qscore
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name dfire F-invert-testscore F-normalize-testscore \
+  V-refscore-name qscore_atom \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/dfire_vs_goap
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name dfire F-invert-testscore F-normalize-testscore \
+  V-refscore-name goap F-invert-refscore F-normalize-refscore \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
 
 SUBDIR=$OUTDIR/rwplus_vs_cadscore
 mkdir -p $SUBDIR
@@ -100,6 +140,43 @@ R --vanilla --args \
 | grep 'Results statistics output' -A 9999 \
 > $SUBDIR/log
 
+SUBDIR=$OUTDIR/rwplus_vs_qscore
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name rwplus F-invert-testscore F-normalize-testscore \
+  V-refscore-name qscore_atom \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/rwplus_vs_goap
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name rwplus F-invert-testscore F-normalize-testscore \
+  V-refscore-name goap F-invert-refscore F-normalize-refscore \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/rwplus_vs_dfire
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name rwplus F-invert-testscore F-normalize-testscore \
+  V-refscore-name dfire F-invert-refscore F-normalize-refscore \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+
 SUBDIR=$OUTDIR/qenergy_vs_cadscore
 mkdir -p $SUBDIR
 R --vanilla --args \
@@ -124,48 +201,48 @@ R --vanilla --args \
 | grep 'Results statistics output' -A 9999 \
 > $SUBDIR/log
 
-SUBDIR=$OUTDIR/goap_vs_qscore
-mkdir -p $SUBDIR
-R --vanilla --args \
-  V-input $INFILE \
-  V-testscore-name goap F-invert-testscore F-normalize-testscore \
-  V-refscore-name qscore_atom \
-  V-pdf-output $SUBDIR/plots.pdf \
-  F-plot-per-target \
-< $SCRIPTDIR/analyze_global_scores.R \
-| grep 'Results statistics output' -A 9999 \
-> $SUBDIR/log
-
-SUBDIR=$OUTDIR/dfire_vs_qscore
-mkdir -p $SUBDIR
-R --vanilla --args \
-  V-input $INFILE \
-  V-testscore-name dfire F-invert-testscore F-normalize-testscore \
-  V-refscore-name qscore_atom \
-  V-pdf-output $SUBDIR/plots.pdf \
-  F-plot-per-target \
-< $SCRIPTDIR/analyze_global_scores.R \
-| grep 'Results statistics output' -A 9999 \
-> $SUBDIR/log
-
-SUBDIR=$OUTDIR/rwplus_vs_qscore
-mkdir -p $SUBDIR
-R --vanilla --args \
-  V-input $INFILE \
-  V-testscore-name rwplus F-invert-testscore F-normalize-testscore \
-  V-refscore-name qscore_atom \
-  V-pdf-output $SUBDIR/plots.pdf \
-  F-plot-per-target \
-< $SCRIPTDIR/analyze_global_scores.R \
-| grep 'Results statistics output' -A 9999 \
-> $SUBDIR/log
-
 SUBDIR=$OUTDIR/qenergy_vs_qscore
 mkdir -p $SUBDIR
 R --vanilla --args \
   V-input $INFILE \
   V-testscore-name qenergy F-invert-testscore F-normalize-testscore-by-area \
   V-refscore-name qscore_atom \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/qenergy_vs_goap
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name qenergy F-invert-testscore F-normalize-testscore \
+  V-refscore-name goap F-invert-refscore F-normalize-refscore \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/qenergy_vs_dfire
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name qenergy F-invert-testscore F-normalize-testscore \
+  V-refscore-name dfire F-invert-refscore F-normalize-refscore \
+  V-pdf-output $SUBDIR/plots.pdf \
+  F-plot-per-target \
+< $SCRIPTDIR/analyze_global_scores.R \
+| grep 'Results statistics output' -A 9999 \
+> $SUBDIR/log
+
+SUBDIR=$OUTDIR/qenergy_vs_rwplus
+mkdir -p $SUBDIR
+R --vanilla --args \
+  V-input $INFILE \
+  V-testscore-name qenergy F-invert-testscore F-normalize-testscore \
+  V-refscore-name rwplus F-invert-refscore F-normalize-refscore \
   V-pdf-output $SUBDIR/plots.pdf \
   F-plot-per-target \
 < $SCRIPTDIR/analyze_global_scores.R \
