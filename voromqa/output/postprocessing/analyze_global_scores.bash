218e36da736fb48aa7699afe479f6d79a772b7f3
kliment <kliment@localhost>
2015-06-02 15:19:33 +0300

    Improved global scores analysis.

--- voromqa/output/postprocessing/analyze_global_scores.bash
+++ voromqa/output/postprocessing/analyze_global_scores.bash
@@ -24,6 +24,7 @@ function analyze
 }
 
 analyze "qscore_atom" "cadscore_residue"
+analyze "qscore_atom" "cadscore_atom"
 analyze "qscore_atom" "tmscore"
 analyze "qscore_atom" "goap" "F-invert-refscore F-normalize-refscore"
 analyze "qscore_atom" "dfire" "F-invert-refscore F-normalize-refscore"
@@ -32,33 +33,46 @@ analyze "qscore_atom" "rwplus" "F-invert-refscore F-normalize-refscore"
 analyze "qscore_atom" "doop" "F-invert-refscore F-normalize-refscore"
 
 analyze "qenergy" "cadscore_residue" "F-invert-testscore F-normalize-testscore-by-area"
+analyze "qenergy" "cadscore_atom" "F-invert-testscore F-normalize-testscore-by-area"
 analyze "qenergy" "tmscore" "F-invert-testscore F-normalize-testscore-by-area"
 
 analyze "qarea" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "qarea" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "qarea" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "qsas" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "qsas" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "qsas" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "goap" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "goap" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "goap" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "dfire" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "dfire" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "dfire" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "goap_ag" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "goap_ag" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "goap_ag" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "rwplus" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "rwplus" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "rwplus" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 analyze "doop" "cadscore_residue" "F-invert-testscore F-normalize-testscore"
+analyze "doop" "cadscore_atom" "F-invert-testscore F-normalize-testscore"
 analyze "doop" "tmscore" "F-invert-testscore F-normalize-testscore"
 
 {
 	find $OUTDIR -type f -name results_summary | head -1 | xargs -L 1 head -1
 	find $OUTDIR -type f -name results_summary | xargs -L 1 tail -1 | grep cadscore_residue | sort
 } | column -t > $OUTDIR/results_summaries_for_cadscore_residue
+
+{
+	find $OUTDIR -type f -name results_summary | head -1 | xargs -L 1 head -1
+	find $OUTDIR -type f -name results_summary | xargs -L 1 tail -1 | grep cadscore_atom | sort
+} | column -t > $OUTDIR/results_summaries_for_cadscore_atom
 
 {
 	find $OUTDIR -type f -name results_summary | head -1 | xargs -L 1 head -1
