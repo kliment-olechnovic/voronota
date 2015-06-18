88aaf9b496befd6793db082b8800e99d59559dd9
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-06-18 15:03:15 +0300

    Added external weights usage for global quality scoring.

--- voromqa/scripts/calc_quality_scores_from_energies.bash
+++ voromqa/scripts/calc_quality_scores_from_energies.bash
@@ -17,11 +17,16 @@ do
 	esac
 done
 
+cat $WORKDIR/contacts \
+| $BINDIR/voronota query-contacts-depth-values \
+> $WORKDIR/depth_values
+
 cat $WORKDIR/atom_energies \
 | $BINDIR/voronota score-contacts-quality \
   --default-mean -0.34 \
   --default-sd 0.19 \
   --means-and-sds-file $ENERGIES_STATS_FILE \
+  --external-weights-file $WORKDIR/depth_values \
   --smoothing-window 5 \
   --atom-scores-file $WORKDIR/atom_quality_scores \
   --residue-scores-file $WORKDIR/residue_quality_scores \
