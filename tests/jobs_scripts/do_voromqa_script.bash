03dd98aab4857be1ed48efb8455b6e2b360824bd
kliment <kliment@localhost>
2015-07-07 15:40:07 +0300

    Improved VoroMQA user script.

--- tests/jobs_scripts/do_voromqa_script.bash
+++ tests/jobs_scripts/do_voromqa_script.bash
@@ -8,5 +8,9 @@ mkdir -p $SUBDIR
 for INFILE in $INPUTDIR/complex/*.pdb
 do
 	INFILEBASENAME=$(basename $INFILE .pdb)
-	$VORONOTADIR/voromqa $INFILE $SUBDIR/$INFILEBASENAME | sed "s/^VoroMQA global score =/$INFILEBASENAME/"
+	$VORONOTADIR/voromqa \
+	  -i $INFILE \
+	  -a $SUBDIR/$INFILEBASENAME/atom_scores.pdb \
+	  -r $SUBDIR/$INFILEBASENAME/residue_scores.pdb \
+	| sed "s/^VoroMQA global score =/$INFILEBASENAME/"
 done > $SUBDIR/global_scores
