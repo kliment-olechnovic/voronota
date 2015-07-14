cd180daddf3d84e0ff49cf9fbab6e57ff7513f2f
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-07-14 14:46:13 +0300

    Refactored.

--- src/modescommon/drawing_utilities.h
+++ src/modescommon/drawing_utilities.h
@@ -10,62 +10,6 @@ namespace
 typedef auxiliaries::ChainResidueAtomDescriptor CRAD;
 typedef auxiliaries::ChainResidueAtomDescriptorsPair CRADsPair;
 
-unsigned int calc_string_color_integer(const std::string& str)
-{
-	const long generator=123456789;
-	const long limiter=0xFFFFFF;
-	long hash=generator;
-	for(std::size_t i=0;i<str.size();i++)
-	{
-		hash+=static_cast<long>(str[i]+1)*static_cast<long>(i+1)*generator;
-	}
-	return static_cast<unsigned int>(hash%limiter);
-}
-
-unsigned int calc_two_crads_color_integer(const CRAD& a, const CRAD& b)
-{
-	return calc_string_color_integer(a<b ? (a.str()+b.str()) : (b.str()+a.str()));
-}
-
-std::string construct_label_from_crad(const CRAD& crad)
-{
-	std::ostringstream output;
-	output << "[ ";
-	if(!crad.chainID.empty())
-	{
-		output << crad.chainID;
-		output << " ";
-	}
-	if(crad.resSeq!=CRAD::null_num())
-	{
-		output << crad.resSeq;
-		if(!crad.iCode.empty())
-		{
-			output << crad.iCode;
-		}
-		output << " ";
-	}
-	if(!crad.resName.empty())
-	{
-		output << crad.resName;
-		output << " ";
-	}
-	if(!crad.name.empty())
-	{
-		output << crad.name;
-		output << " ";
-	}
-	output << "]";
-	return output.str();
-}
-
-std::string construct_label_from_two_crads(const CRAD& a, const CRAD& b)
-{
-	std::ostringstream output;
-	output << construct_label_from_crad(a) << " " << construct_label_from_crad(b);
-	return output.str();
-}
-
 class DrawingParametersWrapper
 {
 public:
@@ -142,6 +86,18 @@ public:
 	}
 
 private:
+	static unsigned int calc_string_color_integer(const std::string& str)
+	{
+		const long generator=123456789;
+		const long limiter=0xFFFFFF;
+		long hash=generator;
+		for(std::size_t i=0;i<str.size();i++)
+		{
+			hash+=static_cast<long>(str[i]+1)*static_cast<long>(i+1)*generator;
+		}
+		return static_cast<unsigned int>(hash%limiter);
+	}
+
 	static unsigned int calc_color_integer(const CRAD& crad)
 	{
 		return calc_string_color_integer(crad.str());
@@ -190,7 +146,7 @@ private:
 	static std::string construct_label(const std::pair<CRAD, CRAD>& crads)
 	{
 		std::ostringstream output;
-		output << construct_label_from_crad(crads.first) << " " << construct_label_from_crad(crads.second);
+		output << construct_label(crads.first) << " " << construct_label(crads.second);
 		return output.str();
 	}
 };
