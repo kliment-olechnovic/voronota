8a6196890f74735d7d015f126a22a75f24e841fa
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-07-22 15:47:49 +0300

    Improved resources script generation.

--- stock.bash
+++ stock.bash
@@ -15,14 +15,23 @@ function write_resource_file
 }
 
 {
-	echo '#!/bin/bash'
-	echo ''
-	echo 'RESOURCE_NAME=$1'
-	find ./resources/ -type f -not -empty | sort | while read RESOURCE_FILE
-	do
-		echo ''
-		write_resource_file $RESOURCE_FILE
-	done
+	
+cat << 'EOF'
+#!/bin/bash
+
+RESOURCE_NAME=$1
+
+if [ -z $RESOURCE_NAME ]
+then
+echo -e 'Need resource name as argument, available options are:\n  radii\n  energy_potential\n  energy_means_and_sds' >&2
+fi
+
+EOF
+
+write_resource_file ./resources/radii
+write_resource_file ./resources/energy_potential
+write_resource_file ./resources/energy_means_and_sds
+
 } > ./voronota-resources
 
 chmod +x ./voronota-resources
