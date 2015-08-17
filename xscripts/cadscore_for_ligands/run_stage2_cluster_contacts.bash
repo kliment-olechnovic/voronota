9e62f260d5746de178e4529944dc1f452dc754f2
kliment <kliment@localhost>
2015-08-17 13:34:40 +0300

    Added ligands contacts consensus scores plotting.

--- xscripts/cadscore_for_ligands/run_stage2_cluster_contacts.bash
+++ xscripts/cadscore_for_ligands/run_stage2_cluster_contacts.bash
@@ -13,14 +13,15 @@ mkdir -p $CLUSTERS_OUTPUT_DIR
 
 find $CONTACTS_INPUT_DIR -type f \
 | voronota vectorize-contacts \
-  --clustering-output $CLUSTERS_OUTPUT_DIR/list.txt \
+  --clustering-output $CLUSTERS_OUTPUT_DIR/list_of_clusters.txt \
   --clustering-threshold $CLUSTERING_THRESHOLD \
+  --consensus-list $CLUSTERS_OUTPUT_DIR/list_of_consensus_scores.txt \
 > /dev/null
 
 {
 CLUSTER_ID=1
 IFS=''
-cat $CLUSTERS_OUTPUT_DIR/list.txt | sed 's/.pdb//g' | while read LINE
+cat $CLUSTERS_OUTPUT_DIR/list_of_clusters.txt | sed 's/.pdb//g' | while read LINE
 do
 	TOKEN_ID=1
 	echo $LINE | tr ' ' '\n' | while read TOKEN
@@ -37,3 +38,14 @@ done
 echo "set all_states, on"
 echo "zoom"
 } > $CLUSTERS_OUTPUT_DIR/pymol_commands.pml
+
+if command -v voronota &> /dev/null
+then
+R --vanilla --args $CLUSTERS_OUTPUT_DIR/list_of_consensus_scores.txt $CLUSTERS_OUTPUT_DIR/distribution_of_consensus_scores.png &> /dev/null << 'EOF'
+args=commandArgs(TRUE);
+t=read.table(args[1], header=FALSE, stringsAsFactors=FALSE);
+png(args[2]);
+plot(density(t$V2), xlab="Consensus score", main="Distribution of consensus scores");
+dev.off();
+EOF
+fi
