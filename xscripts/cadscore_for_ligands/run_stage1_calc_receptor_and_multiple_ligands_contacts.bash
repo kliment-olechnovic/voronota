ffea9f7c486a89a12c5b4dcba27274d1efdb31e1
kliment <kliment@localhost>
2015-08-17 14:40:02 +0300

    Improved interface of preliminary script pipeline for ligands contacts clustering.

--- xscripts/cadscore_for_ligands/run_stage1_calc_receptor_and_multiple_ligands_contacts.bash
+++ xscripts/cadscore_for_ligands/run_stage1_calc_receptor_and_multiple_ligands_contacts.bash
@@ -1,15 +1,59 @@
 #!/bin/bash
 
-readonly RECEPTOR_FILE="./input/pdb_files/receptor.pdb"
-readonly LIGANDS_FILES_SEARCH_DIR="./input/pdb_files"
-readonly LIGAND_FILE_NAME_PATTERN="manualfix_*.pdb"
-readonly SYMMETRY_INPUT_FILE="./input/symmetry_map.txt"
-readonly CONTACTS_OUTPUT_DIR="./output/contacts"
+set +e
 
-find $LIGANDS_FILES_SEARCH_DIR -type f -name "$LIGAND_FILE_NAME_PATTERN" \
+readonly ZEROARG=$0
+RECEPTOR_FILE=""
+LIGANDS_FILES_LIST=""
+SYMMETRY_INPUT_FILE_OPTION=""
+WORK_DIR=""
+HELP_MODE=false
+
+while getopts "r:l:w:s:h" OPTION
+do
+	case $OPTION in
+	r)
+		RECEPTOR_FILE=$OPTARG
+		;;
+	l)
+		LIGANDS_FILES_LIST=$OPTARG
+		;;
+	w)
+		WORK_DIR=$OPTARG
+		;;
+	s)
+		SYMMETRY_INPUT_FILE_OPTION=" -s $OPTARG "
+		;;
+	h)
+		HELP_MODE=true
+		;;
+	esac
+done
+
+if [ -z "$RECEPTOR_FILE" ] || [ -z "$LIGANDS_FILES_LIST" ] || [ -z "$WORK_DIR" ] || $HELP_MODE
+then
+cat >&2 << EOF
+Script parameters:
+    -r receptor_file.pdb
+    -l list_of_ligands_files.txt
+    -w output_working_directory
+    [-s symmetry_map.txt]
+EOF
+exit 1
+fi
+
+if [[ $ZEROARG == *"/"* ]]
+then
+	cd $(dirname $ZEROARG)
+	export PATH=$(pwd):$PATH
+	cd - &> /dev/null
+fi
+
+command -v calc_receptor_and_ligand_contacts.bash &> /dev/null || { echo >&2 "Error: 'calc_receptor_and_ligand_contacts.bash' executable not in binaries path"; exit 1; }
+
+cat $LIGANDS_FILES_LIST \
 | xargs -L 1 -P 4 \
-  ./calc_receptor_and_ligand_contacts.bash \
-  -r $RECEPTOR_FILE \
-  -s $SYMMETRY_INPUT_FILE \
-  -o $CONTACTS_OUTPUT_DIR \
+  calc_receptor_and_ligand_contacts.bash \
+  -r $RECEPTOR_FILE $SYMMETRY_INPUT_FILE_OPTION \
+  -o $WORK_DIR/contacts \
   -l
