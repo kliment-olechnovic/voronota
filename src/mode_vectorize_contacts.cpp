8a876f2d8797c58735496d460b6c2892fc24396b
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-08-19 10:05:34 +0300

    Refactored filepath utilities.

--- src/mode_vectorize_contacts.cpp
+++ src/mode_vectorize_contacts.cpp
@@ -3,6 +3,7 @@
 #include "auxiliaries/chain_residue_atom_descriptor.h"
 
 #include "modescommon/vectorization_utilities.h"
+#include "modescommon/filepath_utilities.h"
 
 namespace
 {
@@ -25,32 +26,6 @@ inline std::ostream& operator<<(std::ostream& output, const CRADsPair& crads_pai
 }
 
 typedef VectorizationUtilities<std::string, CRADsPair, double> Vectorizer;
-
-std::size_t calc_common_path_start_length(const std::set<std::string>& filenames)
-{
-	std::size_t common_path_start_length=0;
-	if(filenames.size()>1)
-	{
-		const std::string& first_filename=(*filenames.begin());
-		bool common_start=true;
-		for(std::size_t pos=0;(common_start && pos<first_filename.size());pos++)
-		{
-			for(std::set<std::string>::const_iterator it=filenames.begin();(common_start && it!=filenames.end());++it)
-			{
-				const std::string& filename=(*it);
-				if(!(pos<filename.size() && filename[pos]==first_filename[pos]))
-				{
-					common_start=false;
-				}
-			}
-			if(common_start && (first_filename[pos]=='/' || first_filename[pos]=='\\'))
-			{
-				common_path_start_length=(pos+1);
-			}
-		}
-	}
-	return common_path_start_length;
-}
 
 Vectorizer::MapOfMaps read_maps_of_contacts()
 {
---
+++ src/modescommon/filepath_utilities.h
@@ -0,0 +1,38 @@
+#ifndef FILEPATH_UTILITIES_H_
+#define FILEPATH_UTILITIES_H_
+
+#include <string>
+#include <vector>
+
+namespace
+{
+
+std::size_t calc_common_path_start_length(const std::set<std::string>& filenames)
+{
+	std::size_t common_path_start_length=0;
+	if(filenames.size()>1)
+	{
+		const std::string& first_filename=(*filenames.begin());
+		bool common_start=true;
+		for(std::size_t pos=0;(common_start && pos<first_filename.size());pos++)
+		{
+			for(std::set<std::string>::const_iterator it=filenames.begin();(common_start && it!=filenames.end());++it)
+			{
+				const std::string& filename=(*it);
+				if(!(pos<filename.size() && filename[pos]==first_filename[pos]))
+				{
+					common_start=false;
+				}
+			}
+			if(common_start && (first_filename[pos]=='/' || first_filename[pos]=='\\'))
+			{
+				common_path_start_length=(pos+1);
+			}
+		}
+	}
+	return common_path_start_length;
+}
+
+}
+
+#endif /* FILEPATH_UTILITIES_H_ */
