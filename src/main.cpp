a93f8d8e7da45788a0eccf9bddf41008a35b9081
kliment <kliment@localhost>
2015-09-15 14:50:35 +0300

    Refactored demo  source files.

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,6 @@ void compare_contacts(const auxiliaries::ProgramOptionsHandler&);
 void vectorize_contacts(const auxiliaries::ProgramOptionsHandler&);
 void vectorize_points(const auxiliaries::ProgramOptionsHandler&);
 void score_scores(const auxiliaries::ProgramOptionsHandler&);
-//void generate_demo(const auxiliaries::ProgramOptionsHandler&);
 
 struct ModeDescriptor
 {
@@ -75,7 +74,6 @@ std::vector<ModeDescriptor> get_list_of_modes()
 	list_of_modes.push_back(ModeDescriptor("vectorize-contacts", ModeDescriptor::FunctionPtr(vectorize_contacts)));
 	list_of_modes.push_back(ModeDescriptor("vectorize-points", ModeDescriptor::FunctionPtr(vectorize_points)));
 	list_of_modes.push_back(ModeDescriptor("score-scores", ModeDescriptor::FunctionPtr(score_scores)));
-//	list_of_modes.push_back(ModeDescriptor("generate-demo", ModeDescriptor::FunctionPtr(generate_demo)));
 	return list_of_modes;
 }
 
