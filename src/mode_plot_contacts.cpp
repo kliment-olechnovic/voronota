f0be843e3cb8111a918a0aa5f63a082afdfeb385
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2015-10-02 14:45:59 +0300

    Enhanced contacts 2D plotting mode.

--- src/mode_plot_contacts.cpp
+++ src/mode_plot_contacts.cpp
@@ -3,6 +3,7 @@
 
 #include "auxiliaries/program_options_handler.h"
 #include "auxiliaries/chain_residue_atom_descriptor.h"
+#include "auxiliaries/io_utilities.h"
 
 #include "modescommon/contact_value.h"
 #include "modescommon/svg_writer.h"
@@ -24,6 +25,9 @@ void plot_contacts(const auxiliaries::ProgramOptionsHandler& poh)
 	const std::string background_color=poh.argument<std::string>(pohw.describe_option("--background-color", "string", "color string in SVG-acceptable format"), "#000000");
 	const std::string default_color=poh.argument<std::string>(pohw.describe_option("--default-color", "string", "color string in SVG-acceptable format"), "#FFFFFF");
 	const bool adjuncts_rgb=poh.contains_option(pohw.describe_option("--adjuncts-rgb", "", "flag to use RGB color values from adjuncts"));
+	const bool no_contraction=poh.contains_option(pohw.describe_option("--no-contraction", "", "flag to not contract gaps"));
+	const std::string axis_output=poh.argument<std::string>(pohw.describe_option("--axis-output", "string", "file path to output axis"), "");
+	const std::string points_output=poh.argument<std::string>(pohw.describe_option("--points-output", "string", "file path to output points"), "");
 
 	if(!pohw.assert_or_print_help(false))
 	{
@@ -51,8 +55,34 @@ void plot_contacts(const auxiliaries::ProgramOptionsHandler& poh)
 		}
 	}
 
-	SVGWriter svg(axis.size(), axis.size());
-	svg.add_rect(0, 0, axis.size(), axis.size(), background_color);
+	if(no_contraction)
+	{
+		std::size_t i=0;
+		std::map<CRAD, std::size_t>::iterator it=axis.begin();
+		it->second=i++;
+		++it;
+		while(it!=axis.end())
+		{
+			std::map<CRAD, std::size_t>::iterator prev_it=it;
+			--prev_it;
+			const CRAD& a=prev_it->first;
+			const CRAD& b=it->first;
+			if(a.chainID==b.chainID)
+			{
+				for(int j=1;j<(b.resSeq-a.resSeq);j++)
+				{
+					i++;
+				}
+			}
+			it->second=i++;
+			++it;
+		}
+	}
+
+	const std::size_t max_coordinate=(axis.rbegin()->second+1);
+
+	SVGWriter svg(max_coordinate, max_coordinate);
+	svg.add_rect(0, 0, max_coordinate, max_coordinate, background_color);
 	for(std::map<CRADsPair, ContactValue>::const_iterator it=map_of_contacts.begin();it!=map_of_contacts.end();++it)
 	{
 		const std::size_t x=axis[it->first.a];
@@ -70,4 +100,27 @@ void plot_contacts(const auxiliaries::ProgramOptionsHandler& poh)
 		svg.add_rect(y, x, 1, 1, color);
 	}
 	svg.write(std::cout);
+
+	if(!axis_output.empty())
+	{
+		auxiliaries::IOUtilities().write_map_to_file(axis, axis_output);
+	}
+
+	if(!points_output.empty())
+	{
+		std::ofstream output(points_output.c_str(), std::ios::out);
+		if(output.good())
+		{
+			for(std::map<CRADsPair, ContactValue>::const_iterator it=map_of_contacts.begin();it!=map_of_contacts.end();++it)
+			{
+				std::size_t x=axis[it->first.a];
+				std::size_t y=axis[it->first.b];
+				if(x>y)
+				{
+					std::swap(x, y);
+				}
+				output << x << " " << y << "\n";
+			}
+		}
+	}
 }
