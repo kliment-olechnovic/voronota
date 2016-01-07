e63ced125818ed0a5f00c0524e7227511872eb45
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2016-01-07 11:34:38 +0200

    Enhanced CAD-score wrapper script to accept external contacts input.

--- tests/jobs_scripts/do_cadscore_script.bash
+++ tests/jobs_scripts/do_cadscore_script.bash
@@ -9,8 +9,33 @@ do
 	$VORONOTADIR/voronota-cadscore \
 	  -t $INPUTDIR/complex/target.pdb \
 	  -m $INFILE \
-	  -T $SUBDIR/$INFILEBASENAME/residue_scores_on_target.pdb \
-	  -M $SUBDIR/$INFILEBASENAME/residue_scores_on_model.pdb \
+	  -a $SUBDIR/$INFILEBASENAME/residue_scores_on_target.pdb \
+	  -b $SUBDIR/$INFILEBASENAME/residue_scores_on_model.pdb \
 	  -s 1 \
 	| sed "s/^CAD-score global score =/$INFILEBASENAME/"
 done > $SUBDIR/global_scores
+
+VORONOTA=$VORONOTADIR/voronota
+for INFILE in $INPUTDIR/complex/*.pdb
+do
+	INFILEBASENAME=$(basename $INFILE .pdb)
+	cat $INFILE \
+	| $VORONOTA get-balls-from-atoms-file --annotated \
+	| $VORONOTA calculate-contacts --annotated \
+	| $VORONOTA query-contacts --no-same-chain --no-solvent \
+	> $SUBDIR/$INFILEBASENAME/interface_contacts
+done
+
+for INFILE in $INPUTDIR/complex/*.pdb
+do
+	INFILEBASENAME=$(basename $INFILE .pdb)
+	$VORONOTADIR/voronota-cadscore \
+	  -t $INPUTDIR/complex/target.pdb \
+	  -m $INFILE \
+	  -T $SUBDIR/target/interface_contacts \
+	  -M $SUBDIR/$INFILEBASENAME/interface_contacts \
+	  -a $SUBDIR/$INFILEBASENAME/interface_residue_scores_on_target.pdb \
+	  -b $SUBDIR/$INFILEBASENAME/interface_residue_scores_on_model.pdb \
+	  -s 1 \
+	| sed "s/^CAD-score global score =/$INFILEBASENAME/"
+done > $SUBDIR/interface_global_scores
