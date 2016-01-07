74f41309cc9904dd2ed2a0ebff348df30a965d0e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2016-01-07 15:04:03 +0200

    Simplified CAD-score wrapper script.

--- tests/jobs_scripts/do_cadscore_script.bash
+++ tests/jobs_scripts/do_cadscore_script.bash
@@ -15,35 +15,15 @@ do
 	| sed "s/^CAD-score global score =/$INFILEBASENAME/"
 done > $SUBDIR/global_scores
 
-VORONOTA=$VORONOTADIR/voronota
-for INFILE in $INPUTDIR/complex/*.pdb
-do
-	INFILEBASENAME=$(basename $INFILE .pdb)
-	cat $INFILE \
-	| $VORONOTA get-balls-from-atoms-file --annotated \
-	| $VORONOTA calculate-contacts --annotated \
-	| $VORONOTA query-contacts --no-same-chain --no-solvent \
-	> $SUBDIR/$INFILEBASENAME/interface_contacts
-done
-
 for INFILE in $INPUTDIR/complex/*.pdb
 do
 	INFILEBASENAME=$(basename $INFILE .pdb)
 	$VORONOTADIR/voronota-cadscore \
 	  -t $INPUTDIR/complex/target.pdb \
 	  -m $INFILE \
-	  -T $SUBDIR/target/interface_contacts \
-	  -M $SUBDIR/$INFILEBASENAME/interface_contacts \
+	  -q "--no-same-chain" \
 	  -a $SUBDIR/$INFILEBASENAME/interface_residue_scores_on_target.pdb \
 	  -b $SUBDIR/$INFILEBASENAME/interface_residue_scores_on_model.pdb \
-	  -s 1
-done > /dev/null
-
-for INFILE in $INPUTDIR/complex/*.pdb
-do
-	INFILEBASENAME=$(basename $INFILE .pdb)
-	$VORONOTADIR/voronota-cadscore \
-	  -T $SUBDIR/target/interface_contacts \
-	  -M $SUBDIR/$INFILEBASENAME/interface_contacts \
+	  -s 1 \
 	| sed "s/^CAD-score global score =/$INFILEBASENAME/"
 done > $SUBDIR/interface_global_scores
