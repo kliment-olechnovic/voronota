bd6050ad32e0469c2bd0ca8c9792d88a254f61f2
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2016-01-22 16:24:05 +0200

    Dropped older qa analysis.

--- voromqa/scripts/calc_dssp_info_from_atoms.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WORKDIR=$1
-
-$BINDIR/dssp $WORKDIR/atoms.pdb > $WORKDIR/dssp_info
--- voromqa/scripts/calc_rwplus_score_from_atoms.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WORKDIR=$1
-
-cd $BINDIR/rwplus
-
-./calRWplus $WORKDIR/atoms.pdb \
-| egrep '^RW potential' \
-> $WORKDIR/rwplus_score
