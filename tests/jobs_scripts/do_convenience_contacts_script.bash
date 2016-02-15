0d5cf5dc0b8146135ea5bf0762cc392f5864d301
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2016-02-15 15:33:22 +0200

    Added drawing feature to the contacts calculation convenience script.

--- tests/jobs_scripts/do_convenience_contacts_script.bash
+++ tests/jobs_scripts/do_convenience_contacts_script.bash
@@ -14,6 +14,8 @@ $VORONOTADIR/voronota-contacts \
   -i $INPUTDIR/single/structure.pdb \
   -a "--match r<1:50>" \
   -c "--inter-residue --match-first R<LYS>" \
+  -d $SUBDIR/contacts1_drawing.py \
+  -g "--default-color 0x00FFFF --drawing-name contacts1" \
   -C $SUBDIR/cache \
 > $SUBDIR/contacts1
 
