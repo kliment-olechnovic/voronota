387eb1cbe35edc03d133fbc9db4c17a65d2186bd
kliment <kliment@localhost>
2016-03-25 09:33:04 +0200

    Added selection scores output feature in VoroMQA script.

--- tests/jobs_scripts/do_voromqa_script.bash
+++ tests/jobs_scripts/do_voromqa_script.bash
@@ -11,10 +11,12 @@ do
 	  --input $INFILE \
 	  --output-atom-scores $SUBDIR/$INFILEBASENAME/atom_scores \
 	  --output-atom-scores-pdb $SUBDIR/$INFILEBASENAME/atom_scores.pdb \
+	  --smoothing-window 5 \
 	  --output-residue-scores $SUBDIR/$INFILEBASENAME/residue_scores \
 	  --output-residue-scores-pdb $SUBDIR/$INFILEBASENAME/residue_scores.pdb \
 	  --output-residue-scores-for-CAMEO $SUBDIR/$INFILEBASENAME/cameo_residue_scores.pdb \
 	  --output-contacts-map-svg $SUBDIR/$INFILEBASENAME/colored_map.svg \
-	  --smoothing-window 5 \
-	  --contacts-query '--no-same-chain --no-solvent'
+	  --contacts-query '--no-same-chain --no-solvent' \
+	  --output-selected-scores $SUBDIR/$INFILEBASENAME/selected_atom_scores
+
 done > $SUBDIR/global_scores
