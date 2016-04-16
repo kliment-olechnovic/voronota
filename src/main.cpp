e53a2e754ab6c4df173c559ef5b803b93d759159
kliment <kliment@localhost>
2016-04-16 18:41:11 +0300

    Added mode to write QA scores in CASP format.

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ void query_contacts_simulating_unfolding(const auxiliaries::ProgramOptionsHandle
 void vectorize_contact_environments(const auxiliaries::ProgramOptionsHandler&);
 void wrapped_get_balls_from_atoms_file_and_calculate_vertices(const auxiliaries::ProgramOptionsHandler&);
 void expand_descriptors(const auxiliaries::ProgramOptionsHandler&);
+void write_qa_scores_in_casp_format(const auxiliaries::ProgramOptionsHandler&);
 
 struct ModeDescriptor
 {
@@ -87,6 +88,7 @@ std::vector<ModeDescriptor> get_list_of_xmodes()
 	list_of_modes.push_back(ModeDescriptor("x-vectorize-contact-environments", ModeDescriptor::FunctionPtr(vectorize_contact_environments)));
 	list_of_modes.push_back(ModeDescriptor("x-wrapped-get-balls-from-atoms-file-and-calculate-vertices", ModeDescriptor::FunctionPtr(wrapped_get_balls_from_atoms_file_and_calculate_vertices)));
 	list_of_modes.push_back(ModeDescriptor("x-expand-descriptors", ModeDescriptor::FunctionPtr(expand_descriptors)));
+	list_of_modes.push_back(ModeDescriptor("x-write-qa-scores-in-casp-format", ModeDescriptor::FunctionPtr(write_qa_scores_in_casp_format)));
 	return list_of_modes;
 }
 
