7c511c5596a1aa21f7f08650f28b6c1949a07c71
kliment <kliment@localhost>
2016-04-18 09:39:29 +0300

    Added testing of side-chains rebuilding.

--- tests/list_missing_external_tools.bash
+++ tests/list_missing_external_tools.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TOOLCOMMAND in g++ cppcheck mpic++ mpirun cmake dssp hbplus
+for TOOLCOMMAND in g++ cppcheck mpic++ mpirun cmake dssp hbplus Scwrl4
 do
 	command -v $TOOLCOMMAND &> /dev/null || { echo "'$TOOLCOMMAND' executable not in binaries path"; }
 done
