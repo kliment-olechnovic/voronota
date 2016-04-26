7422def3ac3e443fc1a666ad800d8216d79a7eda
kliment <kliment@localhost>
2016-04-26 10:28:57 +0300

    Incremented version number, updated some experimental scripts to match the new version.

--- xscripts/show_interface_voromqa_scores.bash
+++ xscripts/show_interface_voromqa_scores.bash
@@ -6,23 +6,18 @@ readonly TMPDIR=$(mktemp -d)
 trap "rm -r $TMPDIR" EXIT
 
 voronota-voromqa \
-  -i $INFILE \
-  -a $TMPDIR/iscores \
-  -q
-
-voronota-bfactor \
-  -p $INFILE \
-  -s $TMPDIR/iscores \
-  -q \
-> $TMPDIR/struct.pdb
+  --input $INFILE \
+  --output-atom-scores $TMPDIR/iscores \
+  --output-atom-scores-pdb $TMPDIR/struct.pdb \
+  --multiple-models
 
 voronota-contacts \
-  -i $INFILE \
-  -m "--set-external-means $TMPDIR/iscores" \
-  -c '--no-same-chain --no-solvent' \
-  -g '--drawing-name faces --adjunct-gradient em --adjunct-gradient-blue 1 --adjunct-gradient-red 0' \
-  -d $TMPDIR/ifaces.py \
-  -q \
+  --input $INFILE \
+  --contacts-query-additional "--set-external-means $TMPDIR/iscores" \
+  --contacts-query '--no-same-chain --no-solvent' \
+  --drawing-parameters '--drawing-name faces --adjunct-gradient em --adjunct-gradient-blue 1 --adjunct-gradient-red 0' \
+  --output-drawing $TMPDIR/ifaces.py \
+  --multiple-models \
 > /dev/null
 
 cat > $TMPDIR/script.pml << EOF
