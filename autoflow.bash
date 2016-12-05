5815ea75c6ee7d39621cff641878e71097a442f7
kliment <kliment@localhost>
2016-12-05 14:54:35 +0200

    Added first version of autoflow script.

---
+++ autoflow.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+VERSIONID=$(cat ./src/main.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version \(\S\+\)";/\1/')
+
+readonly TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+cp -r ./src "$TMPLDIR/"
+cp ./voronota-resources ./voronota-voromqa ./voronota-contacts ./voronota-cadscore "$TMPLDIR/"
+
+cd $TMPLDIR
+
+echo $VERSIONID
+ls -1
+
+{
+cat << 'EOF'
+AC_INIT([voronota], [VERSIONID])
+AM_INIT_AUTOMAKE([foreign])
+AC_PROG_CXX
+AC_CONFIG_FILES([Makefile])
+AC_OUTPUT
+EOF
+} \
+| sed "s/VERSIONID/$VERSIONID/" \
+> "./configure.ac"
+
+cat "./configure.ac"
+
+{
+cat << EOF
+bin_PROGRAMS = voronota
+voronota_SOURCES = $(find src/ -type f | tr '\n' ' ')
+dist_bin_SCRIPTS = voronota-resources voronota-voromqa voronota-contacts voronota-cadscore
+EOF
+} \
+> "./Makefile.am"
+
+autoreconf -i
+
+./configure
+make dist
+
+cd -
+cp "$TMPLDIR/voronota-$VERSIONID.tar.gz" "./voronota-$VERSIONID.tar.gz"
