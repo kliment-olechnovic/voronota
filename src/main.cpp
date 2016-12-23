2c7a0fd9675dbaa18673f2af43bf4cc4ea673e08
kliment <kliment@localhost>
2016-12-23 18:21:30 +0200

    Incremented version number.

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ std::vector<ModeDescriptor> get_list_of_xmodes()
 
 std::string version()
 {
-	static const std::string str="Voronota version 1.13";
+	static const std::string str="Voronota version 1.14";
 	return str;
 }
 
