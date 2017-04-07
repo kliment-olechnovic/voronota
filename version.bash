1e4a1eab2c955a691e2ec7b50ae90ff62534b24e
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2017-04-07 15:08:14 +0300

    Added version printing script.

---
+++ version.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+VERSION_MAJOR=$(cat ./src/main.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version \(\S\+\)";/\1/')
+VERSION_MINOR=$(hg branches | egrep '^default' | tr ':' ' ' | awk '{print $2}')
+
+echo "${VERSION_MAJOR}.${VERSION_MINOR}"
