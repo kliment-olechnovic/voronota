2d5b9673ce9f29f84de245ebcf3ed2f5d50f0f48
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2017-04-07 12:53:40 +0300

    Improved autoflow experimental script.

--- xscripts/autoflow.bash
+++ xscripts/autoflow.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-VERSIONID=$(cat ./src/main.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version \(\S\+\)";/\1/')
+VERSION_MAJOR=$(cat ./src/main.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version \(\S\+\)";/\1/')
+VERSION_MINOR=$(hg branches | egrep '^default' | tr ':' ' ' | awk '{print $2}')
+VERSIONID="${VERSION_MAJOR}.${VERSION_MINOR}"
 
 readonly TMPLDIR=$(mktemp -d)
 trap "rm -r $TMPLDIR" EXIT
@@ -32,6 +34,8 @@ EOF
 > "./Makefile.am"
 
 autoreconf -i
+
+make maintainer-clean
 
 ./configure
 make dist
