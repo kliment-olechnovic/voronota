3852d758bdb2287445c7941b1571f6b51bdbf8dd
kliment <kliment@localhost>
2017-06-26 15:50:17 +0300

    Added sparse mock solvent generation mode.

--- src/mode_calculate_mock_solvent.cpp
+++ src/mode_calculate_mock_solvent.cpp
@@ -20,6 +20,7 @@ void calculate_mock_solvent(const auxiliaries::ProgramOptionsHandler& poh)
 	const double solvent_radius=poh.restrict_value_in_range(1.0, 2.0, poh.argument<double>(pohw.describe_option("--solvent-radius", "number", "solvent atom radius"), 1.4));
 	const double solvent_distance=poh.restrict_value_in_range(0.0, 10.0, poh.argument<double>(pohw.describe_option("--solvent-distance", "number", "min distance from non-solvent atoms to solvent atoms"), 1.0));
 	const int sih_depth=poh.restrict_value_in_range(0, 3, poh.argument<int>(pohw.describe_option("--sih-depth", "number", "spherical surface optimization depth"), 1));
+	const bool sparse_mode=poh.contains_option(pohw.describe_option("--sparse-mode", "", "flag to use sparse mode"));
 
 	if(!pohw.assert_or_print_help(false))
 	{
@@ -28,48 +29,127 @@ void calculate_mock_solvent(const auxiliaries::ProgramOptionsHandler& poh)
 
 	const double probe=(solvent_radius+solvent_distance);
 
-	std::vector<apollota::SimpleSphere> spheres;
+	std::vector<apollota::SimpleSphere> input_spheres;
 	std::vector< std::pair<auxiliaries::ChainResidueAtomDescriptor, BallValue> > ball_records;
 	auxiliaries::IOUtilities().read_lines_to_map(std::cin, ball_records);
-	spheres.reserve(ball_records.size());
+	input_spheres.reserve(ball_records.size());
 	for(std::size_t i=0;i<ball_records.size();i++)
 	{
-		spheres.push_back(apollota::SimpleSphere(ball_records[i].second));
+		input_spheres.push_back(apollota::SimpleSphere(ball_records[i].second));
 	}
-	if(spheres.size()<4)
+	if(input_spheres.size()<4)
 	{
 		throw std::runtime_error("Less than 4 balls provided to stdin.");
 	}
 
-	const std::size_t input_spheres_count=spheres.size();
-	const std::vector<apollota::SimpleSphere> artificial_boundary=apollota::construct_artificial_boundary(spheres, probe*2.0);
-	spheres.insert(spheres.end(), artificial_boundary.begin(), artificial_boundary.end());
-
-	const apollota::Triangulation::Result triangulation_result=apollota::Triangulation::construct_result(spheres, 3.5, false, false);
-	const apollota::TriangulationQueries::IDsMap ids_map=apollota::TriangulationQueries::collect_neighbors_map_from_quadruples_map(triangulation_result.quadruples_map);
-	const apollota::TriangulationQueries::IDsGraph ids_graph=apollota::TriangulationQueries::collect_ids_graph_from_ids_map(ids_map, input_spheres_count);
-
-	const apollota::SubdividedIcosahedron initial_sih(sih_depth);
-
 	std::vector<apollota::SimpleSphere> mock_solvent_spheres;
-	mock_solvent_spheres.reserve(ids_graph.size()*2);
-	for(std::size_t i=0;i<ids_graph.size();i++)
+
+	if(sparse_mode)
 	{
-		apollota::SubdividedIcosahedron sih=initial_sih;
-		sih.fit_into_sphere(spheres[i], spheres[i].r+probe);
-		for(std::size_t l=0;l<sih.vertices().size();l++)
+		bool repeat=true;
+		while(repeat)
 		{
-			apollota::SimpleSphere sphere_l(sih.vertices()[l], solvent_radius);
-			const double dist_i=apollota::minimal_distance_from_point_to_sphere(sphere_l, spheres[i]);
-			bool good=true;
-			for(std::size_t j=0;j<ids_graph[i].size() && good;j++)
+			std::vector<apollota::SimpleSphere> spheres=input_spheres;
+			if(!mock_solvent_spheres.empty())
 			{
-				const double dist_j=apollota::minimal_distance_from_point_to_sphere(sphere_l, spheres[ids_graph[i][j]]);
-				good=(dist_i<dist_j);
+				spheres.insert(spheres.end(), mock_solvent_spheres.begin(), mock_solvent_spheres.end());
 			}
-			if(good)
+			const std::size_t real_spheres_count=spheres.size();
+
+			const std::vector<apollota::SimpleSphere> artificial_boundary=apollota::construct_artificial_boundary(spheres, probe*2.0);
+			spheres.insert(spheres.end(), artificial_boundary.begin(), artificial_boundary.end());
+
+			const apollota::Triangulation::Result triangulation_result=apollota::Triangulation::construct_result(spheres, 3.5, false, false);
+			const apollota::Triangulation::VerticesVector vertices=apollota::Triangulation::collect_vertices_vector_from_quadruples_map(triangulation_result.quadruples_map);
+
+			apollota::TriangulationQueries::TriplesMap triples_all;
+			apollota::TriangulationQueries::TriplesMap triples_exposed;
+			for(std::size_t i=0;i<vertices.size();i++)
 			{
-				mock_solvent_spheres.push_back(sphere_l);
+				const apollota::Quadruple& q=vertices[i].first;
+				for(unsigned int j=0;j<4;j++)
+				{
+					const apollota::Triple& t=q.exclude(j);
+					if(t.get_min_max().second<real_spheres_count && t.get_min_max().first<input_spheres.size())
+					{
+						const std::size_t n=q.get(j);
+						triples_all[t].insert(n);
+						if(vertices[i].second.r>probe)
+						{
+							triples_exposed[t].insert(n);
+						}
+					}
+				}
+			}
+
+			std::vector<apollota::SimpleSphere> new_mock_solvent_spheres;
+			new_mock_solvent_spheres.reserve(triples_exposed.size()*2);
+			for(apollota::TriangulationQueries::TriplesMap::const_iterator it=triples_exposed.begin();it!=triples_exposed.end();++it)
+			{
+				const apollota::Triple& t=it->first;
+				const std::vector<apollota::SimpleSphere> tangents=apollota::TangentSphereOfThreeSpheres::calculate(spheres[t.get(0)], spheres[t.get(1)], spheres[t.get(2)], probe);
+				if(!tangents.empty())
+				{
+					const std::set<std::size_t>& ns=triples_all[t];
+					for(std::size_t i=0;i<tangents.size();i++)
+					{
+						bool candidate_good=true;
+						for(std::set<std::size_t>::const_iterator jt=ns.begin();jt!=ns.end() && candidate_good;++jt)
+						{
+							if(apollota::sphere_intersects_sphere(tangents[i], spheres[*jt]))
+							{
+								candidate_good=false;
+							}
+						}
+						if(candidate_good)
+						{
+							new_mock_solvent_spheres.push_back(apollota::SimpleSphere(tangents[i], solvent_radius));
+						}
+					}
+				}
+			}
+
+			if(!new_mock_solvent_spheres.empty())
+			{
+				mock_solvent_spheres.insert(mock_solvent_spheres.end(), new_mock_solvent_spheres.begin(), new_mock_solvent_spheres.end());
+			}
+			else
+			{
+				repeat=false;
+			}
+		}
+	}
+	else
+	{
+		std::vector<apollota::SimpleSphere> spheres=input_spheres;
+		const std::vector<apollota::SimpleSphere> artificial_boundary=apollota::construct_artificial_boundary(spheres, probe*2.0);
+		spheres.insert(spheres.end(), artificial_boundary.begin(), artificial_boundary.end());
+
+		const apollota::Triangulation::Result triangulation_result=apollota::Triangulation::construct_result(spheres, 3.5, false, false);
+		const apollota::TriangulationQueries::IDsMap ids_map=apollota::TriangulationQueries::collect_neighbors_map_from_quadruples_map(triangulation_result.quadruples_map);
+		const apollota::TriangulationQueries::IDsGraph ids_graph=apollota::TriangulationQueries::collect_ids_graph_from_ids_map(ids_map, input_spheres.size());
+
+		const apollota::SubdividedIcosahedron initial_sih(sih_depth);
+
+		mock_solvent_spheres.reserve(ids_graph.size()*2);
+		for(std::size_t i=0;i<ids_graph.size();i++)
+		{
+			apollota::SubdividedIcosahedron sih=initial_sih;
+			sih.fit_into_sphere(spheres[i], spheres[i].r+probe);
+			for(std::size_t l=0;l<sih.vertices().size();l++)
+			{
+				apollota::SimpleSphere sphere_l(sih.vertices()[l], solvent_radius);
+				const double dist_i=apollota::minimal_distance_from_point_to_sphere(sphere_l, spheres[i]);
+				bool good=true;
+				for(std::size_t j=0;j<ids_graph[i].size() && good;j++)
+				{
+					const double dist_j=apollota::minimal_distance_from_point_to_sphere(sphere_l, spheres[ids_graph[i][j]]);
+					good=(dist_i<dist_j);
+				}
+				if(good)
+				{
+					mock_solvent_spheres.push_back(sphere_l);
+				}
 			}
 		}
 	}
