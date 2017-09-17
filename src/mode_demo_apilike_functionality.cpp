1967ae19e140844bb7e5b686ddc9bd2aa98ec4ce
kliment <kliment@localhost>
2017-09-17 20:42:58 +0300

    Added some debug printing.

--- src/mode_demo_apilike_functionality.cpp
+++ src/mode_demo_apilike_functionality.cpp
@@ -2,6 +2,112 @@
 
 #include "common/filtering_of_atoms_and_contacts.h"
 
+namespace
+{
+
+class mocking_test
+{
+public:
+	std::string label;
+
+	bool operator()(const bool val) const
+	{
+		return val;
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& output, const common::FilteringOfAtomsAndContacts::TestingExpressionToken<mocking_test>& token)
+{
+	typedef common::FilteringOfAtomsAndContacts::TestingExpressionToken<mocking_test> Token;
+	if(token.type==Token::TYPE_TESTER)
+	{
+		if(token.tester.label.empty())
+		{
+			output << " . ";
+		}
+		else
+		{
+			output << " " << token.tester.label << " ";
+		}
+	}
+	else if(token.type==Token::TYPE_OPERATOR_OR)
+	{
+		output << " | ";
+	}
+	else if(token.type==Token::TYPE_OPERATOR_AND)
+	{
+		output << " & ";
+	}
+	else if(token.type==Token::TYPE_OPERATOR_NOT)
+	{
+		output << " ! ";
+	}
+	else if(token.type==Token::TYPE_BRACKET_OPEN)
+	{
+		output << " ( ";
+	}
+	else if(token.type==Token::TYPE_BRACKET_CLOSE)
+	{
+		output << " ) ";
+	}
+	return output;
+}
+
+void demo_expression_conversion(const std::string& str)
+{
+	typedef common::FilteringOfAtomsAndContacts::TestingExpressionToken<mocking_test> Token;
+
+	std::vector<Token> infix;
+
+	for(std::size_t i=0;i<str.size();i++)
+	{
+		const char c=str[i];
+		if(c=='(')
+		{
+			infix.push_back(Token(Token::TYPE_BRACKET_OPEN));
+		}
+		else if(c==')')
+		{
+			infix.push_back(Token(Token::TYPE_BRACKET_CLOSE));
+		}
+		else if(c=='|')
+		{
+			infix.push_back(Token(Token::TYPE_OPERATOR_OR));
+		}
+		else if(c=='&')
+		{
+			infix.push_back(Token(Token::TYPE_OPERATOR_AND));
+		}
+		else if(c=='!')
+		{
+			infix.push_back(Token(Token::TYPE_OPERATOR_NOT));
+		}
+		else if(c>' ')
+		{
+			Token token(Token::TYPE_TESTER);
+			token.tester.label.push_back(c);
+			infix.push_back(token);
+		}
+	}
+
+	std::clog << "demo_expression_conversion '" << str << "'\n";
+
+	for(std::size_t i=0;i<infix.size();i++)
+	{
+		std::clog << infix[i];
+	}
+	std::clog << std::endl;
+
+	const std::vector<Token> postfix=common::FilteringOfAtomsAndContacts::convert_testing_expression_from_infix_to_postfix_form(infix);
+	for(std::size_t i=0;i<postfix.size();i++)
+	{
+		std::clog << postfix[i];
+	}
+	std::clog << std::endl;
+}
+
+}
+
 void demo_apilike_functionality(const auxiliaries::ProgramOptionsHandler& poh)
 {
 	auxiliaries::ProgramOptionsHandlerWrapper pohw(poh);
@@ -13,13 +119,18 @@ void demo_apilike_functionality(const auxiliaries::ProgramOptionsHandler& poh)
 		return;
 	}
 
+	demo_expression_conversion("A&(B|C)");
+	demo_expression_conversion("((A)&(B|(C)))");
+	demo_expression_conversion("!A|(B&C)|!D");
+	demo_expression_conversion("(A&B)|(C&D)");
+
 	common::ConstructionOfAtomicBalls::collect_atomic_balls_from_file collect_atomic_balls_from_file;
 
 	std::vector<common::ConstructionOfAtomicBalls::AtomicBall> atomic_balls;
 
 	if(collect_atomic_balls_from_file(std::cin, atomic_balls))
 	{
-		std::clog<< atomic_balls.size() << " atoms" << std::endl;
+		std::clog << atomic_balls.size() << " atoms" << std::endl;
 
 		common::ConstructionOfContacts::construct_bundle_of_contact_information construct_bundle_of_contact_information;
 		construct_bundle_of_contact_information.calculate_volumes=true;
