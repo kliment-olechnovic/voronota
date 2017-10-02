8a97fccba6ed15d74663de9b224d0517132f631c
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2017-10-02 17:00:13 +0300

    Improved manipulator class.

--- src/mode_demo_apilike_functionality.cpp
+++ src/mode_demo_apilike_functionality.cpp
@@ -29,9 +29,10 @@ void demo_apilike_functionality(const auxiliaries::ProgramOptionsHandler& poh)
 	manager.execute_plainly("query-contacts", std::cout);
 	manager.execute_plainly("load-contacts file 'tmp/plain_contacts.txt'", std::cout);
 	manager.execute_plainly("query-contacts use '{atom-first {match R<PHE>} atom-second {match R<PHE>} min-area 5.0 min-seq-sep 1}' print name cs1", std::cout);
-	manager.execute_plainly("query-contacts use '{no-solvent min-seq-sep 2}' print-sorted-reversed 2 print-limit 3", std::cout);
-	manager.execute_plainly("query-contacts use '{no-solvent min-seq-sep 2}' print-sorted-reversed 2 print-limit 3 print-inter-residue", std::cout);
+	manager.execute_plainly("query-contacts use '{no-solvent min-seq-sep 2}' print-sorted-reversed 2 print-limit 3 print-expanded", std::cout);
+	manager.execute_plainly("query-contacts use '{no-solvent min-seq-sep 2}' print-sorted-reversed 2 print-limit 3 print-expanded print-inter-residue", std::cout);
 	manager.execute_plainly("query-atoms use '{match r<64>&A<C,N,O,CA,CB>}' print name as1", std::cout);
+	manager.execute_plainly("query-atoms use '{match r<64>&A<C,N,O,CA,CB>}' print-expanded", std::cout);
 	manager.execute_plainly("rename-selection-of-atoms nosel1 nodel2", std::cout);
 	manager.execute_plainly("delete-selections-of-contacts nosel1", std::cout);
 	manager.execute_plainly("list-selections-of-atoms", std::cout);
