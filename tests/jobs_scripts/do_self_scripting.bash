fee3a7271c465376bb2d253ac4f8bce1578c48b8
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2017-12-19 20:34:07 +0200

    Major refactoring of  self-scripting.

--- tests/jobs_scripts/do_self_scripting.bash
+++ tests/jobs_scripts/do_self_scripting.bash
@@ -19,9 +19,9 @@ select-contacts
 load-contacts --file '$SUBDIR/plain_contacts'
 
 select-contacts {--atom1 {--match R<PHE>} --atom2 {--match R<PHE>} --min-area 5.0 --min-seq-sep 1} --name cs1
-print-contacts {--sel cs1} --sort-r area --file '$SUBDIR/printed_contacts'
-print-contacts {--no-solvent --min-seq-sep 2} --sort-r area --limit 3 --expand
-print-contacts {--no-solvent --min-seq-sep 2} --sort-r area --limit 3 --expand --inter-residue
+print-contacts {--sel cs1} --desc --sort area --file '$SUBDIR/printed_contacts'
+print-contacts {--no-solvent --min-seq-sep 2} --desc --sort area --limit 3 --expand
+print-contacts {--no-solvent --min-seq-sep 2} --desc --sort area --limit 3 --expand --inter-residue
 
 select-atoms {--match r<64>&A<C,N,O,CA,CB>} --name as1
 print-atoms {--sel as1} --sort tags --file '$SUBDIR/printed_atoms'
