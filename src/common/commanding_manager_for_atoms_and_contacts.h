1d2f15be1e00a80a8c5ffa5422015c07dcbae6e6
kliment <kliment@localhost>
2018-02-22 11:08:12 +0200

    Improved atoms loading.

--- src/common/commanding_manager_for_atoms_and_contacts.h
+++ src/common/commanding_manager_for_atoms_and_contacts.h
@@ -1328,6 +1328,34 @@ private:
 		}
 	}
 
+	static std::string get_format_from_atoms_file_name(const std::string& filename)
+	{
+		std::multimap<std::string, std::string> map_of_format_extensions;
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("pdb", ".pdb"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("pdb", ".PDB"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("pdb", ".ent"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("pdb", ".ENT"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("mmcif", ".cif"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("mmcif", ".CIF"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("mmcif", ".mmcif"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("mmcif", ".mmCIF"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("mmcif", ".MMCIF"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("plain", ".atoms"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("plain", ".pa"));
+		map_of_format_extensions.insert(std::pair<std::string, std::string>("plain", ".pac"));
+		for(std::multimap<std::string, std::string>::const_iterator it=map_of_format_extensions.begin();it!=map_of_format_extensions.end();++it)
+		{
+			const std::string& format=it->first;
+			const std::string& extension=it->second;
+			const std::size_t pos=filename.find(extension);
+			if(pos<filename.size() && (pos+extension.size())==filename.size())
+			{
+				return format;
+			}
+		}
+		return std::string();
+	}
+
 	void assert_atoms_representations_availability() const
 	{
 		if(atoms_representation_names_.empty())
@@ -1568,14 +1596,23 @@ private:
 		const std::string atoms_file=cargs.input.get_value_or_first_unused_unnamed_value("file");
 		const std::string radii_file=cargs.input.get_value_or_default<std::string>("radii-file", "");
 		const double default_radius=cargs.input.get_value_or_default<double>("default-radii", ConstructionOfAtomicBalls::ParametersToCollectAtomicBallsFromFile::default_default_radius());
-		const std::string format=cargs.input.get_value_or_default<std::string>("format", "pdb");
 		const bool only_default_radius=cargs.input.get_flag("same-radius-for-all");
+		std::string format=cargs.input.get_value_or_default<std::string>("format", "");
 
 		cargs.input.assert_nothing_unusable();
 
 		if(atoms_file.empty())
 		{
 			throw std::runtime_error(std::string("Empty input atoms file name."));
+		}
+
+		if(format.empty())
+		{
+			format=get_format_from_atoms_file_name(atoms_file);
+			if(format.empty())
+			{
+				throw std::runtime_error(std::string("Could not deduce format from file name '")+atoms_file+"'.");
+			}
 		}
 
 		if(format!="pdb" && format!="mmcif" && format!="plain")
