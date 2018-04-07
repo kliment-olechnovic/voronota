4f5f57df52d6a384a0dc1302e80e7dcbc0abda06
kliment <kliment@localhost>
2018-04-07 17:22:46 +0300

    Improved generalization of atom descriptors.

--- src/common/summary_of_contacts.h
+++ src/common/summary_of_contacts.h
@@ -96,39 +96,13 @@ public:
 	static CRAD generalize_crad(const CRAD& input_crad)
 	{
 		CRAD crad=input_crad.without_numbering();
-		if(crad.resName=="ARG" && (crad.name=="NH1" || crad.name=="NH2"))
-		{
-			crad.name="NH1";
-		}
-		else if(crad.resName=="ASP" && (crad.name=="OD1" || crad.name=="OD2"))
-		{
-			crad.name="OD1";
-		}
-		else if(crad.resName=="GLU" && (crad.name=="OE1" || crad.name=="OE2"))
-		{
-			crad.name="OE1";
-		}
-		else if(crad.resName=="PHE" && (crad.name=="CD1" || crad.name=="CD2"))
-		{
-			crad.name="CD1";
-		}
-		else if(crad.resName=="PHE" && (crad.name=="CE1" || crad.name=="CE2"))
-		{
-			crad.name="CE1";
-		}
-		else if(crad.resName=="TYR" && (crad.name=="CD1" || crad.name=="CD2"))
-		{
-			crad.name="CD1";
-		}
-		else if(crad.resName=="TYR" && (crad.name=="CE1" || crad.name=="CE2"))
-		{
-			crad.name="CE1";
-		}
-		else if(crad.name=="OXT")
+
+		if(crad.name=="OXT")
 		{
 			crad.name="O";
 		}
-		else if(crad.resName=="MSE")
+
+		if(crad.resName=="MSE")
 		{
 			crad.resName="MET";
 			if(crad.name=="SE")
@@ -144,6 +118,291 @@ public:
 				crad.name="SG";
 			}
 		}
+
+		if(crad.resName=="ALA")
+		{
+			if(crad.name=="HB1" || crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB1";
+			}
+		}
+
+		if(crad.resName=="ARG")
+		{
+			if(crad.name=="NH1" || crad.name=="NH2")
+			{
+				crad.name="NH1";
+			}
+			else if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+			else if(crad.name=="HD2" || crad.name=="HD3")
+			{
+				crad.name="HD2";
+			}
+			else if(crad.name=="HH11" || crad.name=="HH12" || crad.name=="HH21" || crad.name=="HH22")
+			{
+				crad.name="HH11";
+			}
+		}
+
+		if(crad.resName=="ASP")
+		{
+			if(crad.name=="OD1" || crad.name=="OD2")
+			{
+				crad.name="OD1";
+			}
+			else if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+		}
+
+		if(crad.resName=="ASN")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HD21" || crad.name=="HD22")
+			{
+				crad.name="HD21";
+			}
+		}
+
+		if(crad.resName=="CYS")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+		}
+
+		if(crad.resName=="GLU")
+		{
+			if(crad.name=="OE1" || crad.name=="OE2")
+			{
+				crad.name="OE1";
+			}
+			else if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+		}
+
+		if(crad.resName=="GLN")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+			else if(crad.name=="HE21" || crad.name=="HE22")
+			{
+				crad.name="HE21";
+			}
+		}
+
+		if(crad.resName=="GLY")
+		{
+			if(crad.name=="HA2" || crad.name=="HA3")
+			{
+				crad.name="HA2";
+			}
+		}
+
+		if(crad.resName=="HIS")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+		}
+
+		if(crad.resName=="ILE")
+		{
+			if(crad.name=="HG12" || crad.name=="HG13")
+			{
+				crad.name="HG12";
+			}
+			else if(crad.name=="HG21" || crad.name=="HG22" || crad.name=="HG23")
+			{
+				crad.name="HG21";
+			}
+			else if(crad.name=="HD11" || crad.name=="HD12" || crad.name=="HD13")
+			{
+				crad.name="HD11";
+			}
+		}
+
+		if(crad.resName=="LEU")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HD11" || crad.name=="HD12" || crad.name=="HD13")
+			{
+				crad.name="HD11";
+			}
+			else if(crad.name=="HD21" || crad.name=="HD22" || crad.name=="HD23")
+			{
+				crad.name="HD21";
+			}
+		}
+
+		if(crad.resName=="LYS")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+			else if(crad.name=="HD2" || crad.name=="HD3")
+			{
+				crad.name="HD2";
+			}
+			else if(crad.name=="HE2" || crad.name=="HE3")
+			{
+				crad.name="HE2";
+			}
+			else if(crad.name=="HZ1" || crad.name=="HZ2" || crad.name=="HZ3")
+			{
+				crad.name="HZ1";
+			}
+		}
+
+		if(crad.resName=="MET")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+			else if(crad.name=="HE1" || crad.name=="HE2" || crad.name=="HE3")
+			{
+				crad.name="HE1";
+			}
+		}
+
+		if(crad.resName=="PHE")
+		{
+			if(crad.name=="CD1" || crad.name=="CD2")
+			{
+				crad.name="CD1";
+			}
+			else if(crad.name=="CE1" || crad.name=="CE2")
+			{
+				crad.name="CE1";
+			}
+			else if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HD1" || crad.name=="HD2")
+			{
+				crad.name="HD1";
+			}
+			else if(crad.name=="HE1" || crad.name=="HE2")
+			{
+				crad.name="HE1";
+			}
+		}
+
+		if(crad.resName=="PRO")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HG2" || crad.name=="HG3")
+			{
+				crad.name="HG2";
+			}
+			else if(crad.name=="HD2" || crad.name=="HD3")
+			{
+				crad.name="HD2";
+			}
+		}
+
+		if(crad.resName=="SER")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+		}
+
+		if(crad.resName=="THR")
+		{
+			if(crad.name=="HG21" || crad.name=="HG22" || crad.name=="HG23")
+			{
+				crad.name="HG21";
+			}
+		}
+
+		if(crad.resName=="TRP")
+		{
+			if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+		}
+
+		if(crad.resName=="TYR")
+		{
+			if(crad.name=="CD1" || crad.name=="CD2")
+			{
+				crad.name="CD1";
+			}
+			else if(crad.name=="CE1" || crad.name=="CE2")
+			{
+				crad.name="CE1";
+			}
+			else if(crad.name=="HB2" || crad.name=="HB3")
+			{
+				crad.name="HB2";
+			}
+			else if(crad.name=="HD1" || crad.name=="HD2")
+			{
+				crad.name="HD1";
+			}
+			else if(crad.name=="HE1" || crad.name=="HE2")
+			{
+				crad.name="HE1";
+			}
+		}
+
+		if(crad.resName=="VAL")
+		{
+			if(crad.name=="HG11" || crad.name=="HG12" || crad.name=="HG13")
+			{
+				crad.name="HG11";
+			}
+			else if(crad.name=="HG21" || crad.name=="HG22" || crad.name=="HG23")
+			{
+				crad.name="HG21";
+			}
+		}
+
 		return crad;
 	}
 
