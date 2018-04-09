e511abd9fc26d0955a275b223a7fed482a15809a
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2018-04-09 14:21:54 +0300

    Refactored self-scripting.

--- src/mode_run_script.cpp
+++ src/mode_run_script.cpp
@@ -5,27 +5,6 @@
 namespace
 {
 
-void execute(common::CommandingManagerForAtomsAndContacts& manager, const std::string& command)
-{
-	if(command.empty())
-	{
-		return;
-	}
-
-	std::ostream& output=std::cout;
-	common::CommandingManagerForAtomsAndContacts::CommandOutputSink sink;
-
-	output << "\n> " << command << std::endl;
-	const common::CommandingManagerForAtomsAndContacts::CommandRecord record=manager.execute(command, sink);
-	output << sink.output_stream.str();
-	output << record.output_log;
-	if(!record.output_error.empty())
-	{
-		output << "Error: " << record.output_error << "\n";
-	}
-	output << std::endl;
-}
-
 void run_loop(std::istream& input)
 {
 	common::CommandingManagerForAtomsAndContacts manager;
@@ -51,7 +30,7 @@ void run_loop(std::istream& input)
 			}
 			if(!line.empty())
 			{
-				execute(manager, line);
+				manager.execute_verbosely(std::cout, line);
 			}
 		}
 	}
