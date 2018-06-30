3d7f267f7fa5bf25029478708342c6d3d1b2354e
kliment <kliment@localhost>
2018-06-30 19:28:47 +0300

    Started implementing script partitioning.

--- src/common/script_partitioning.h
+++ src/common/script_partitioning.h
@@ -1,21 +1,40 @@
-#ifndef COMMON_SCRIPTING_ALIASING_H_
-#define COMMON_SCRIPTING_ALIASING_H_
+#ifndef COMMON_SCRIPT_PARTITIONING_H_
+#define COMMON_SCRIPT_PARTITIONING_H_
 
 #include "command_input.h"
 
 namespace common
 {
 
-class ScriptingAliasing
+class ScriptPartitioning
 {
 public:
-	ScriptingAliasing()
+	struct Sentence
+	{
+		std::string body;
+		std::vector<std::string> parents;
+
+		Sentence()
+		{
+		}
+
+		explicit Sentence(const std::string& body) : body(body)
+		{
+		}
+	};
+
+	ScriptPartitioning()
 	{
 	}
 
 	const std::map<std::string, std::string>& aliases() const
 	{
 		return aliases_;
+	}
+
+	std::vector<Sentence> partition(const std::string& script)
+	{
+		return translate_sentences(split_script_into_sentences(script));
 	}
 
 	void set_alias(const std::string& name, const std::string& script_template)
@@ -43,27 +62,47 @@ public:
 		return (aliases_.erase(name)>0);
 	}
 
-	std::string translate_alias(const std::string& alias_str) const
+private:
+	static std::vector<Sentence> split_script_into_sentences(const std::string& script)
 	{
-		if(alias_str.empty())
+		std::vector<Sentence> sentences;
+
+		std::size_t p1=0;
+		while(p1<script.size())
+		{
+			p1=script.find_first_not_of(";\n \t", p1);
+			if(p1<script.size())
+			{
+				std::size_t p2=script.find_first_of(";\n", p1);
+				sentences.push_back(Sentence(script.substr(p1, p2-p1)));
+				p1=p2;
+			}
+		}
+
+		return sentences;
+	}
+
+	std::string translate_sentence_body_with_alias(const std::string& sentence_body) const
+	{
+		if(sentence_body.empty())
 		{
 			throw std::runtime_error(std::string("Command string is empty."));
 		}
 
-		std::istringstream input(alias_str);
+		std::istringstream input(sentence_body);
 		std::vector< std::pair<int, std::string> > tokens;
 		CommandInput::read_all_strings_considering_quotes_and_brackets(input, tokens);
 
 		if(tokens.empty())
 		{
-			throw std::runtime_error(std::string("Failed to read command string '")+alias_str+"'.");
+			throw std::runtime_error(std::string("Failed to read command string '")+sentence_body+"'.");
 		}
 
 		const std::string& alias_name=tokens[0].second;
 
 		if(aliases_.count(alias_name)==0)
 		{
-			return alias_str;
+			return sentence_body;
 		}
 
 		const std::string& script_template=aliases_.find(alias_name)->second;
@@ -101,10 +140,31 @@ public:
 		return result;
 	}
 
-private:
+	std::vector<Sentence> translate_sentences(const std::vector<Sentence>& input_sentences)
+	{
+		std::vector<Sentence> result;
+
+		for(std::size_t i=0;i<input_sentences.size();i++)
+		{
+			std::vector<Sentence> subsentences=split_script_into_sentences(translate_sentence_body_with_alias(input_sentences[i].body));
+			const bool used_alias=(subsentences.size()>1 || subsentences[0].body!=input_sentences[i].body);
+			for(std::size_t j=0;j<subsentences.size();j++)
+			{
+				subsentences[j].parents=input_sentences[i].parents;
+				if(used_alias)
+				{
+					subsentences[j].parents.push_back(input_sentences[i].body);
+				}
+			}
+			result.insert(result.end(), subsentences.begin(), subsentences.end());
+		}
+
+		return result;
+	}
+
 	std::map<std::string, std::string> aliases_;
 };
 
 }
 
-#endif /* COMMON_SCRIPTING_ALIASING_H_ */
+#endif /* COMMON_SCRIPT_PARTITIONING_H_ */
