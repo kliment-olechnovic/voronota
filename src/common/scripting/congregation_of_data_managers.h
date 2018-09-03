0c3cb6a1669a2aac659d05059193381846efd015
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2018-09-03 19:36:55 +0300

    Enhanced scripting.

--- src/common/scripting/congregation_of_data_managers.h
+++ src/common/scripting/congregation_of_data_managers.h
@@ -27,6 +27,11 @@ public:
 					data_manager_ptr(&data_manager)
 		{
 		}
+
+		DataManager& data_manager()
+		{
+			return (*data_manager_ptr);
+		}
 	};
 
 	CongregationOfDataManagers()
@@ -44,12 +49,20 @@ public:
 	void assert_objects_availability(const std::vector<std::string>& names)
 	{
 		ensure_unique_names();
-		const std::set<std::string> set_of_names(names.begin(), names.end());
+		std::map<std::string, bool> map_of_names;
+		for(std::size_t i=0;i<names.size();i++)
+		{
+			map_of_names[names[i]]=false;
+		}
 		for(std::list<WrapperForDataManager>::iterator it=all_data_managers_.begin();it!=all_data_managers_.end();++it)
 		{
-			if(set_of_names.count(it->name)==0)
+			map_of_names[it->name]=true;
+		}
+		for(std::map<std::string, bool>::const_iterator it=map_of_names.begin();it!=map_of_names.end();++it)
+		{
+			if(!it->second)
 			{
-				throw std::runtime_error(std::string("Invalid object name '")+(it->name)+"'.");
+				throw std::runtime_error(std::string("Invalid object name '")+(it->first)+"'.");
 			}
 		}
 	}
@@ -57,6 +70,14 @@ public:
 	void assert_object_availability(const std::string& name)
 	{
 		assert_objects_availability(std::vector<std::string>(1, name));
+	}
+
+	void assert_object_availability(DataManager* pointer)
+	{
+		if(get_iterator(pointer)==all_data_managers_.end())
+		{
+			throw std::runtime_error(std::string("Invalid object."));
+		}
 	}
 
 	std::vector<ObjectDescriptor> get_descriptors(const bool only_enabled)
@@ -73,44 +94,69 @@ public:
 		return result;
 	}
 
-	std::vector<ObjectDescriptor> get_descriptor(const std::string& name)
+	std::vector<ObjectDescriptor> get_descriptors(const std::vector<std::string>& names)
 	{
 		ensure_unique_names();
+		std::set<std::string> set_of_names(names.begin(), names.end());
 		std::vector<ObjectDescriptor> result;
 		for(std::list<WrapperForDataManager>::iterator it=all_data_managers_.begin();it!=all_data_managers_.end();++it)
 		{
-			if(name==it->name)
+			if(set_of_names.count(it->name)>0)
 			{
 				result.push_back(it->get_descriptor());
-				return result;
 			}
 		}
 		return result;
 	}
 
-	std::vector<ObjectDescriptor> add_object(const DataManager& data_manager)
+	std::vector<ObjectDescriptor> get_descriptor(const std::string& name)
+	{
+		return get_descriptors(std::vector<std::string>(1, name));
+	}
+
+	std::vector<ObjectDescriptor> get_descriptor(DataManager* pointer)
+	{
+		std::vector<ObjectDescriptor> result;
+		std::list<WrapperForDataManager>::iterator it=get_iterator(pointer);
+		if(it!=all_data_managers_.end())
+		{
+			ensure_unique_names();
+			result.push_back(it->get_descriptor());
+		}
+		return result;
+	}
+
+	ObjectDescriptor add_object(const DataManager& data_manager)
 	{
 		all_data_managers_.push_back(WrapperForDataManager(data_manager));
-		ensure_unique_names();
-		return std::vector<ObjectDescriptor>(1, all_data_managers_.back().get_descriptor());
+		return all_data_managers_.back().get_descriptor();
 	}
 
-	void delete_all_objects()
+	std::vector<ObjectDescriptor> delete_all_objects()
 	{
+		std::vector<ObjectDescriptor> result=get_descriptors(false);
 		all_data_managers_.clear();
+		return result;
 	}
 
-	void delete_object(const std::string& name)
+	std::vector<ObjectDescriptor> delete_object(const std::string& name)
 	{
-		ensure_unique_names();
-		for(std::list<WrapperForDataManager>::iterator it=all_data_managers_.begin();it!=all_data_managers_.end();++it)
+		std::vector<ObjectDescriptor> result=get_descriptor(name);
+		if(!result.empty())
 		{
-			if(name==it->name)
-			{
-				all_data_managers_.erase(it);
-				return;
-			}
+			all_data_managers_.erase(get_iterator(result[0].data_manager_ptr));
 		}
+		return result;
+	}
+
+	std::vector<ObjectDescriptor> delete_object(DataManager* pointer)
+	{
+		std::vector<ObjectDescriptor> result=get_descriptor(pointer);
+		if(!result.empty())
+		{
+			all_data_managers_.erase(get_iterator(result[0].data_manager_ptr));
+		}
+		return result;
 	}
 
 	void disable_all_objects()
@@ -121,18 +167,26 @@ public:
 		}
 	}
 
-	void enable_object(const std::string& name)
+	std::vector<ObjectDescriptor> enable_object(const std::string& name)
 	{
-		ensure_unique_names();
 		disable_all_objects();
-		for(std::list<WrapperForDataManager>::iterator it=all_data_managers_.begin();it!=all_data_managers_.end();++it)
+		std::vector<ObjectDescriptor> result=get_descriptor(name);
+		if(!result.empty())
 		{
-			if(name==it->name)
-			{
-				it->enabled=true;
-				return;
-			}
+			get_iterator(result[0].data_manager_ptr)->enabled=true;
 		}
+		return result;
+	}
+
+	std::vector<ObjectDescriptor> enable_object(DataManager* pointer)
+	{
+		disable_all_objects();
+		std::vector<ObjectDescriptor> result=get_descriptor(pointer);
+		if(!result.empty())
+		{
+			get_iterator(result[0].data_manager_ptr)->enabled=true;
+		}
+		return result;
 	}
 
 private:
@@ -177,6 +231,18 @@ private:
 				it->name=output.str();
 			}
 		}
+	}
+
+	std::list<WrapperForDataManager>::iterator get_iterator(DataManager* pointer)
+	{
+		for(std::list<WrapperForDataManager>::iterator it=all_data_managers_.begin();it!=all_data_managers_.end();++it)
+		{
+			if(pointer==&(it->data_manager))
+			{
+				return it;
+			}
+		}
+		return all_data_managers_.end();
 	}
 
 	std::list<WrapperForDataManager> all_data_managers_;
