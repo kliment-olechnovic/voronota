e6525da83d3e26240233d4694c54b392ef6a0000
kliment <kliment@localhost>
2018-09-04 14:23:16 +0300

    Enhanced scripting.

--- src/common/scripting/custom_commands_for_script_partitioner.h
+++ src/common/scripting/custom_commands_for_script_partitioner.h
@@ -1,6 +1,8 @@
 #ifndef COMMON_SCRIPTING_CUSTOM_COMMANDS_FOR_SCRIPT_PARTITIONER_H_
 #define COMMON_SCRIPTING_CUSTOM_COMMANDS_FOR_SCRIPT_PARTITIONER_H_
 
+#include <fstream>
+
 #include "generic_command_for_script_partitioner.h"
 
 namespace common
@@ -57,6 +59,43 @@ public:
 					cargs.output_for_log << "Unset alias '" << names[i] << "'\n";
 				}
 			}
+		}
+	};
+
+	class run_script : public GenericCommandForScriptPartitioner
+	{
+	protected:
+		void run(CommandArguments& cargs)
+		{
+			const std::vector<std::string>& files=cargs.input.get_list_of_unnamed_values();
+
+			if(files.empty())
+			{
+				throw std::runtime_error(std::string("No files provided."));
+			}
+
+			if(files.size()!=1)
+			{
+				throw std::runtime_error(std::string("Not exactly one file provided."));
+			}
+
+			const std::string file=files.front();
+
+			std::ifstream finput(file.c_str(), std::ios::in);
+			if(!finput.good())
+			{
+				throw std::runtime_error(std::string("Failed to read file '")+file+"'.");
+			}
+
+			std::istreambuf_iterator<char> eos;
+			std::string script(std::istreambuf_iterator<char>(finput), eos);
+
+			if(script.empty())
+			{
+				throw std::runtime_error(std::string("No script in file '")+file+"'.");
+			}
+
+			cargs.script_partitioner.add_pending_sentences_from_string_to_front(script);
 		}
 	};
 };
