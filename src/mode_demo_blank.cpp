c6335a564dda376a95b557495b60c2c3d00510e0
kliment <kliment@localhost>
2018-11-02 13:19:00 +0200

    Added blank demo mode.

---
+++ src/mode_demo_blank.cpp
@@ -0,0 +1,11 @@
+#include "auxiliaries/program_options_handler.h"
+
+void demo_blank(const auxiliaries::ProgramOptionsHandler& poh)
+{
+	auxiliaries::ProgramOptionsHandlerWrapper pohw(poh);
+	pohw.describe_io("stdin", true, false, "nothing");
+	pohw.describe_io("stdout", false, true, "output");
+
+	std::cout << std::endl;
+}
+
