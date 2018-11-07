383b74c91a92193ac057d57d07671e160fc7c1e7
kliment <kliment@localhost>
2018-11-07 11:48:04 +0200

    Redesigned file writing in commands for data manager.

--- src/common/scripting/basic_assertions.h
+++ src/common/scripting/basic_assertions.h
@@ -20,11 +20,11 @@ inline void assert_selection_name_input(const std::string& name, const bool allo
 	}
 	else if(name.find_first_of(" {}()[]<>,;.:\\/+*/='\"@#$%^&`~?|")!=std::string::npos)
 	{
-		throw std::runtime_error(std::string("Selection name contains invalid symbols."));
+		throw std::runtime_error(std::string("Selection name '")+name+"' contains invalid symbols.");
 	}
 	else if(name.rfind("-", 0)==0 || name.rfind("_", 0)==0)
 	{
-		throw std::runtime_error(std::string("Selection name starts with invalid symbol."));
+		throw std::runtime_error(std::string("Selection name '")+name+"' starts with invalid symbol.");
 	}
 }
 
@@ -39,11 +39,11 @@ inline void assert_tag_input(const std::string& tag, const bool allow_empty)
 	}
 	else if(tag.find_first_of(" {}()[]<>,;.:\\/+*/'\"@#$%^&`~?|")!=std::string::npos)
 	{
-		throw std::runtime_error(std::string("Tag contains invalid symbols."));
+		throw std::runtime_error(std::string("Tag '")+tag+"' contains invalid symbols.");
 	}
 	else if(tag.rfind("-", 0)==0)
 	{
-		throw std::runtime_error(std::string("Tag starts with invalid symbol."));
+		throw std::runtime_error(std::string("Tag '")+tag+"' starts with invalid symbol.");
 	}
 }
 
@@ -58,11 +58,42 @@ inline void assert_adjunct_name_input(const std::string& name, const bool allow_
 	}
 	else if(name.find_first_of(" {}()[]<>,;.:\\/+*/'\"@#$%^&`~?|=")!=std::string::npos)
 	{
-		throw std::runtime_error(std::string("Adjunct name contains invalid symbols."));
+		throw std::runtime_error(std::string("Adjunct name '")+name+"' contains invalid symbols.");
 	}
 	else if(name.rfind("-", 0)==0)
 	{
-		throw std::runtime_error(std::string("Adjunct name starts with invalid symbol."));
+		throw std::runtime_error(std::string("Adjunct name '")+name+"' starts with invalid symbol.");
+	}
+}
+
+inline void assert_file_name_input(const std::string& name, const bool allow_empty)
+{
+	if(name.empty())
+	{
+		if(!allow_empty)
+		{
+			throw std::runtime_error(std::string("File name is empty."));
+		}
+	}
+	else if(name.find_first_of("?*$'\";:<>,|")!=std::string::npos)
+	{
+		throw std::runtime_error(std::string("File name '")+name+"' contains invalid symbols.");
+	}
+}
+
+inline void assert_io_stream(const std::string& name, const std::ostream& output)
+{
+	if(!output.good())
+	{
+		throw std::runtime_error(std::string("Failed to use output stream '")+name+"'");
+	}
+}
+
+inline void assert_io_stream(const std::string& name, const std::istream& input)
+{
+	if(!input.good())
+	{
+		throw std::runtime_error(std::string("Failed to use input stream '")+name+"'");
 	}
 }
 
