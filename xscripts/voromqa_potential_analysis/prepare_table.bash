786240755bdc3908b44fc7ac5acbbd1c3a785b04
kliment <kliment@localhost>
2019-02-07 18:31:34 +0200

    Added VoroMQA potential analysis script.

---
+++ xscripts/voromqa_potential_analysis/prepare_table.bash
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+readonly TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+{
+	echo "atom1 atom2 value_central"
+	{
+		cat ../../resources/voromqa_v1_energy_potential | awk '{if($3=="central_sep2"){print $1 " " $2 " " $4}}'
+		cat ../../resources/voromqa_v1_energy_potential | awk '{if($3=="central_sep2"){print $2 " " $1 " " $4}}'
+	} | sort | uniq
+} > "$TMPLDIR/table_central"
+
+{
+	echo "atom1 atom2 value_noncentral"
+	{
+		cat ../../resources/voromqa_v1_energy_potential | awk '{if($3=="sep2"){print $1 " " $2 " " $4}}'
+		cat ../../resources/voromqa_v1_energy_potential | awk '{if($3=="sep2"){print $2 " " $1 " " $4}}'
+	} | sort | uniq
+} > "$TMPLDIR/table_noncentral"
+
+{
+	echo "atom1 value_solvent1"
+	cat ../../resources/voromqa_v1_energy_potential | grep 'solvent' | awk '{print $1 " " $4}'
+} > "$TMPLDIR/table_solvent1"
+
+{
+	echo "atom2 value_solvent2"
+	cat ../../resources/voromqa_v1_energy_potential | grep 'solvent' | awk '{print $1 " " $4}'
+} > "$TMPLDIR/table_solvent2"
+
+{
+	echo "atom1 atom2 area_central"
+	{
+		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="central_sep2"){print $1 " " $2 " " $4}}'
+		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="central_sep2"){print $2 " " $1 " " $4}}'
+	} | sort | uniq
+} > "$TMPLDIR/table_area_central"
+
+{
+	echo "atom1 atom2 area_noncentral"
+	{
+		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="sep2"){print $1 " " $2 " " $4}}'
+		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="sep2"){print $2 " " $1 " " $4}}'
+	} | sort | uniq
+} > "$TMPLDIR/table_area_noncentral"
+
+{
+	echo "atom1 area_solvent1"
+	cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
+} > "$TMPLDIR/table_area_solvent1"
+
+{
+	echo "atom2 area_solvent2"
+	cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
+} > "$TMPLDIR/table_area_solvent2"
+
+cd "$TMPLDIR"
+
+R --vanilla << 'EOF'
+table_central=read.table("table_central", header=TRUE, stringsAsFactors=FALSE);
+table_noncentral=read.table("table_noncentral", header=TRUE, stringsAsFactors=FALSE);
+table_solvent1=read.table("table_solvent1", header=TRUE, stringsAsFactors=FALSE);
+table_solvent2=read.table("table_solvent2", header=TRUE, stringsAsFactors=FALSE);
+table_area_central=read.table("table_area_central", header=TRUE, stringsAsFactors=FALSE);
+table_area_noncentral=read.table("table_area_noncentral", header=TRUE, stringsAsFactors=FALSE);
+table_area_solvent1=read.table("table_area_solvent1", header=TRUE, stringsAsFactors=FALSE);
+table_area_solvent2=read.table("table_area_solvent2", header=TRUE, stringsAsFactors=FALSE);
+result=merge(table_central, table_noncentral);
+result=merge(result, merge(table_solvent1, table_solvent2));
+result=merge(result, merge(table_area_central, table_area_noncentral));
+result=merge(result, merge(table_area_solvent1, table_area_solvent2));
+write.table(result, "result", quote=FALSE, col.names=TRUE, row.names=FALSE);
+EOF
+
+cd - &> /dev/null
+
+cat "$TMPLDIR/result" | column -t > ./table
