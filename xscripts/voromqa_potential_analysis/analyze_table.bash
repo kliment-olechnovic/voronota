483ccb47c243d239c8f7d3cd86e7f289af28e3b2
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2019-02-08 15:08:00 +0200

    Improved VoroMQA potential analysis script.

--- xscripts/voromqa_potential_analysis/analyze_table.bash
+++ xscripts/voromqa_potential_analysis/analyze_table.bash
@@ -10,9 +10,6 @@ t$area_c_nc=(t$area_central+t$area_noncentral);
 ut=t[!duplicated(t[,"atom1"]),];
 ut=data.frame(atom=ut$atom1, value_solvent=ut$value_solvent1, area_solvent=ut$area_solvent1);
 
-ut$value_c_nc=rep(0, nrow(ut));
-ut$area_c_nc=rep(0, nrow(ut));
-
 sel_atom2_carbon=grep("A<C", t$atom2);
 sel_atom2_sidechain=grep("A<C>|A<CA>|A<N>|A<O>", t$atom2, invert=TRUE);
 sel_atom2_carbon_sidechain=intersect(sel_atom2_carbon, sel_atom2_sidechain);
@@ -42,6 +39,9 @@ for(i in 1:nrow(ut))
 	ut$area_nonsolvent_hydrophobic[i]=sum(t$area_c_nc[sel]);
 	ut$value_nonsolvent_hydrophobic[i]=sum(t$value_c_nc[sel]*t$area_c_nc[sel])/sum(t$area_c_nc[sel]);
 }
+
+ut=ut[order(0-abs(ut$value_solvent-ut$value_nonsolvent_carbon_sidechain)),];
+write.table(ut[,grep("area_", colnames(ut), invert=TRUE)], "solvent_vs_nonsolvent", quote=FALSE, col.names=TRUE, row.names=FALSE);
 
 ####################
 
@@ -125,4 +125,6 @@ lines(c(-100, 100), c(-100, 100));
 ####################
 
 EOF
+
+cat "solvent_vs_nonsolvent" | column -t | sponge "solvent_vs_nonsolvent"
 
