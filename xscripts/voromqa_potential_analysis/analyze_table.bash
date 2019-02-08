e9fcda817f1cd418fcc111f7e7234d4d28f8efea
Kliment Olechnovič <54395582+kliment-olechnovic@users.noreply.github.com>
2019-02-08 11:54:59 +0200

    Added VoroMQA potential analysis script.

--- xscripts/voromqa_potential_analysis/analyze_table.bash
+++ xscripts/voromqa_potential_analysis/analyze_table.bash
@@ -16,6 +16,7 @@ ut$area_c_nc=rep(0, nrow(ut));
 sel_atom2_carbon=grep("A<C", t$atom2);
 sel_atom2_sidechain=grep("A<C>|A<CA>|A<N>|A<O>", t$atom2, invert=TRUE);
 sel_atom2_carbon_sidechain=intersect(sel_atom2_carbon, sel_atom2_sidechain);
+sel_atom2_hydrophobic=intersect(sel_atom2_carbon_sidechain, grep("R<ILE>|R<LEU>", t$atom2));
 
 for(i in 1:nrow(ut))
 {
@@ -36,23 +37,41 @@ for(i in 1:nrow(ut))
 	sel=intersect(sel_atom1_relevant, sel_atom2_carbon_sidechain);
 	ut$area_nonsolvent_carbon_sidechain[i]=sum(t$area_c_nc[sel]);
 	ut$value_nonsolvent_carbon_sidechain[i]=sum(t$value_c_nc[sel]*t$area_c_nc[sel])/sum(t$area_c_nc[sel]);
+	
+	sel=intersect(sel_atom1_relevant, sel_atom2_hydrophobic);
+	ut$area_nonsolvent_hydrophobic[i]=sum(t$area_c_nc[sel]);
+	ut$value_nonsolvent_hydrophobic[i]=sum(t$value_c_nc[sel]*t$area_c_nc[sel])/sum(t$area_c_nc[sel]);
 }
 
 ####################
 
 sel=grep("A<C>|A<CA>|A<N>|A<O>", ut$atom);
+sel_gly=intersect(sel, grep("R<GLY>", ut$atom));
 
 plot(ut$value_solvent, ut$value_nonsolvent, main="solvent vs nonsolvent");
 points(ut$value_solvent[sel], ut$value_nonsolvent[sel], col="red", pch=3);
+points(ut$value_solvent[sel_gly], ut$value_nonsolvent[sel_gly], col="green", pch=4);
+lines(c(-100, 100), c(100, -100));
 
 plot(ut$value_solvent, ut$value_nonsolvent_carbon, main="solvent vs nonsolvent_carbon");
 points(ut$value_solvent[sel], ut$value_nonsolvent_carbon[sel], col="red", pch=3);
+points(ut$value_solvent[sel_gly], ut$value_nonsolvent_carbon[sel_gly], col="green", pch=4);
+lines(c(-100, 100), c(100, -100));
 
 plot(ut$value_solvent, ut$value_nonsolvent_sidechain, main="solvent vs nonsolvent_sidechain");
 points(ut$value_solvent[sel], ut$value_nonsolvent_sidechain[sel], col="red", pch=3);
+points(ut$value_solvent[sel_gly], ut$value_nonsolvent_sidechain[sel_gly], col="green", pch=4);
+lines(c(-100, 100), c(100, -100));
 
 plot(ut$value_solvent, ut$value_nonsolvent_carbon_sidechain, main="solvent vs nonsolvent_carbon_sidechain");
 points(ut$value_solvent[sel], ut$value_nonsolvent_carbon_sidechain[sel], col="red", pch=3);
+points(ut$value_solvent[sel_gly], ut$value_nonsolvent_carbon_sidechain[sel_gly], col="green", pch=4);
+lines(c(-100, 100), c(100, -100));
+
+plot(ut$value_solvent, ut$value_nonsolvent_hydrophobic, main="solvent vs nonsolvent_hydrophobic");
+points(ut$value_solvent[sel], ut$value_nonsolvent_hydrophobic[sel], col="red", pch=3);
+points(ut$value_solvent[sel_gly], ut$value_nonsolvent_hydrophobic[sel_gly], col="green", pch=4);
+lines(c(-100, 100), c(100, -100));
 
 ####################
 
@@ -70,6 +89,10 @@ plot(ut$value_nonsolvent, ut$value_nonsolvent_carbon_sidechain, main="nonsolvent
 points(ut$value_nonsolvent[sel], ut$value_nonsolvent_carbon_sidechain[sel], col="red", pch=3);
 lines(c(-100, 100), c(-100, 100));
 
+plot(ut$value_nonsolvent, ut$value_nonsolvent_hydrophobic, main="nonsolvent vs nonsolvent_hydrophobic");
+points(ut$value_nonsolvent[sel], ut$value_nonsolvent_hydrophobic[sel], col="red", pch=3);
+lines(c(-100, 100), c(-100, 100));
+
 plot(ut$value_nonsolvent_carbon, ut$value_nonsolvent_sidechain, main="nonsolvent_carbon vs nonsolvent_sidechain");
 points(ut$value_nonsolvent_carbon[sel], ut$value_nonsolvent_sidechain[sel], col="red", pch=3);
 lines(c(-100, 100), c(-100, 100));
@@ -78,9 +101,23 @@ plot(ut$value_nonsolvent_carbon, ut$value_nonsolvent_carbon_sidechain, main="non
 points(ut$value_nonsolvent_carbon[sel], ut$value_nonsolvent_carbon_sidechain[sel], col="red", pch=3);
 lines(c(-100, 100), c(-100, 100));
 
+plot(ut$value_nonsolvent_carbon, ut$value_nonsolvent_hydrophobic, main="nonsolvent_carbon vs nonsolvent_hydrophobic");
+points(ut$value_nonsolvent_carbon[sel], ut$value_nonsolvent_hydrophobic[sel], col="red", pch=3);
+lines(c(-100, 100), c(-100, 100));
+
 plot(ut$value_nonsolvent_sidechain, ut$value_nonsolvent_carbon_sidechain, main="nonsolvent_sidechain vs nonsolvent_carbon_sidechain");
 points(ut$value_nonsolvent_sidechain[sel], ut$value_nonsolvent_carbon_sidechain[sel], col="red", pch=3);
 lines(c(-100, 100), c(-100, 100));
+
+plot(ut$value_nonsolvent_sidechain, ut$value_nonsolvent_hydrophobic, main="nonsolvent_sidechain vs nonsolvent_hydrophobic");
+points(ut$value_nonsolvent_sidechain[sel], ut$value_nonsolvent_hydrophobic[sel], col="red", pch=3);
+lines(c(-100, 100), c(-100, 100));
+
+plot(ut$value_nonsolvent_carbon_sidechain, ut$value_nonsolvent_hydrophobic, main="nonsolvent_carbon_sidechain vs nonsolvent_hydrophobic");
+points(ut$value_nonsolvent_carbon_sidechain[sel], ut$value_nonsolvent_hydrophobic[sel], col="red", pch=3);
+lines(c(-100, 100), c(-100, 100));
+
+####################
 
 EOF
 
