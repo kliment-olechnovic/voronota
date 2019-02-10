7786e459f4797ee1b5b03ff04f06d5373fd5ba37
kliment <kliment@localhost>
2019-02-10 17:55:50 +0200

    Improved VoroMQA potential analysis scripts.

--- xscripts/voromqa_potential_analysis/prepare_table.bash
+++ xscripts/voromqa_potential_analysis/prepare_table.bash
@@ -6,26 +6,15 @@ trap "rm -r $TMPLDIR" EXIT
 cp ../../resources/voromqa_v1_energy_potential ./potential
 
 cat ../../resources/voromqa_v1_energy_potential_source/contact_areas \
+| sed 's/central_//g' \
+| ../../voronota score-contacts-potential \
+> ./contact_areas
+
+cat ./contact_areas \
 | ../../voronota score-contacts-potential \
   --probabilities-file ./probabilities \
   --potential-file ./potential_raw \
 > /dev/null
-
-{
-	echo "atom1 atom2 value_central"
-	{
-		cat ./potential_raw | awk '{if($3=="central_sep2"){print $1 " " $2 " " $4}}'
-		cat ./potential_raw | awk '{if($3=="central_sep2"){print $2 " " $1 " " $4}}'
-	} | sort | uniq
-} > "$TMPLDIR/table_central"
-
-{
-	echo "atom1 atom2 value_noncentral"
-	{
-		cat ./potential_raw | awk '{if($3=="sep2"){print $1 " " $2 " " $4}}'
-		cat ./potential_raw | awk '{if($3=="sep2"){print $2 " " $1 " " $4}}'
-	} | sort | uniq
-} > "$TMPLDIR/table_noncentral"
 
 {
 	echo "atom1 value_solvent1"
@@ -38,65 +27,43 @@ cat ../../resources/voromqa_v1_energy_potential_source/contact_areas \
 } > "$TMPLDIR/table_solvent2"
 
 {
-	echo "atom1 atom2 area_central"
+	echo "atom1 atom2 area"
 	{
-		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="central_sep2"){print $1 " " $2 " " $4}}'
-		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="central_sep2"){print $2 " " $1 " " $4}}'
+		cat ./contact_areas | awk '{if($3=="sep2"){print $1 " " $2 " " $4}}'
+		cat ./contact_areas | awk '{if($3=="sep2"){print $2 " " $1 " " $4}}'
 	} | sort | uniq
-} > "$TMPLDIR/table_area_central"
-
-{
-	echo "atom1 atom2 area_noncentral"
-	{
-		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="sep2"){print $1 " " $2 " " $4}}'
-		cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | awk '{if($3=="sep2"){print $2 " " $1 " " $4}}'
-	} | sort | uniq
-} > "$TMPLDIR/table_area_noncentral"
+} > "$TMPLDIR/table_area"
 
 {
 	echo "atom1 area_solvent1"
-	cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
+	cat ./contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
 } > "$TMPLDIR/table_area_solvent1"
 
 {
 	echo "atom2 area_solvent2"
-	cat ../../resources/voromqa_v1_energy_potential_source/contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
+	cat ./contact_areas | grep 'solvent' | awk '{print $1 " " $4}'
 } > "$TMPLDIR/table_area_solvent2"
 
 {
-	echo "atom1 atom2 prob_obs_central prob_exp_central"
-	{
-		cat ./probabilities | awk '{if($3=="central_sep2"){print $1 " " $2 " " $4 " " $5}}'
-		cat ./probabilities | awk '{if($3=="central_sep2"){print $2 " " $1 " " $4 " " $5}}'
-	} | sort | uniq
-} > "$TMPLDIR/table_prob_central"
-
-{
-	echo "atom1 atom2 prob_obs_noncentral prob_exp_noncentral"
+	echo "atom1 atom2 prob_obs prob_exp"
 	{
 		cat ./probabilities | awk '{if($3=="sep2"){print $1 " " $2 " " $4 " " $5}}'
 		cat ./probabilities | awk '{if($3=="sep2"){print $2 " " $1 " " $4 " " $5}}'
 	} | sort | uniq
-} > "$TMPLDIR/table_prob_noncentral"
+} > "$TMPLDIR/table_prob"
 
 cd "$TMPLDIR"
 
 R --vanilla << 'EOF'
-table_central=read.table("table_central", header=TRUE, stringsAsFactors=FALSE);
-table_noncentral=read.table("table_noncentral", header=TRUE, stringsAsFactors=FALSE);
+table_prob=read.table("table_prob", header=TRUE, stringsAsFactors=FALSE);
+table_area=read.table("table_area", header=TRUE, stringsAsFactors=FALSE);
 table_solvent1=read.table("table_solvent1", header=TRUE, stringsAsFactors=FALSE);
 table_solvent2=read.table("table_solvent2", header=TRUE, stringsAsFactors=FALSE);
-table_area_central=read.table("table_area_central", header=TRUE, stringsAsFactors=FALSE);
-table_area_noncentral=read.table("table_area_noncentral", header=TRUE, stringsAsFactors=FALSE);
 table_area_solvent1=read.table("table_area_solvent1", header=TRUE, stringsAsFactors=FALSE);
 table_area_solvent2=read.table("table_area_solvent2", header=TRUE, stringsAsFactors=FALSE);
-table_prob_central=read.table("table_prob_central", header=TRUE, stringsAsFactors=FALSE);
-table_prob_noncentral=read.table("table_prob_noncentral", header=TRUE, stringsAsFactors=FALSE);
-result=merge(table_central, table_noncentral);
+result=merge(table_prob, table_area);
 result=merge(result, merge(table_solvent1, table_solvent2));
-result=merge(result, merge(table_area_central, table_area_noncentral));
 result=merge(result, merge(table_area_solvent1, table_area_solvent2));
-result=merge(result, merge(table_prob_central, table_prob_noncentral));
 write.table(result, "result", quote=FALSE, col.names=TRUE, row.names=FALSE);
 EOF
 
