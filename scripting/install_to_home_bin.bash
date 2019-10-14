bc411c00b87424684a89c9cb311ad67a8d2443d6
kliment-olechnovic <kliment.olechnovic@gmail.com>
2019-10-14 10:47:04 +0300

    Reorganized bash-based scripting scripts.

---
+++ scripting/install_to_home_bin.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+cp ./voronota-scripting-env ~/bin/voronota-scripting-env
+cp ./voronota-scripting-interactive-env ~/bin/voronota-scripting-interactive-env
+cp ./v-tmalign ~/bin/v-tmalign
+cp ./v-scwrl ~/bin/v-scwrl
+
+cp ../voronota ~/bin/voronota
+cp ../voronota-resources ~/bin/voronota-resources
+cp ../viewer/voronota-viewer ~/bin/voronota-viewer
