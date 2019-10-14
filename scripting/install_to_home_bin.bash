56d8cea0a1e15388f141babbe02f366fdce68657
kliment-olechnovic <kliment.olechnovic@gmail.com>
2019-10-14 15:03:04 +0300

    Added fetching routine for bash-based scripting.

--- scripting/install_to_home_bin.bash
+++ scripting/install_to_home_bin.bash
@@ -6,6 +6,7 @@ cp ./voronota-scripting-env ~/bin/voronota-scripting-env
 cp ./voronota-scripting-interactive-env ~/bin/voronota-scripting-interactive-env
 cp ./v-tmalign ~/bin/v-tmalign
 cp ./v-scwrl ~/bin/v-scwrl
+cp ./v-fetch ~/bin/v-fetch
 
 cp ../voronota ~/bin/voronota
 cp ../voronota-resources ~/bin/voronota-resources
