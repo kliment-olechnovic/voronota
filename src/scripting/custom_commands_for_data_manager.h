c1ba76a7cc2c3a8f9462075d36795016bbcd1819
kliment-olechnovic <kliment.olechnovic@gmail.com>
2019-10-30 16:18:19 +0200

    Added scripting command for atom adjuncts residue pooling.

--- src/scripting/custom_commands_for_data_manager.h
+++ src/scripting/custom_commands_for_data_manager.h
@@ -1,6 +1,10 @@
 #ifndef SCRIPTING_CUSTOM_COMMANDS_FOR_DATA_MANAGER_H_
 #define SCRIPTING_CUSTOM_COMMANDS_FOR_DATA_MANAGER_H_
 
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
 #include "../auxiliaries/color_utilities.h"
 #include "../auxiliaries/residue_letters_coding.h"
 
@@ -658,6 +662,125 @@ public:
 
 			VariantSerialization::write(SummaryOfContacts(cargs.data_manager.contacts(), contacts_ids), cargs.heterostorage.variant_object.object("contacts_summary"));
 			VariantSerialization::write(SummaryOfAtoms(cargs.data_manager.atoms(), atom_ids), cargs.heterostorage.variant_object.object("atoms_summary"));
+		}
+	};
+
+	class set_adjunct_of_atoms_by_residue_pooling : public GenericCommandForDataManager
+	{
+	public:
+		bool allowed_to_work_on_multiple_data_managers(const CommandInput&) const
+		{
+			return true;
+		}
+
+	protected:
+		void run(CommandArguments& cargs)
+		{
+			cargs.data_manager.assert_atoms_availability();
+
+			const SelectionManager::Query parameters_for_selecting_source_atoms=read_generic_selecting_query("source-atoms-", "[]", cargs.input);
+			const SelectionManager::Query parameters_for_selecting_destination_atoms=read_generic_selecting_query("destination-atoms-", "[]", cargs.input);
+			const std::string source_name=cargs.input.get_value<std::string>("source-name");
+			const std::string destination_name=cargs.input.get_value<std::string>("destination-name");
+			const std::string pooling_mode=cargs.input.get_value_or_default<std::string>("pooling-mode", "mean");
+
+			cargs.input.assert_nothing_unusable();
+
+			assert_adjunct_name_input(source_name, false);
+			assert_adjunct_name_input(destination_name, false);
+
+			if(pooling_mode!="mean" && pooling_mode!="sum" && pooling_mode!="product" && pooling_mode!="min" && pooling_mode!="max")
+			{
+				throw std::runtime_error(std::string("Invalid pooling mode, valid options are: 'mean', 'sum', 'product', 'min', 'max'."));
+			}
+
+			const std::set<std::size_t> source_atom_ids=cargs.data_manager.selection_manager().select_atoms(parameters_for_selecting_source_atoms);
+			if(source_atom_ids.empty())
+			{
+				throw std::runtime_error(std::string("No source atoms selected."));
+			}
+
+			const std::set<std::size_t> destination_atom_ids=cargs.data_manager.selection_manager().select_atoms(parameters_for_selecting_destination_atoms);
+			if(destination_atom_ids.empty())
+			{
+				throw std::runtime_error(std::string("No destination atoms selected."));
+			}
+
+			std::map< std::size_t, std::vector<double> > residue_source_values;
+			std::set<std::size_t> used_source_atom_ids;
+
+			for(std::set<std::size_t>::const_iterator it=destination_atom_ids.begin();it!=destination_atom_ids.end();++it)
+			{
+				const std::size_t residue_id=cargs.data_manager.primary_structure_info().map_of_atoms_to_residues[*it];
+				if(residue_source_values.count(residue_id)==0)
+				{
+					const std::vector<std::size_t>& residue_atom_ids=cargs.data_manager.primary_structure_info().residues[residue_id].atom_ids;
+					for(std::size_t i=0;i<residue_atom_ids.size();i++)
+					{
+						const std::size_t atom_id=residue_atom_ids[i];
+						if(source_atom_ids.count(atom_id)>0)
+						{
+							const Atom& atom=cargs.data_manager.atoms()[atom_id];
+							std::map<std::string, double>::const_iterator jt=atom.value.props.adjuncts.find(source_name);
+							if(jt!=atom.value.props.adjuncts.end())
+							{
+								residue_source_values[residue_id].push_back(jt->second);
+								used_source_atom_ids.insert(atom_id);
+							}
+						}
+					}
+				}
+			}
+
+			std::map<std::size_t, double> residue_pooled_values;
+			for(std::map< std::size_t, std::vector<double> >::const_iterator it=residue_source_values.begin();it!=residue_source_values.end();++it)
+			{
+				const std::size_t residue_id=it->first;
+				const std::vector<double>& values=it->second;
+				if(!values.empty())
+				{
+					if(pooling_mode=="mean")
+					{
+						residue_pooled_values[residue_id]=std::accumulate(values.begin(), values.end(), 0.0)/values.size();
+					}
+					else if(pooling_mode=="sum")
+					{
+						residue_pooled_values[residue_id]=std::accumulate(values.begin(), values.end(), 0.0);
+					}
+					else if(pooling_mode=="product")
+					{
+						residue_pooled_values[residue_id]=std::accumulate(values.begin(), values.end(), 1.0, std::multiplies<double>());
+					}
+					else if(pooling_mode=="min")
+					{
+						residue_pooled_values[residue_id]=(*std::min_element(values.begin(), values.end()));
+					}
+					else if(pooling_mode=="max")
+					{
+						residue_pooled_values[residue_id]=(*std::max_element(values.begin(), values.end()));
+					}
+					else
+					{
+						throw std::runtime_error(std::string("Invalid pooling mode."));
+					}
+				}
+			}
+
+			cargs.change_indicator.changed_atoms_adjuncts=true;
+
+			for(std::set<std::size_t>::const_iterator it=destination_atom_ids.begin();it!=destination_atom_ids.end();++it)
+			{
+				Atom& atom=cargs.data_manager.atoms_mutable()[*it];
+				atom.value.props.adjuncts.erase(destination_name);
+				const std::size_t residue_id=cargs.data_manager.primary_structure_info().map_of_atoms_to_residues[*it];
+				if(residue_pooled_values.count(residue_id)>0)
+				{
+					atom.value.props.adjuncts[destination_name]=residue_pooled_values[residue_id];
+				}
+			}
+
+			VariantSerialization::write(SummaryOfAtoms(cargs.data_manager.atoms(), used_source_atom_ids), cargs.heterostorage.variant_object.object("used_source_atoms_summary"));
+			VariantSerialization::write(SummaryOfAtoms(cargs.data_manager.atoms(), destination_atom_ids), cargs.heterostorage.variant_object.object("destination_atoms_summary"));
 		}
 	};
 
