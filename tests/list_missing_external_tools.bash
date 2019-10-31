6f609a322ddfe97ae1b806a6bdc9573681c01098
kliment-olechnovic <kliment.olechnovic@gmail.com>
2019-10-31 17:55:06 +0200

    Simplified global testing pipeline.

--- tests/list_missing_external_tools.bash
+++ tests/list_missing_external_tools.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TOOLCOMMAND in g++ cppcheck mpic++ mpirun cmake dssp hbplus Scwrl4 valgrind
+for TOOLCOMMAND in g++ mpic++ mpirun cmake dssp hbplus Scwrl4 valgrind
 do
 	command -v $TOOLCOMMAND &> /dev/null || { echo "'$TOOLCOMMAND' executable not in binaries path"; }
 done
--- tests/run_all_tests.bash
+++ tests/run_all_tests.bash
@@ -4,7 +4,6 @@ cd $(dirname "$0")
 
 echo "External tools missing:             " $(./list_missing_external_tools.bash 2>&1 | wc -l)
 
-echo "Static code analysis report lines:  " $(./check_cpp_code_statically.bash 2>&1 | wc -l)
-
 ./run_all_jobs_scripts.bash
 echo "Altered jobs tests output files:    " $(git status -s ./jobs_output/ 2>&1 | wc -l)
+
