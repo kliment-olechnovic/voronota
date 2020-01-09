fa6b5055e00d7370da309b8d7165b7c2fe33f701
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-01-09 10:58:57 +0200

    Updated swig test.

--- swig/python/test.py
+++ swig/python/test.py
@@ -4,7 +4,7 @@ import json
 import voronota_scripting
 
 sc=voronota_scripting.ScriptingController()
-result=sc.run("load structure.pdb")
+result=sc.run("import structure.pdb")
 print(result)
 
 decoded_result=json.loads(result)
