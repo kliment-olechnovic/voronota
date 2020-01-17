7dfecf29bf6b35cd073789a17c3da56d5314b5f2
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-01-17 14:33:53 +0200

    Added file IO functions in duktaper.

--- duktaper/src/duktaper/duktape_wrapper.h
+++ duktaper/src/duktaper/duktape_wrapper.h
@@ -116,6 +116,28 @@ public:
 				"    raw_print(obj);"
 				"  }"
 				"}"
+				"\n"
+				"fprint=function(filename, obj, json_spacing)"
+				"{"
+				"  var obj_type=Object.prototype.toString.call(obj);"
+				"  if(obj_type==='[object Object]')"
+				"  {"
+				"    var spaces=0;"
+				"    if(json_spacing)"
+				"    {"
+				"      spaces=json_spacing;"
+				"    }"
+				"    raw_fprint(filename, JSON.stringify(obj, null, spaces));"
+				"  }"
+				"  else"
+				"  {"
+				"    raw_print(obj);"
+				"  }"
+				"}"
+				"\n"
+				"fread=raw_fread;"
+				"\n"
+				"shell=raw_shell;"
 				"\n";
 
 		const std::string namespace_name="voronota";
@@ -219,12 +241,66 @@ private:
 		return 0;
 	}
 
+	static duk_ret_t native_raw_fprint(duk_context *ctx)
+	{
+		const std::string filename=duk_require_string(ctx, -2);
+		const std::string content=duk_safe_to_string(ctx, -1);
+
+		if(filename.empty())
+		{
+			duk_push_string(ctx, "Missing file name");
+			return duk_throw(ctx);
+		}
+
+		std::ofstream output(filename.c_str(), std::ios::out);
+
+		if(!output.good())
+		{
+			std::string error_message=std::string("Failed to write file '")+filename+"'";
+			duk_push_string(ctx, error_message.c_str());
+			return duk_throw(ctx);
+		}
+
+		output << content;
+
+		return 0;
+	}
+
+	static duk_ret_t native_raw_fread(duk_context *ctx)
+	{
+		const std::string filename=duk_require_string(ctx, -1);
+
+		if(filename.empty())
+		{
+			duk_push_string(ctx, "Missing file name");
+			return duk_throw(ctx);
+		}
+
+		std::ifstream input(filename.c_str(), std::ios::in);
+
+		if(!input.good())
+		{
+			std::string error_message=std::string("Failed to read file '")+filename+"'";
+			duk_push_string(ctx, error_message.c_str());
+			return duk_throw(ctx);
+		}
+
+		std::istreambuf_iterator<char> eos;
+		std::string output(std::istreambuf_iterator<char>(input), eos);
+
+		duk_push_string(ctx, output.c_str());
+
+		return 1;
+	}
+
 	static duk_ret_t native_raw_shell(duk_context *ctx)
 	{
 		const std::string command=duk_require_string(ctx, -1);
+
 		if(command.empty())
 		{
-			return 0;
+			duk_push_string(ctx, "Missing shell command");
+			return duk_throw(ctx);
 		}
 
 		redi::ipstream proc(command, redi::pstreams::pstdout|redi::pstreams::pstderr);
@@ -364,7 +440,13 @@ private:
 			duk_push_c_function(context_, native_raw_print, DUK_VARARGS);
 			duk_put_global_string(context_, "raw_print");
 
-			duk_push_c_function(context_, native_raw_shell, DUK_VARARGS);
+			duk_push_c_function(context_, native_raw_fprint, 2);
+			duk_put_global_string(context_, "raw_fprint");
+
+			duk_push_c_function(context_, native_raw_fread, 1);
+			duk_put_global_string(context_, "raw_fread");
+
+			duk_push_c_function(context_, native_raw_shell, 1);
 			duk_put_global_string(context_, "raw_shell");
 
 			duk_push_c_function(context_, native_raw_voronota, DUK_VARARGS);
