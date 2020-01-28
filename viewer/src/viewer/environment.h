ac748373a303f3146b85280dd36a64821bfbf9b3
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-01-28 16:26:08 +0200

    Improved web javascript calling procedure.

--- viewer/src/viewer/environment.h
+++ viewer/src/viewer/environment.h
@@ -23,9 +23,9 @@ public:
 	{
 #ifdef FOR_WEB
 		std::string wrapped_script;
-		wrapped_script+="try {\n eval(\"";
+		wrapped_script+="try {\n var f=new Function(\"";
 		wrapped_script+=scripting::JSONWriter::replace_special_characters_with_escape_sequences(script);
-		wrapped_script+="\");\n}\ncatch(err) {\n console.log('Error in delegated JS script: '+err.message);\n}\n";
+		wrapped_script+="\");\n f();\n}\ncatch(err) {\n console.log('Error in delegated JS script: '+err.message);\n}\n";
 		emscripten_run_script(wrapped_script.c_str());
 #else
 		DuktapeManager::eval(script);
