da5697fcbfe140c224a3dd4cd3fa5b67ce9bce9d
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-02-07 12:03:02 +0200

    Improved residue voromqa energy profile output.

--- src/scripting/operators/generate_residue_voromqa_energy_profile.h
+++ src/scripting/operators/generate_residue_voromqa_energy_profile.h
@@ -71,7 +71,7 @@ public:
 
 		std::map<CCEG::CRAD, CCEG::ContactEffectGroupingEnergyProfile> map_of_residue_energy_profiles=
 				CCEG::ContactEffectGroupingEnergyProfile::construct_map_of_contact_effect_grouping_energy_profiles(
-						data_manager.atoms(), data_manager.contacts(), 3, adjunct_atom_volumes, adjunct_atom_quality_scores, adjunct_inter_atom_energy_scores_raw);
+						data_manager.atoms(), data_manager.contacts(), 4, adjunct_atom_volumes, adjunct_atom_quality_scores, adjunct_inter_atom_energy_scores_raw);
 
 		OutputSelector output_selector(file);
 
@@ -79,18 +79,21 @@ public:
 			std::ostream& output=output_selector.stream();
 			assert_io_stream(file, output);
 
-			for(std::map<CCEG::CRAD, CCEG::ContactEffectGroupingEnergyProfile>::const_iterator it=map_of_residue_energy_profiles.begin();it!=map_of_residue_energy_profiles.end();++it)
+			if(!map_of_residue_energy_profiles.empty())
 			{
-				const CCEG::CRAD& crad=it->first;
-				const CCEG::ContactEffectGroupingEnergyProfile& cegep=it->second;
-				output << crad << " " << crad.resName;
+				output << "pos ncat";
+				for(int i=0;i<20;i++)
+				{
+					output << " nf" << i;
+				}
+				const CCEG::ContactEffectGroupingEnergyProfile& cegep=(map_of_residue_energy_profiles.begin()->second);
 				for(std::size_t i=0;i<cegep.layered_residue_attributes.size();i++)
 				{
-					output << " " << cegep.layered_residue_attributes[i].atom_count_sum;
-					output << " " << cegep.layered_residue_attributes[i].atom_volume_sum;
-					output << " " << cegep.layered_residue_attributes[i].atom_quality_score_sum;
-					output << " " << cegep.layered_residue_attributes[i].solvent_contact_area_sum;
-					output << " " << cegep.layered_residue_attributes[i].solvent_contact_energy_sum;
+					output << " rl" << i << "_ac";
+					output << " rl" << i << "_av";
+					output << " rl" << i << "_aqs";
+					output << " rl" << i << "_sca";
+					output << " rl" << i << "_sce";
 				}
 				for(std::size_t i=0;i<cegep.layered_inter_residue_attributes.size();i++)
 				{
@@ -99,10 +102,58 @@ public:
 							cegep.layered_inter_residue_attributes[i].inter_atom_contact_energy_split_sum.size());
 					for(std::size_t j=0;j<num_of_splits;j++)
 					{
+						output << " irl" << i << "_ss" << j << "_iacas";
+						output << " irl" << i << "_ss" << j << "_iaces";
+					}
+				}
+				output << "\n";
+			}
+
+			for(std::map<CCEG::CRAD, CCEG::ContactEffectGroupingEnergyProfile>::const_iterator it=map_of_residue_energy_profiles.begin();it!=map_of_residue_energy_profiles.end();++it)
+			{
+				{
+					const CCEG::CRAD& crad=it->first;
+
+					output << crad.chainID << "_" << crad.resSeq << "_" << crad.resName;
+					if(!crad.iCode.empty())
+					{
+						output << "_i" << crad.iCode;
+					}
+
+					const int res_name_number=convert_residue_name_to_number(crad.resName);
+
+					output << " " << res_name_number;
+
+					for(int i=0;i<20;i++)
+					{
+						output << " " << (i==res_name_number ? 1 : 0);
+					}
+				}
+
+				const CCEG::ContactEffectGroupingEnergyProfile& cegep=it->second;
+
+				for(std::size_t i=0;i<cegep.layered_residue_attributes.size();i++)
+				{
+					output << " " << cegep.layered_residue_attributes[i].atom_count_sum;
+					output << " " << cegep.layered_residue_attributes[i].atom_volume_sum;
+					output << " " << cegep.layered_residue_attributes[i].atom_quality_score_sum;
+					output << " " << cegep.layered_residue_attributes[i].solvent_contact_area_sum;
+					output << " " << cegep.layered_residue_attributes[i].solvent_contact_energy_sum;
+				}
+
+				for(std::size_t i=0;i<cegep.layered_inter_residue_attributes.size();i++)
+				{
+					const std::size_t num_of_splits=std::min(
+							cegep.layered_inter_residue_attributes[i].inter_atom_contact_area_split_sum.size(),
+							cegep.layered_inter_residue_attributes[i].inter_atom_contact_energy_split_sum.size());
+
+					for(std::size_t j=0;j<num_of_splits;j++)
+					{
 						output << " " << cegep.layered_inter_residue_attributes[i].inter_atom_contact_area_split_sum[j];
 						output << " " << cegep.layered_inter_residue_attributes[i].inter_atom_contact_energy_split_sum[j];
 					}
 				}
+
 				output << "\n";
 			}
 		}
@@ -115,6 +166,49 @@ public:
 		}
 
 		return result;
+	}
+
+private:
+	static int convert_residue_name_to_number(const std::string& name)
+	{
+		static const std::map<std::string, int> m=create_map_of_residue_names_to_numbers();
+		const std::map<std::string, int>::const_iterator it=m.find(name);
+		return (it==m.end() ? -1 : it->second);
+	}
+
+	static std::map<std::string, int> create_map_of_residue_names_to_numbers()
+	{
+		std::map<std::string, int> m;
+
+		m["LEU"]=-1;
+		m["VAL"]=-1;
+		m["ILE"]=-1;
+		m["ALA"]=-1;
+		m["PHE"]=-1;
+		m["TRP"]=-1;
+		m["MET"]=-1;
+		m["PRO"]=-1;
+		m["ASP"]=-1;
+		m["GLU"]=-1;
+		m["LYS"]=-1;
+		m["ARG"]=-1;
+		m["HIS"]=-1;
+		m["CYS"]=-1;
+		m["SER"]=-1;
+		m["THR"]=-1;
+		m["TYR"]=-1;
+		m["ASN"]=-1;
+		m["GLN"]=-1;
+		m["GLY"]=-1;
+
+		int id=0;
+		for(std::map<std::string, int>::iterator it=m.begin();it!=m.end();++it)
+		{
+			it->second=id;
+			id++;
+		}
+
+		return m;
 	}
 };
 
