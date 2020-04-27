0699ca36aa2b46cc7de98c1f7c7084c2ea925eaf
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-04-27 17:04:56 +0300

    Improved global adjuncts exporting command.

--- src/scripting/operators/export_global_adjuncts.h
+++ src/scripting/operators/export_global_adjuncts.h
@@ -1,6 +1,8 @@
 #ifndef SCRIPTING_OPERATORS_EXPORT_GLOBAL_ADJUNCTS_H_
 #define SCRIPTING_OPERATORS_EXPORT_GLOBAL_ADJUNCTS_H_
 
+#include <algorithm>
+
 #include "../operators_common.h"
 
 namespace voronota
@@ -26,8 +28,11 @@ public:
 	CongregationOfDataManagers::ObjectQuery query;
 	bool all;
 	std::vector<std::string> adjuncts;
+	std::string sort_inc;
+	std::string sort_dec;
+	bool format_columns;
 
-	ExportGlobalAdjuncts() : all(false)
+	ExportGlobalAdjuncts() : all(false), format_columns(false)
 	{
 	}
 
@@ -38,6 +43,9 @@ public:
 		query=Utilities::read_congregation_of_data_managers_object_query(input);
 		all=input.get_flag("all");
 		adjuncts=input.get_value_vector_or_default<std::string>("adjuncts", std::vector<std::string>());
+		sort_inc=input.get_value_or_default<std::string>("sort-inc", "");
+		sort_dec=input.get_value_or_default<std::string>("sort-dec", "");
+		format_columns=input.get_flag("format-columns");
 	}
 
 	void document(CommandDocumentation& doc) const
@@ -46,6 +54,9 @@ public:
 		Utilities::document_read_congregation_of_data_managers_object_query(doc);
 		doc.set_option_decription(CDOD("all", CDOD::DATATYPE_BOOL, "flag to export all adjuncts"));
 		doc.set_option_decription(CDOD("adjuncts", CDOD::DATATYPE_STRING_ARRAY, "adjunct names", ""));
+		doc.set_option_decription(CDOD("sort-inc", CDOD::DATATYPE_STRING, "adjunct to sort in increasing order", ""));
+		doc.set_option_decription(CDOD("sort-dec", CDOD::DATATYPE_STRING, "adjunct to sort in decreasing order", ""));
+		doc.set_option_decription(CDOD("format-columns", CDOD::DATATYPE_BOOL, "flag to format columns"));
 	}
 
 	Result run(CongregationOfDataManagers& congregation_of_data_managers) const
@@ -62,6 +73,11 @@ public:
 		if(all && !adjuncts.empty())
 		{
 			throw std::runtime_error(std::string("Conflicting specification of adjuncts."));
+		}
+
+		if(!sort_inc.empty() && !sort_dec.empty())
+		{
+			throw std::runtime_error(std::string("Conflicting specification of sorting adjunct."));
 		}
 
 		const std::vector<DataManager*> objects=congregation_of_data_managers.get_objects(query);
@@ -85,7 +101,7 @@ public:
 			}
 			if(all_adjuncts.empty())
 			{
-				throw std::runtime_error(std::string("Selected atoms have no adjuncts."));
+				throw std::runtime_error(std::string("Selected objects have no adjuncts."));
 			}
 			adjuncts_filled=std::vector<std::string>(all_adjuncts.begin(), all_adjuncts.end());
 		}
@@ -99,40 +115,136 @@ public:
 			throw std::runtime_error(std::string("No adjuncts specified."));
 		}
 
+		if(!sort_inc.empty() && std::find(adjuncts_filled.begin(), adjuncts_filled.end(), sort_inc)==adjuncts_filled.end())
+		{
+			throw std::runtime_error(std::string("No adjunct for sorting."));
+		}
+
+		if(!sort_dec.empty() && std::find(adjuncts_filled.begin(), adjuncts_filled.end(), sort_dec)==adjuncts_filled.end())
+		{
+			throw std::runtime_error(std::string("No adjunct for sorting."));
+		}
+
+		std::vector<std::string> header;
+		header.push_back("ID");
+		header.insert(header.end(), adjuncts_filled.begin(), adjuncts_filled.end());
+
+		std::vector<ContentRow> rows;
+
+		for(std::size_t i=0;i<objects.size();i++)
+		{
+			const DataManager& dm=(*(objects[i]));
+			rows.push_back(ContentRow(i));
+			ContentRow& row=rows.back();
+			row.add_value(congregation_of_data_managers.get_object_attributes(objects[i]).name);
+			for(std::size_t j=0;j<adjuncts_filled.size();j++)
+			{
+				std::map<std::string, double>::const_iterator it=dm.global_numeric_adjuncts().find(adjuncts_filled[j]);
+				if(it!=dm.global_numeric_adjuncts().end())
+				{
+					row.add_value(it->second);
+					if(sort_inc==it->first)
+					{
+						row.sort_value=it->second;
+					}
+					if(sort_dec==it->first)
+					{
+						row.sort_value=0.0-(it->second);
+					}
+				}
+				else
+				{
+					row.add_value("NA");
+				}
+			}
+		}
+
+		std::sort(rows.begin(), rows.end());
+
+		std::vector<std::size_t> column_widths(header.size(), 0);
+
+		if(format_columns)
+		{
+			update_column_widths(column_widths, header);
+			for(std::size_t i=0;i<rows.size();i++)
+			{
+				update_column_widths(column_widths, rows[i].values);
+			}
+		}
+
 		OutputSelector output_selector(file);
 		std::ostream& output=output_selector.stream();
 		assert_io_stream(file, output);
 
-		output << "ID";
-		for(std::size_t i=0;i<adjuncts_filled.size();i++)
+		output_vector_columns(column_widths, header, output);
+		for(std::size_t i=0;i<rows.size();i++)
 		{
-			output << " " << adjuncts_filled[i];
-		}
-		output << "\n";
-
-		for(std::size_t i=0;i<objects.size();i++)
-		{
-			output << congregation_of_data_managers.get_object_attributes(objects[i]).name;
-			const DataManager& dm=(*(objects[i]));
-			for(std::size_t j=0;j<adjuncts_filled.size();j++)
-			{
-				std::map<std::string, double>::const_iterator it=dm.global_numeric_adjuncts().find(adjuncts_filled[j]);
-				output << " ";
-				if(it!=dm.global_numeric_adjuncts().end())
-				{
-					output << (it->second);
-				}
-				else
-				{
-					output << "NA";
-				}
-			}
-			output << "\n";
+			output_vector_columns(column_widths, rows[i].values, output);
 		}
 
 		Result result;
 
 		return result;
+	}
+
+private:
+	class ContentRow
+	{
+	public:
+		std::size_t original_row_number;
+		double sort_value;
+		std::vector<std::string> values;
+
+		ContentRow(const std::size_t index) : original_row_number(index), sort_value(std::numeric_limits<double>::max())
+		{
+		}
+
+		template<typename T>
+		void add_value(const T& value)
+		{
+			std::ostringstream output;
+			output << value;
+			values.push_back(output.str());
+		}
+
+		bool operator<(const ContentRow& cr) const
+		{
+			if(sort_value<cr.sort_value)
+			{
+				return true;
+			}
+			else if(sort_value==cr.sort_value && original_row_number<cr.original_row_number)
+			{
+				return true;
+			}
+			return false;
+		}
+	};
+
+	static void update_column_widths(std::vector<std::size_t>& column_widths, const std::vector<std::string>& values)
+	{
+		for(std::size_t j=0;j<column_widths.size() && j<values.size();j++)
+		{
+			column_widths[j]=std::max(column_widths[j], values[j].size());
+		}
+	}
+
+	static void output_vector_columns(const std::vector<std::size_t>& column_widths, const std::vector<std::string>& values, std::ostream& output)
+	{
+		for(std::size_t j=0;j<column_widths.size() && j<values.size();j++)
+		{
+			output << values[j];
+			if(column_widths[j]>values[j].size())
+			{
+				std::size_t padding=(column_widths[j]-values[j].size());
+				output << std::string(padding, ' ');
+			}
+			if(j+1<values.size())
+			{
+				output << "  ";
+			}
+		}
+		output << "\n";
 	}
 };
 
