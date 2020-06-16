8f6004647dca92642de32a1c303bccfd13518c1e
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-06-16 21:18:12 +0300

    Improved duktaper preview.

---
+++ viewer/generate_duktaper_preview.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+readonly TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+mkdir -p "$TMPLDIR/voronota-duktaper-preview/src/duktaper"
+
+cp "./voronota-duktaper" "$TMPLDIR/voronota-duktaper-preview/voronota-duktaper"
+cp "./voronota-duktaper-voromqa" "$TMPLDIR/voronota-duktaper-preview/voronota-duktaper-voromqa"
+
+cp -r "../src" "$TMPLDIR/voronota-duktaper-preview/src/src"
+cp -r "../viewer/src" "$TMPLDIR/voronota-duktaper-preview/src/duktaper/src"
+
+cd "$TMPLDIR/voronota-duktaper-preview"
+
+rm -f "./src/src/voronota.cpp"
+rm -rf "./src/src/modes"
+
+rm -f "./src/duktaper/src/voronota_viewer.cpp"
+rm -rf "./src/duktaper/src/viewer"
+rm -rf "./src/duktaper/src/uv"
+rm -rf "./src/duktaper/src/dependencies/imgui"
+
+cat > "./compile.sh" << 'EOF'
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+g++ -std=c++14 -I"./src/duktaper/src/dependencies" -O3 -o "./voronota-duktaper" \
+  "./src/duktaper/src/dependencies/tmalign/TMalign.cpp" \
+  "./src/duktaper/src/dependencies/duktape/duktape.cpp" \
+  "./src/duktaper/src/voronota_duktaper.cpp"
+EOF
+
+chmod +x "./compile.sh"
+
+cd - &> /dev/null
+
+cd "$TMPLDIR"
+
+tar -czf "./voronota-duktaper-preview.tar.gz" "./voronota-duktaper-preview"
+
+cd - &> /dev/null
+
+mkdir -p "./previews"
+
+mv "$TMPLDIR/voronota-duktaper-preview.tar.gz" "./previews/voronota-duktaper-preview.tar.gz"
+
