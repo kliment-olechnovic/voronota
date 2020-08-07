7ccbc05db764b40ed429012d4c31bce3571c382b
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-08-07 11:19:38 +0300

    Improved distance constraint checking operator.

--- src/scripting/operators/check_distance_constraint.h
+++ src/scripting/operators/check_distance_constraint.h
@@ -26,8 +26,9 @@ public:
 	std::string selection_expresion_for_atoms_b;
 	double interval_min;
 	double interval_max;
+	bool all;
 
-	CheckDistanceConstraint() : interval_min(std::numeric_limits<double>::min()), interval_max(std::numeric_limits<double>::max())
+	CheckDistanceConstraint() : interval_min(std::numeric_limits<double>::min()), interval_max(std::numeric_limits<double>::max()), all(false)
 	{
 	}
 
@@ -37,6 +38,7 @@ public:
 		selection_expresion_for_atoms_b=input.get_value<std::string>("atoms-second");
 		interval_min=input.get_value_or_default<double>("interval-min", std::numeric_limits<double>::min());
 		interval_max=input.get_value_or_default<double>("interval-max", std::numeric_limits<double>::max());
+		all=input.get_flag("all");
 	}
 
 	void document(CommandDocumentation& doc) const
@@ -45,6 +47,7 @@ public:
 		doc.set_option_decription(CDOD("atoms-second", CDOD::DATATYPE_STRING, "selection expression for the second group of atoms"));
 		doc.set_option_decription(CDOD("interval-min", CDOD::DATATYPE_FLOAT, "distance interval minimum", "-inf"));
 		doc.set_option_decription(CDOD("interval-max", CDOD::DATATYPE_FLOAT, "distance interval maximum", "+inf"));
+		doc.set_option_decription(CDOD("all", CDOD::DATATYPE_BOOL, "flag to check if all selected distances are in the interval"));
 	}
 
 	Result run(DataManager& data_manager) const
@@ -65,28 +68,38 @@ public:
 			throw std::runtime_error(std::string("No second atoms selected."));
 		}
 
-		bool found=false;
+		Result result;
+		int count_in=0;
 
-		for(std::set<std::size_t>::const_iterator it_a=atom_ids_a.begin();it_a!=atom_ids_a.end() && !found;++it_a)
+		for(std::set<std::size_t>::const_iterator it_a=atom_ids_a.begin();it_a!=atom_ids_a.end();++it_a)
 		{
 			const apollota::SimplePoint pount_a(data_manager.atoms()[*it_a].value);
-			for(std::set<std::size_t>::const_iterator it_b=atom_ids_b.begin();it_b!=atom_ids_b.end() && !found;++it_b)
+			for(std::set<std::size_t>::const_iterator it_b=atom_ids_b.begin();it_b!=atom_ids_b.end();++it_b)
 			{
 				const apollota::SimplePoint pount_b(data_manager.atoms()[*it_b].value);
 				const double distance=apollota::distance_from_point_to_point(pount_a, pount_b);
 				if(distance>=interval_min && distance<=interval_max)
 				{
-					found=true;
+					count_in++;
+					if(!all)
+					{
+						return result;
+					}
+				}
+				else
+				{
+					if(all)
+					{
+						throw std::runtime_error(std::string("Found a pair of atoms that did not satisfy the distance constraint."));
+					}
 				}
 			}
 		}
 
-		if(!found)
+		if(count_in==0)
 		{
 			throw std::runtime_error(std::string("No pair of atoms satisfied the distance constraint."));
 		}
-
-		Result result;
 
 		return result;
 	}
