717732c45775ce71032fd16b6cb4456b747fc08f
kliment-olechnovic <kliment.olechnovic@gmail.com>
2021-06-07 15:31:59 +0300

    Improved distance stats operator.

--- src/scripting/operators/distance.h
+++ src/scripting/operators/distance.h
@@ -56,9 +56,12 @@ public:
 
 	std::string selection_expresion_for_atoms_a;
 	std::string selection_expresion_for_atoms_b;
+	bool inter_chain;
 	std::vector<double> histogram_breaks;
+	std::string adjunct_min;
+	std::string adjunct_max;
 
-	Distance()
+	Distance() : inter_chain(false)
 	{
 	}
 
@@ -66,19 +69,28 @@ public:
 	{
 		selection_expresion_for_atoms_a=input.get_value<std::string>("atoms-first");
 		selection_expresion_for_atoms_b=input.get_value<std::string>("atoms-second");
+		inter_chain=input.get_flag("inter-chain");
 		histogram_breaks=input.get_value_vector_or_default<double>("histogram-breaks", std::vector<double>());
+		adjunct_min=input.get_value_or_default<std::string>("adjunct-min", "");
+		adjunct_max=input.get_value_or_default<std::string>("adjunct-max", "");
 	}
 
 	void document(CommandDocumentation& doc) const
 	{
 		doc.set_option_decription(CDOD("atoms-first", CDOD::DATATYPE_STRING, "selection expression for the first group of atoms"));
 		doc.set_option_decription(CDOD("atoms-second", CDOD::DATATYPE_STRING, "selection expression for the second group of atoms"));
+		doc.set_option_decription(CDOD("inter-chain", CDOD::DATATYPE_BOOL, "flag to only calculate inter-chain distances"));
 		doc.set_option_decription(CDOD("histogram-breaks", CDOD::DATATYPE_FLOAT_ARRAY, "values of histogram breaks", ""));
+		doc.set_option_decription(CDOD("adjunct-min", CDOD::DATATYPE_STRING, "name of output adjunct for min distance", ""));
+		doc.set_option_decription(CDOD("adjunct-max", CDOD::DATATYPE_STRING, "name of output adjunct for max distance", ""));
 	}
 
 	Result run(DataManager& data_manager) const
 	{
 		data_manager.assert_atoms_availability();
+
+		assert_adjunct_name_input(adjunct_min, true);
+		assert_adjunct_name_input(adjunct_max, true);
 
 		const std::set<std::size_t> atom_ids_a=data_manager.selection_manager().select_atoms(SelectionManager::Query(selection_expresion_for_atoms_a, false));
 
@@ -102,48 +114,79 @@ public:
 
 		for(std::set<std::size_t>::const_iterator it_a=atom_ids_a.begin();it_a!=atom_ids_a.end();++it_a)
 		{
-			const apollota::SimplePoint point_a(data_manager.atoms()[*it_a].value);
+			const Atom& atom_a=data_manager.atoms()[*it_a];
 			for(std::set<std::size_t>::const_iterator it_b=atom_ids_b.begin();it_b!=atom_ids_b.end();++it_b)
 			{
-				const apollota::SimplePoint point_b(data_manager.atoms()[*it_b].value);
-				const double distance=apollota::distance_from_point_to_point(point_a, point_b);
+				const Atom& atom_b=data_manager.atoms()[*it_b];
+				if(!inter_chain || atom_a.crad.chainID!=atom_b.crad.chainID)
+				{
+					const double distance=apollota::distance_from_point_to_point(atom_a.value, atom_b.value);
 
-				if(distance<result.distance_min)
-				{
-					result.distance_min=distance;
-				}
-				if(distance>result.distance_max)
-				{
-					result.distance_max=distance;
-				}
-
-				result.total_count++;
-
-				if(result.histogram_values.size()==1)
-				{
-					result.histogram_values[0]++;
-				}
-				else
-				{
-					bool found_bin=false;
-					for(std::size_t i=0;i<result.histogram_values.size() && !found_bin;i++)
+					if(distance<result.distance_min)
 					{
-						if(i==0)
-						{
-							found_bin=(distance<=result.histogram_breaks[i]);
-						}
-						else if((i+1)==result.histogram_values.size())
-						{
-							found_bin=(distance>result.histogram_breaks[i-1]);
-						}
-						else
-						{
-							found_bin=((distance<=result.histogram_breaks[i]) && (distance>result.histogram_breaks[i-1]));
-						}
+						result.distance_min=distance;
+					}
+					if(distance>result.distance_max)
+					{
+						result.distance_max=distance;
+					}
 
-						if(found_bin)
+					result.total_count++;
+
+					if(result.histogram_values.size()==1)
+					{
+						result.histogram_values[0]++;
+					}
+					else
+					{
+						bool found_bin=false;
+						for(std::size_t i=0;i<result.histogram_values.size() && !found_bin;i++)
 						{
-							result.histogram_values[i]++;
+							if(i==0)
+							{
+								found_bin=(distance<=result.histogram_breaks[i]);
+							}
+							else if((i+1)==result.histogram_values.size())
+							{
+								found_bin=(distance>result.histogram_breaks[i-1]);
+							}
+							else
+							{
+								found_bin=((distance<=result.histogram_breaks[i]) && (distance>result.histogram_breaks[i-1]));
+							}
+
+							if(found_bin)
+							{
+								result.histogram_values[i]++;
+							}
+						}
+					}
+
+					if(!adjunct_min.empty() || !adjunct_max.empty())
+					{
+						std::map<std::string, double>& map_a=data_manager.atom_adjuncts_mutable(*it_a);
+						std::map<std::string, double>& map_b=data_manager.atom_adjuncts_mutable(*it_b);
+						if(!adjunct_min.empty())
+						{
+							if(map_a.count(adjunct_min)<1 || map_a[adjunct_min]>distance)
+							{
+								map_a[adjunct_min]=distance;
+							}
+							if(map_b.count(adjunct_min)<1 || map_b[adjunct_min]>distance)
+							{
+								map_b[adjunct_min]=distance;
+							}
+						}
+						if(!adjunct_max.empty())
+						{
+							if(map_a.count(adjunct_max)<1 || map_a[adjunct_max]<distance)
+							{
+								map_a[adjunct_max]=distance;
+							}
+							if(map_b.count(adjunct_max)<1 || map_b[adjunct_max]<distance)
+							{
+								map_b[adjunct_max]=distance;
+							}
 						}
 					}
 				}
