2dee8b31dca78e3670fff0019eee7bc6d920e409
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-02-23 16:27:22 +0200

    Improved faspr call operator.

--- expansion_js/src/duktaper/operators/run_faspr.h
+++ expansion_js/src/duktaper/operators/run_faspr.h
@@ -51,10 +51,49 @@ public:
 
 		CallShellUtilities::TemporaryDirectory tmpdir;
 
-		scripting::operators::ExportAtoms().init(CMDIN()
-				.set("use", "[--aname CA,C,N,O,OXT]")
-				.set("as-pdb", true)
-				.set("file", tmpdir.dir_path()+"/backbone.pdb")).run(data_manager);
+		{
+			std::vector<scripting::Atom> atoms_to_export;
+			atoms_to_export.reserve(data_manager.atoms().size());
+
+			for(std::size_t i=0;i<data_manager.primary_structure_info().residues.size();i++)
+			{
+				const common::ConstructionOfPrimaryStructure::Residue& residue=data_manager.primary_structure_info().residues[i];
+				bool has_CA=false;
+				bool has_C=false;
+				bool has_N=false;
+				bool has_O=false;
+				for(std::size_t j=0;j<residue.atom_ids.size() && !(has_CA && has_C && has_N && has_O);j++)
+				{
+					const scripting::Atom& atom=data_manager.atoms()[residue.atom_ids[j]];
+					has_CA=(has_CA || atom.crad.name=="CA");
+					has_C=(has_C || atom.crad.name=="C");
+					has_N=(has_N || atom.crad.name=="N");
+					has_O=(has_O || atom.crad.name=="O");
+				}
+				if(has_CA && has_C && has_N && has_O)
+				{
+					for(std::size_t j=0;j<residue.atom_ids.size();j++)
+					{
+						const scripting::Atom& atom=data_manager.atoms()[residue.atom_ids[j]];
+						if(atom.crad.name=="CA" || atom.crad.name=="C" || atom.crad.name=="N" || atom.crad.name=="O")
+						{
+							atoms_to_export.push_back(atom);
+						}
+					}
+				}
+			}
+
+			if(atoms_to_export.empty())
+			{
+				throw std::runtime_error(std::string("No residues with full set of backbone atoms found."));
+			}
+
+			const std::string backbone_file=(tmpdir.dir_path()+"/backbone.pdb");
+			scripting::OutputSelector output_selector(backbone_file);
+			std::ostream& output=output_selector.stream();
+			scripting::assert_io_stream(backbone_file, output);
+			common::WritingAtomicBallsInPDBFormat::write_atomic_balls(atoms_to_export, "tf", false, output);
+		}
 
 		{
 			std::ostringstream command_output;
