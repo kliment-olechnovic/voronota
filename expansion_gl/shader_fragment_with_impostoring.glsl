b5d9069a55b1358119a5cbec4cd1c0f78eac3c17
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-07-25 17:31:48 +0300

    Improved impostoring shaders in viewer.

--- expansion_gl/shader_fragment_with_impostoring.glsl
+++ expansion_gl/shader_fragment_with_impostoring.glsl
@@ -25,7 +25,18 @@ void main()
 	
     if(selection_mode_enabled==0)
     {
-        vec3 final_color=mix(fragment_color_for_display, vec3(1.0, 1.0, 1.0), offset);
+    	vec3 fragment_normal=normalize(vec3((gl_FragCoord.xy-fragment_center_in_screen)/fragment_radius_in_screen, sqrt(1.0-offset*offset)));
+        vec3 final_color=fragment_color_for_display;
+        if(fragment_adjunct[1]<0.9)
+        {
+            vec3 light_direction=vec3(0.0, 0.0, 1.0);
+            vec3 light_color=vec3(1.0, 1.0, 1.0);
+            float ambient_value=0.05;
+            vec3 ambient=ambient_value*light_color;
+            float diffuse_value=abs(dot(normalize(fragment_normal), normalize(light_direction)));
+            vec3 diffuse=diffuse_value*light_color;
+            final_color=(ambient+diffuse)*fragment_color_for_display;
+        }
         if((fragment_adjunct[0]>0.5) && (mod(floor(gl_FragCoord.x), 2.0)<0.5 || mod(floor(gl_FragCoord.y), 2.0)<0.5))
         {
             final_color=vec3(1.0, 0.0, 1.0);
--- expansion_gl/shader_vertex_with_impostoring.glsl
+++ expansion_gl/shader_vertex_with_impostoring.glsl
@@ -17,26 +17,25 @@ varying vec3 fragment_color_for_display;
 varying vec3 fragment_adjunct;
 void main()
 {
-    vec4 vertex_position_in_clip=projection_matrix*viewtransform_matrix*modeltransform_matrix*vec4(vec3(vertex_info1), 1.0);
+	mat4 MVP=projection_matrix*viewtransform_matrix*modeltransform_matrix;
+	
+    vec4 vertex_position_in_clip=MVP*vec4(vec3(vertex_info1), 1.0);
     vec3 vertex_position_in_ndc=vertex_position_in_clip.xyz/vertex_position_in_clip.w;
     vec2 vertex_position_in_screen=(vertex_position_in_ndc.xy*0.5+0.5)*vec2(viewport[2], viewport[3])+vec2(viewport[0], viewport[1]);
     
-    vec4 sa100=projection_matrix*viewtransform_matrix*modeltransform_matrix*vec4(vec3(vertex_info1)+vec3(1.0, 0.0, 0.0), 1.0);
+    vec4 sa100=MVP*vec4(vec3(vertex_info1)+vec3(1.0, 0.0, 0.0), 1.0);
     vec3 sb100=sa100.xyz/sa100.w;
     vec2 sc100=(sb100.xy*0.5+0.5)*vec2(viewport[2], viewport[3])+vec2(viewport[0], viewport[1]);
     
-    vec4 sa010=projection_matrix*viewtransform_matrix*modeltransform_matrix*vec4(vec3(vertex_info1)+vec3(0.0, 1.0, 0.0), 1.0);
+    vec4 sa010=MVP*vec4(vec3(vertex_info1)+vec3(0.0, 1.0, 0.0), 1.0);
     vec3 sb010=sa010.xyz/sa010.w;
     vec2 sc010=(sb010.xy*0.5+0.5)*vec2(viewport[2], viewport[3])+vec2(viewport[0], viewport[1]);
     
-    vec4 sa001=projection_matrix*viewtransform_matrix*modeltransform_matrix*vec4(vec3(vertex_info1)+vec3(0.0, 0.0, 1.0), 1.0);
+    vec4 sa001=MVP*vec4(vec3(vertex_info1)+vec3(0.0, 0.0, 1.0), 1.0);
     vec3 sb001=sa001.xyz/sa001.w;
     vec2 sc001=(sb001.xy*0.5+0.5)*vec2(viewport[2], viewport[3])+vec2(viewport[0], viewport[1]);
     
-    vec2 ob100=sb100.xy-vertex_position_in_ndc.xy;
-    vec2 ob010=sb010.xy-vertex_position_in_ndc.xy;
-    vec2 ob001=sb001.xy-vertex_position_in_ndc.xy;
-    float scale_in_ndc=sqrt((dot(ob100, ob100)+dot(ob010, ob010)+dot(ob001, ob001))/2.0);
+    float scale_in_ndc=length(vec3(MVP[2]));
     
     vec2 oc100=sc100-vertex_position_in_screen;
     vec2 oc010=sc010-vertex_position_in_screen;
