063e43031b89dfadc29f6e455b30780dd17ab7f1
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-08-17 14:04:25 +0300

    Updated build environment configuration files.

--- version.bash
+++ version.bash
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 
-VERSION_MAJOR=$(cat ./src/voronota.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version \(\S\+\)";/\1/')
+VERSION_MAJOR=$(cat ./src/voronota.cpp | grep '"Voronota version ' | sed 's/.*"Voronota version //' | sed 's/";//')
 VERSION_MINOR=$(git rev-list --all --count)
 
 echo "${VERSION_MAJOR}.${VERSION_MINOR}"
