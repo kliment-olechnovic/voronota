a9d7359951b41a8c839b2e4001783cca2717cedc
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-12-02 09:49:42 +0200

    Updated shaders and web viewer.

--- expansion_gl/shaders/shader_fragment_with_impostoring.glsl
+++ expansion_gl/shaders/shader_fragment_with_impostoring.glsl
@@ -58,7 +58,7 @@ void main()
             float diffuse_value=abs(dot(normalize(fragment_normal), normalize(light_direction)));
             final_color=max(0.2, min(ambient_value+diffuse_value, 1.0))*fragment_color_for_display;
         }
-        if((fragment_adjunct[0]>0.5) && ((mod(floor(gl_FragCoord.x), 4.0)<0.5 && mod(floor(gl_FragCoord.y), 4.0)>0.5) || (mod(floor(gl_FragCoord.x), 4.0)>0.5 && mod(floor(gl_FragCoord.y), 4.0)<0.5) || (mod(floor(gl_FragCoord.x+2.0), 4.0)==mod(floor(gl_FragCoord.y+2.0), 4.0))))
+        if(fragment_adjunct[0]>0.5 && mod(floor(gl_FragCoord.x*0.75), 2.0)==mod(floor(gl_FragCoord.y*0.75), 2.0))
         {
             final_color=vec3(1.0, 0.0, 1.0);
         }
