56ef6308e476b9769897e5523428235999b027b4
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-12-23 14:30:30 +0200

    Improved marking pattern in shaders in viewer.

--- expansion_gl/shaders/shader_fragment_with_impostoring.glsl
+++ expansion_gl/shaders/shader_fragment_with_impostoring.glsl
@@ -58,9 +58,24 @@ void main()
             float diffuse_value=abs(dot(normalize(fragment_normal), normalize(light_direction)));
             final_color=max(0.2, min(ambient_value+diffuse_value, 1.0))*fragment_color_for_display;
         }
-        if(fragment_adjunct[0]>0.5 && mod(floor(gl_FragCoord.x*0.75), 2.0)==mod(floor(gl_FragCoord.y*0.75), 2.0))
+        if(fragment_adjunct[0]>0.5)
         {
-            final_color=vec3(1.0, 0.0, 1.0);
+            float x_shift=0.0;
+            if(mod(gl_FragCoord.y, 8.0)<4.0)
+            {
+                x_shift=2.0;
+            }
+            float y_a=1.0;
+            float y_b=0.0;
+            if(mod(gl_FragCoord.x, 8.0)<4.0)
+            {
+                y_a=-1.0;
+                y_b=3.0;
+            }
+            if((mod(floor(gl_FragCoord.x+x_shift), 4.0)<0.5 && (mod(floor(gl_FragCoord.y), 4.0)*y_a+y_b)>0.5) || (mod(floor(gl_FragCoord.x+x_shift), 4.0)>0.5 && (mod(floor(gl_FragCoord.y), 4.0)*y_a+y_b)<0.5) || (mod(floor(gl_FragCoord.x+x_shift+2.0), 4.0)==(mod(floor(gl_FragCoord.y+2.0), 4.0)*y_a+y_b)))
+            {
+                final_color=vec3(1.0, 0.0, 1.0);
+            }
         }
         gl_FragColor=vec4(final_color, z);
     }
