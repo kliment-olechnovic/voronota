2ff96a9b7a28d420b96d448152dba888e4c57a8f
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-02-15 15:29:52 +0200

    Augmented version.

--- src/voronota.cpp
+++ src/voronota.cpp
@@ -128,7 +128,7 @@ std::vector<ModeDescriptor> get_list_of_xmodes()
 
 std::string version()
 {
-	static const std::string str="Voronota version 1.24";
+	static const std::string str="Voronota version 1.25";
 	return str;
 }
 
