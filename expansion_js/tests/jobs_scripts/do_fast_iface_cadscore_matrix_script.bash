83e36441e5270fe9257f6764b10a3a5769ad6960
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-02-28 19:12:41 +0200

    Improved fast iface cadscore tests.

--- expansion_js/tests/jobs_scripts/do_fast_iface_cadscore_matrix_script.bash
+++ expansion_js/tests/jobs_scripts/do_fast_iface_cadscore_matrix_script.bash
@@ -3,27 +3,41 @@
 SUBDIR=$OUTPUTDIR/fast_iface_cadscore_matrix_script
 mkdir -p $SUBDIR
 
+mkdir -p "${SUBDIR}/augmented_input"
+
 find "$INPUTDIR/complex/" -type f \
+| while read -r FNAME
+do
+	for NUM in 1 2 3 4
+	do
+		cp "$FNAME" "${SUBDIR}/augmented_input/copy${NUM}_$(basename ${FNAME})"
+	done
+done
+
+find "${SUBDIR}/augmented_input" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-cadscore-matrix \
   --processors 4 \
   --output-table-file "$SUBDIR/global_scores"
 
-find "$INPUTDIR/complex/" -type f \
+find "${SUBDIR}/augmented_input" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-cadscore-matrix \
 | column -t \
 > "$SUBDIR/global_scores_formatted"
 
-find "$INPUTDIR/complex/" -type f \
+find "${SUBDIR}/augmented_input" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-cadscore-matrix \
   --processors 4 \
   --remap-chains \
 | column -t \
 > "$SUBDIR/global_scores_remapped_formatted"
 
-find "$INPUTDIR/complex/" -type f \
+find "${SUBDIR}/augmented_input" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-cadscore-matrix \
   --processors 4 \
   --remap-chains \
   --crude \
 | column -t \
 > "$SUBDIR/global_scores_crude_remapped_formatted"
+
+rm -r "${SUBDIR}/augmented_input"
+
