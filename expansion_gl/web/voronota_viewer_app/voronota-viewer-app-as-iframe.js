40806cfe5d0a3fe6c2270a634103c2bc8cb0ee36
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-08-12 15:56:06 +0300

    Improved iframe wrapper.

--- expansion_gl/web/voronota_viewer_app/voronota-viewer-app-as-iframe.js
+++ expansion_gl/web/voronota_viewer_app/voronota-viewer-app-as-iframe.js
@@ -29,20 +29,30 @@ function voronota_viewer_init_as_iframe(width, height, post_init_operations, ifr
 	iframe.style.border = 'none';
 	
 	document.body.appendChild(iframe);
+	
+	const main_script_path_string=JSON.stringify(voronota_viewer_app_as_iframe_subdirectory+"voronota-viewer-app.js");
+	const post_init_operations_string=JSON.stringify(post_init_operations);
 
-	var srcdoc = '<!doctype html><html lang="en-us"><head><meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
-	srcdoc += '<title>Voronota-GL</title>'
-	srcdoc += '<style> body { font-family: arial; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px none; display: block; overflow: hidden; } </style>'
-	srcdoc += '</head><body>'
-	
-	srcdoc += '<script src=';
-	srcdoc += JSON.stringify(voronota_viewer_app_as_iframe_subdirectory+"voronota-viewer-app.js");
-	srcdoc += '><\/script>';
-	
-	srcdoc += '<script> voronota_viewer_init(function(){return window.innerWidth;}, function(){return window.innerHeight;}, ';
-	srcdoc += JSON.stringify(post_init_operations);
-	srcdoc += ');<\/script>';
-	srcdoc += '</body></html>';
+	var srcdoc = 
+		`<!doctype html>
+		<html lang="en-us">
+			<head>
+				<meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+				<title>Voronota-GL</title>
+				<style>
+					body { font-family: arial; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px none; display: block; overflow: hidden; }
+				</style>
+			</head>
+			<body>
+				<script src=${main_script_path_string}><\/script>
+				<script>
+					voronota_viewer_init(
+						function(){return window.innerWidth;},
+						function(){return window.innerHeight;},
+						${post_init_operations_string});
+				<\/script>
+			</body>
+		</html>`;
 	
 	iframe.contentWindow.document.open();
 	iframe.contentWindow.document.write(srcdoc);
