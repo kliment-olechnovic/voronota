d2d84f51c0842c91b104da1a4855bd2322f206fa
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-08-12 20:35:24 +0300

    Added automatic styling option for web viewer in iframe.

--- expansion_gl/web/voronota_viewer_app/voronota-viewer-app-as-iframe.js
+++ expansion_gl/web/voronota_viewer_app/voronota-viewer-app-as-iframe.js
@@ -54,23 +54,48 @@ function voronota_viewer_init_as_iframe(config)
 		
 	const main_script_path_string=JSON.stringify(voronota_viewer_app_as_iframe_subdirectory+"voronota-viewer-app.js");
 	const post_init_operations_string=(config.post_init_operations) ? JSON.stringify(config.post_init_operations) : "null";
-	const bottom_margin_string=(config.bottom_margin) ? JSON.stringify(config.bottom_margin) : "null";
 	const add_buttons_string=(config.add_buttons) ? JSON.stringify(config.add_buttons) : "null";
-	const canvas_container_id_string=(config.canvas_container_id) ? JSON.stringify(config.canvas_container_id) : "null";
-	const buttons_container_id_string=(config.buttons_container_id) ? JSON.stringify(config.buttons_container_id) : "null";
-	const buttons_style_class_string=(config.buttons_style_class) ? JSON.stringify(config.buttons_style_class) : "null";
 	
-	const additional_head_content_string=(config.additional_head_content) ? config.additional_head_content : "";
-	const additional_body_content_string=(config.additional_body_content) ? config.additional_body_content : "";
+	var bottom_margin_string=(config.bottom_margin) ? JSON.stringify(config.bottom_margin) : ((config.add_buttons) ? JSON.stringify(30) : "null");
+	var canvas_container_id_string=(config.canvas_container_id) ? JSON.stringify(config.canvas_container_id) : "null";
+	var buttons_container_id_string=(config.buttons_container_id) ? JSON.stringify(config.buttons_container_id) : "null";
+	var buttons_style_class_string=(config.buttons_style_class) ? JSON.stringify(config.buttons_style_class) : "null";
+	
+	var additional_head_content_string=(config.additional_head_content) ? config.additional_head_content : "";
+	var additional_body_content_string=(config.additional_body_content) ? config.additional_body_content : "";
+	
+	if(config.prettify_buttons_panel)
+	{
+		bottom_margin_to_use=(config.bottom_margin) ? config.bottom_margin : 33;
+		
+		additional_head_content_string=`
+			<style>
+				.voronota-viewer-added-button { background-color: #555555; border: none; color: white; text-align: center; text-decoration: none; display: inline-block; padding-top: 5px; padding-bottom: 5px; padding-left: 10px; padding-right: 10px; margin-top: 5px; margin-bottom: 5px; margin-left: 5px; margin-right: 5px; height: 22px; font-size: 12px; }
+				.voronota-viewer-added-button:hover { background-color: #777777; }
+				#buttons_panel { bottom: 0; left: 0; position: absolute; margin: 0; padding: 0; height: ${bottom_margin_to_use}px; }
+			</style>
+			`+additional_head_content_string;
+		
+		additional_body_content_string=`
+			<div id="canvas_container"></div>
+			
+			<div id="buttons_panel"></div>
+			`+additional_body_content_string;
+		
+		bottom_margin_string=JSON.stringify(bottom_margin_to_use);
+		canvas_container_id_string=JSON.stringify("canvas_container");
+		buttons_container_id_string=JSON.stringify("buttons_panel");
+		buttons_style_class_string=JSON.stringify("voronota-viewer-added-button");
+	}
 
-	var srcdoc = 
-		`<!doctype html>
+	var srcdoc = `
+		<!doctype html>
 		<html lang="en-us">
 			<head>
 				<meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 				<title>Voronota-GL</title>
 				<style>
-					body { font-family: arial; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px none; display: block; overflow: hidden; }
+					body { font-family: arial; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px none; display: block; overflow: hidden; background-color: #333333; }
 				</style>
 				${additional_head_content_string}
 			</head>
@@ -90,7 +115,8 @@ function voronota_viewer_init_as_iframe(config)
 					});
 				<\/script>
 			</body>
-		</html>`;
+		</html>
+		`;
 	
 	iframe.contentWindow.document.open();
 	iframe.contentWindow.document.write(srcdoc);
