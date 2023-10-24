0dfec22551a1ce0417bdb4b8834ef2794eeb3f4d
kliment-olechnovic <kliment-olechnovic@local>
2023-10-24 13:53:56 +0200

    Improved expansion_lt tests.

--- expansion_lt/tests/run_all_tests.bash
+++ expansion_lt/tests/run_all_tests.bash
@@ -5,7 +5,9 @@ cd $(dirname "$0")
 rm -rf "./output"
 mkdir -p "./output"
 
-cat "./input/balls_2zsk.xyzr" | ../voronota-lt > "./output/contacts_2zsk_summary.txt"
+../voronota-lt < "./input/balls_2zsk.xyzr" > "./output/contacts_2zsk_summary.txt"
 
-cat "./input/balls_3dlb.xyzr" | ../voronota-lt > "./output/contacts_3dlb_summary.txt"
+../voronota-lt < "./input/balls_3dlb.xyzr" > "./output/contacts_3dlb_summary.txt"
+
+../voronota-lt < "./input/balls_7br8.xyzr" > "./output/contacts_7br8_summary.txt"
 
