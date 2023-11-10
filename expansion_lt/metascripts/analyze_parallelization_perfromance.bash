fdf6e34e5ca422d958a9b7bf4131e18bdd4e8233
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-11-10 17:59:59 +0100

    Improved parallelization analysis script.

--- expansion_lt/metascripts/analyze_parallelization_perfromance.bash
+++ expansion_lt/metascripts/analyze_parallelization_perfromance.bash
@@ -34,14 +34,18 @@ cd "$TMPLDIR"
 R --vanilla > /dev/null << 'EOF'
 dt=read.table("results", header=TRUE);
 
+dt$time_tessellation_stage__construct_contacts_ideal=(dt$time_tessellation_stage__construct_contacts[1]*dt$threads_max_number[1])/dt$threads_max_number;
+dt$time_full_program_elapsed_ideal=(dt$time_full_program_elapsed[1]*dt$threads_max_number[1])/dt$threads_max_number;
+
 dt$time_tessellation_stage__find_potential_neighbors=(dt$time_tessellation_stage__init_spheres_searcher+dt$time_tessellation_stage__pre.allocate_colliding_IDs+dt$time_tessellation_stage__detect_all_collisions+dt$time_tessellation_stage__count_all_collisions+dt$time_tessellation_stage__collect_relevant_collision_indices+dt$time_tessellation_stage__allocate_possible_contact_summaries);
 
 dt$time_tessellation_stage__collect_contact_summaries=(dt$time_tessellation_stage__count_valid_contact_summaries+dt$time_tessellation_stage__collect_indices_of_valid_contact_summaries+dt$time_tessellation_stage__copy_valid_contact_summaries+dt$time_tessellation_stage__accumulate_total_contacts_summary+dt$time_tessellation_stage__accumulate_cell_summaries+dt$time_tessellation_stage__compute_sas_for_cell_summaries+dt$time_tessellation_stage__accumulate_total_cells_summary);
 
-section_ids=c("time_full_program_elapsed", "time_input_stage__read_balls_from_stdin", "time_tessellation_stage__find_potential_neighbors", "time_tessellation_stage__construct_contacts", "time_tessellation_stage__collect_contact_summaries");
+section_ids=c("time_full_program_elapsed", "time_input_stage__read_balls_from_stdin", "time_tessellation_stage__find_potential_neighbors", "time_tessellation_stage__construct_contacts", "time_tessellation_stage__collect_contact_summaries", "time_full_program_elapsed_ideal", "time_tessellation_stage__construct_contacts_ideal");
 
-section_names=c("full", "input", "find potential neighbors", "construct contacts", "copy contact summaries");
-section_colors=c("black", "green", "blue", "red", "purple");
+section_names=c("full", "input", "find potential neighbors", "construct contacts", "copy contact summaries", "full ideal speed-up", "construct contacts ideal speed-up");
+section_colors=c("black", "green", "blue", "red", "purple", "gray", "cyan");
+
 png("plot.png", units="in", width=10, height=10, res=150);
 plot(x=dt$threads_max_number, y=dt[,section_ids[1]], col=section_colors[1], ylim=c(0, max(dt[,section_ids[1]])), xlab="num of threads", ylab="miliseconds", main="num of threads vs running miliseconds");
 points(x=dt$threads_max_number, y=dt[,section_ids[1]], col=section_colors[1], type="l");
