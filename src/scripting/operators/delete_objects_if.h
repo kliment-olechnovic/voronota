f27f9b028966ae02256b726fb3238e47d1e7ca26
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-12-13 13:15:32 +0100

    Enhanced conditional deletion operator.

--- src/scripting/operators/delete_objects_if.h
+++ src/scripting/operators/delete_objects_if.h
@@ -33,9 +33,13 @@ public:
 	unsigned int chains_fewer_than;
 	unsigned int objects_fewer_than;
 	double atoms_to_residues_ratio_less_than;
+	unsigned int min_atoms_in_selection;
+	unsigned int min_contacts_in_selection;
+	std::string not_enough_atoms_in_selection;
+	std::string not_enough_contacts_in_selection;
 	CongregationOfDataManagers::ObjectQuery query;
 
-	DeleteObjectsIf() : atoms_fewer_than(1), residues_fewer_than(1), chains_fewer_than(1), objects_fewer_than(1), atoms_to_residues_ratio_less_than(0.0)
+	DeleteObjectsIf() : atoms_fewer_than(1), residues_fewer_than(1), chains_fewer_than(1), objects_fewer_than(1), atoms_to_residues_ratio_less_than(0.0), min_atoms_in_selection(1), min_contacts_in_selection(1)
 	{
 	}
 
@@ -47,6 +51,10 @@ public:
 		chains_fewer_than=input.get_value_or_default<unsigned int>("chains-fewer-than", 1);
 		objects_fewer_than=input.get_value_or_default<unsigned int>("objects-fewer-than", 1);
 		atoms_to_residues_ratio_less_than=input.get_value_or_default<double>("atoms-to-residues-ratio-less-than", 0.0);
+		min_atoms_in_selection=input.get_value_or_default<unsigned int>("min-atoms-in-selection", 1);
+		min_contacts_in_selection=input.get_value_or_default<unsigned int>("min-contacts-in-selection", 1);
+		not_enough_atoms_in_selection=input.get_value_or_default<std::string>("not-enough-atoms-in-selection", "");
+		not_enough_contacts_in_selection=input.get_value_or_default<std::string>("not-enough-contacts-in-selection", "");
 	}
 
 	void document(CommandDocumentation& doc) const
@@ -57,6 +65,10 @@ public:
 		doc.set_option_decription(CDOD("chains-fewer-than", CDOD::DATATYPE_INT, "minimal allowed number of chains", 1));
 		doc.set_option_decription(CDOD("objects-fewer-than", CDOD::DATATYPE_INT, "minimal allowed number of objects", 1));
 		doc.set_option_decription(CDOD("atoms-to-residues-ratio-less-than", CDOD::DATATYPE_FLOAT, "minimal allowed atoms to residues ratio", 0.0));
+		doc.set_option_decription(CDOD("min-atoms-in-selection", CDOD::DATATYPE_INT, "minimal allowed number of atoms in selection", 1));
+		doc.set_option_decription(CDOD("min-contacts-in-selection", CDOD::DATATYPE_INT, "minimal allowed number of contacts in selection", 1));
+		doc.set_option_decription(CDOD("not-enough-atoms-in-selection", CDOD::DATATYPE_STRING, "atoms selection expression", ""));
+		doc.set_option_decription(CDOD("not-enough-contacts-in-selection", CDOD::DATATYPE_STRING, "contacts selection expression", ""));
 	}
 
 	Result run(CongregationOfDataManagers& congregation_of_data_managers) const
@@ -73,12 +85,14 @@ public:
 
 		for(std::size_t i=0;i<objects.size();i++)
 		{
-			const DataManager& data_manager=*(objects[i]);
+			DataManager& data_manager=*(objects[i]);
 			bool need_to_delete=objects.size()<objects_fewer_than;
 			need_to_delete=need_to_delete || (data_manager.atoms().size()<atoms_fewer_than);
 			need_to_delete=need_to_delete || (data_manager.primary_structure_info().residues.size()<residues_fewer_than);
 			need_to_delete=need_to_delete || (data_manager.primary_structure_info().chains.size()<chains_fewer_than);
 			need_to_delete=need_to_delete || (static_cast<double>(data_manager.atoms().size())/static_cast<double>(data_manager.primary_structure_info().residues.size())<atoms_to_residues_ratio_less_than);
+			need_to_delete=need_to_delete || (!not_enough_atoms_in_selection.empty() && data_manager.selection_manager().select_atoms(SelectionManager::Query(not_enough_atoms_in_selection, false)).size()<min_atoms_in_selection);
+			need_to_delete=need_to_delete || (!not_enough_contacts_in_selection.empty() && (data_manager.contacts().empty() || data_manager.selection_manager().select_contacts(SelectionManager::Query(not_enough_contacts_in_selection, false)).size()<min_contacts_in_selection));
 			if(need_to_delete)
 			{
 				result.deleted_objects.push_back(congregation_of_data_managers.get_object_attributes(objects[i]).name);
