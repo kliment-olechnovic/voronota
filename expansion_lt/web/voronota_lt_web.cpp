2775cf5f91464672e5c633cb403fa2caf534d626
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-01-15 13:02:58 +0100

    Enhanced voronota-lt web app.

--- expansion_lt/web/voronota_lt_web.cpp
+++ expansion_lt/web/voronota_lt_web.cpp
@@ -7,7 +7,7 @@
 #include "../src/voronotalt/voronotalt.h"
 #include "../src/voronotalt_cli/voronotalt_cli.h"
 
-std::vector<std::string> generate_results(const std::string& input_data)
+std::vector<std::string> generate_results(const std::string& input_data, const std::string& options)
 {
 	std::vector<std::string> results;
 
@@ -22,13 +22,22 @@ std::vector<std::string> generate_results(const std::string& input_data)
 	voronotalt::TimeRecorder time_recorder;
 
 	const voronotalt::MolecularFileReading::Parameters molecular_file_reading_parameters(true, false, true);
+	const double probe=1.4;
+	const bool compute_only_inter_chain_contacts=(options=="only_inter_chain");
 
 	voronotalt::SpheresInput::Result spheres_input_result;
 
-	if(!voronotalt::SpheresInput::read_labeled_or_unlabeled_spheres_from_string(input_data, molecular_file_reading_parameters, 1.4, spheres_input_result, log_stream, time_recorder))
+	if(!voronotalt::SpheresInput::read_labeled_or_unlabeled_spheres_from_string(input_data, molecular_file_reading_parameters, probe, spheres_input_result, log_stream, time_recorder))
 	{
 		results.push_back(std::string("error"));
 		results.push_back(std::string("Failed to read input without errors.\n")+log_stream.str());
+		return results;
+	}
+
+	if(compute_only_inter_chain_contacts && spheres_input_result.number_of_chain_groups<2)
+	{
+		results.push_back(std::string("error"));
+		results.push_back(std::string("Error: inter-chain contact selection not possible - not enough distinct chains derived from labels\n"));
 		return results;
 	}
 
@@ -41,7 +50,13 @@ std::vector<std::string> generate_results(const std::string& input_data)
 	voronotalt::RadicalTessellation::Result result;
 	voronotalt::RadicalTessellation::ResultGraphics result_graphics;
 
-	voronotalt::RadicalTessellation::construct_full_tessellation(spheres_container, std::vector<int>(), false, true, result, result_graphics, time_recorder);
+	{
+		const std::vector<int> null_grouping;
+		const std::vector<int>& grouping_for_filtering=(compute_only_inter_chain_contacts ? spheres_input_result.grouping_by_chain : null_grouping);
+		const bool summarize_cells=grouping_for_filtering.empty();
+
+		voronotalt::RadicalTessellation::construct_full_tessellation(spheres_container, grouping_for_filtering, false, summarize_cells, result, result_graphics, time_recorder);
+	}
 
 	voronotalt::RadicalTessellation::GroupedResult result_grouped_by_residue;
 	voronotalt::RadicalTessellation::group_results(result, spheres_input_result.grouping_by_residue, result_grouped_by_residue, time_recorder);
@@ -53,6 +68,12 @@ std::vector<std::string> generate_results(const std::string& input_data)
 		results.push_back(std::string("log_total_input_balls"));
 		results.push_back(std::to_string(result.total_spheres));
 
+		results.push_back(std::string("log_total_residues_in_input"));
+		results.push_back(std::to_string(spheres_input_result.number_of_residue_groups));
+
+		results.push_back(std::string("log_total_chains_in_input"));
+		results.push_back(std::to_string(spheres_input_result.number_of_chain_groups));
+
 		results.push_back(std::string("log_total_contacts_count"));
 		results.push_back(std::to_string(result.total_contacts_summary.count));
 
@@ -62,23 +83,41 @@ std::vector<std::string> generate_results(const std::string& input_data)
 		results.push_back(std::string("log_total_residue_level_contacts_count"));
 		results.push_back(std::to_string(result_grouped_by_residue.grouped_contacts_summaries.size()));
 
-		results.push_back(std::string("log_total_chain_level_contacts_count"));
-		results.push_back(std::to_string(result_grouped_by_chain.grouped_contacts_summaries.size()));
+		if(spheres_input_result.number_of_chain_groups>1)
+		{
+			results.push_back(std::string("log_total_chain_level_contacts_count"));
+			results.push_back(std::to_string(result_grouped_by_chain.grouped_contacts_summaries.size()));
+		}
 
-		results.push_back(std::string("log_total_cells_count"));
-		results.push_back(std::to_string(result.total_cells_summary.count));
+		if(!compute_only_inter_chain_contacts)
+		{
+			results.push_back(std::string("log_total_cells_count"));
+			results.push_back(std::to_string(result.total_cells_summary.count));
 
-		results.push_back(std::string("log_total_cells_sas_area"));
-		results.push_back(std::to_string(result.total_cells_summary.sas_area));
+			results.push_back(std::string("log_total_cells_sas_area"));
+			results.push_back(std::to_string(result.total_cells_summary.sas_area));
 
-		results.push_back(std::string("log_total_cells_sas_inside_volume"));
-		results.push_back(std::to_string(result.total_cells_summary.sas_inside_volume));
+			results.push_back(std::string("log_total_cells_sas_inside_volume"));
+			results.push_back(std::to_string(result.total_cells_summary.sas_inside_volume));
 
-		results.push_back(std::string("log_total_residue_level_cells_count"));
-		results.push_back(std::to_string(result_grouped_by_residue.grouped_cells_summaries.size()));
+			results.push_back(std::string("log_total_residue_level_cells_count"));
+			results.push_back(std::to_string(result_grouped_by_residue.grouped_cells_summaries.size()));
 
-		results.push_back(std::string("log_total_chain_level_cells_count"));
-		results.push_back(std::to_string(result_grouped_by_chain.grouped_cells_summaries.size()));
+			if(spheres_input_result.number_of_chain_groups>1)
+			{
+				results.push_back(std::string("log_total_chain_level_cells_count"));
+				results.push_back(std::to_string(result_grouped_by_chain.grouped_cells_summaries.size()));
+			}
+		}
+	}
+
+	if(!spheres_input_result.spheres.empty())
+	{
+		std::ostringstream output;
+		output << "ID_chain\tID_residue\tID_atom\tx\ty\tz\tradius\n";
+		voronotalt::PrintingCustomTypes::print_balls_to_stream(spheres_input_result.spheres, spheres_input_result.sphere_labels, probe, output);
+		results.push_back(std::string("input-atom-balls"));
+		results.push_back(output.str());
 	}
 
 	if(!result.contacts_summaries.empty())
