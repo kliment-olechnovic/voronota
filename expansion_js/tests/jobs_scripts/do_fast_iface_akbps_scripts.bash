eab0fb3c8e8a748b1cea5c06b64723434532c9d3
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-01-28 13:10:02 +0100

    Improved layered areas calculation script.

--- expansion_js/tests/jobs_scripts/do_fast_iface_akbps_scripts.bash
+++ expansion_js/tests/jobs_scripts/do_fast_iface_akbps_scripts.bash
@@ -5,10 +5,36 @@ mkdir -p $SUBDIR
 
 find "$INPUTDIR/complex/" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-areas-layered \
+  --layering-mode "inward" \
+  --directing-mode "none" \
   --input _list \
   --processors 4 \
-  --output-table-file "$SUBDIR/areas_layered"
+  --output-table-file "$SUBDIR/areas_layered_inward"
 
+find "$INPUTDIR/complex/" -type f \
+| $VORONOTAJSDIR/voronota-js-fast-iface-areas-layered \
+  --layering-mode "outward" \
+  --directing-mode "none" \
+  --input _list \
+  --processors 4 \
+  --output-table-file "$SUBDIR/areas_layered_outward"
+
+find "$INPUTDIR/complex/" -type f \
+| $VORONOTAJSDIR/voronota-js-fast-iface-areas-layered \
+  --layering-mode "inward" \
+  --directing-mode "d4" \
+  --input _list \
+  --processors 4 \
+  --output-table-file "$SUBDIR/areas_layered_inward_d4"
+
+find "$INPUTDIR/complex/" -type f \
+| $VORONOTAJSDIR/voronota-js-fast-iface-areas-layered \
+  --layering-mode "outward" \
+  --directing-mode "d4" \
+  --input _list \
+  --processors 4 \
+  --output-table-file "$SUBDIR/areas_layered_outward_d4"
+  
 find "$INPUTDIR/complex/" -type f \
 | $VORONOTAJSDIR/voronota-js-fast-iface-akbps \
   --akbps-lib ${INPUTDIR}/../../../resources/voromqalike_area_kbps_raw \
