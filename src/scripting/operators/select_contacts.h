9c2900eade08c7747672b3b3f767b9edb20e7937
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-03-26 13:24:55 +0100

    Added adjacency-based selection option in contacts selector.

--- src/scripting/operators/select_contacts.h
+++ src/scripting/operators/select_contacts.h
@@ -37,8 +37,9 @@ public:
 	SelectionManager::Query parameters_for_selecting;
 	std::string name;
 	bool mark;
+	bool with_adjacent;
 
-	SelectContacts() : mark(false)
+	SelectContacts() : mark(false), with_adjacent(false)
 	{
 	}
 
@@ -47,6 +48,7 @@ public:
 		parameters_for_selecting=OperatorsUtilities::read_generic_selecting_query(input);
 		name=input.get_value_or_first_unused_unnamed_value_or_default("name", "");
 		mark=input.get_flag("mark");
+		with_adjacent=input.get_flag("with-adjacent");
 	}
 
 	void document(CommandDocumentation& doc) const
@@ -54,11 +56,17 @@ public:
 		OperatorsUtilities::document_read_generic_selecting_query(doc);
 		doc.set_option_decription(CDOD("name", CDOD::DATATYPE_STRING, "contact selection name", ""));
 		doc.set_option_decription(CDOD("mark", CDOD::DATATYPE_BOOL, "flag to mark selected contacts"));
+		doc.set_option_decription(CDOD("with-adjacent", CDOD::DATATYPE_BOOL, "flag to additionally select adjacent contacts"));
 	}
 
 	Result run(DataManager& data_manager) const
 	{
 		data_manager.assert_contacts_availability();
+
+		if(with_adjacent)
+		{
+			data_manager.assert_contacts_adjacencies_availability();
+		}
 
 		assert_selection_name_input(name, true);
 
@@ -66,6 +74,24 @@ public:
 		if(ids.empty())
 		{
 			throw std::runtime_error(std::string("No contacts selected."));
+		}
+
+		if(with_adjacent)
+		{
+			std::set<std::size_t> more_ids;
+			for(std::set<std::size_t>::const_iterator it=ids.begin();it!=ids.end();++it)
+			{
+				const std::size_t contact_id=(*it);
+				if(contact_id<data_manager.contacts_adjacencies().size())
+				{
+					const std::map<std::size_t, double>& all_neighbors=data_manager.contacts_adjacencies()[contact_id];
+					for(std::map<std::size_t, double>::const_iterator jt=all_neighbors.begin();jt!=all_neighbors.end();++jt)
+					{
+						more_ids.insert(jt->first);
+					}
+				}
+			}
+			ids.insert(more_ids.begin(), more_ids.end());
 		}
 
 		if(!name.empty())
