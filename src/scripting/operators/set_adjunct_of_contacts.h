48b285c65c78fb11c575e754cb46b13fb945fee3
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-07-23 16:46:09 +0200

    Expanded the contact adjunct setting operator.

--- src/scripting/operators/set_adjunct_of_contacts.h
+++ src/scripting/operators/set_adjunct_of_contacts.h
@@ -31,6 +31,7 @@ public:
 	double value;
 	bool remove;
 	bool multiply_by_area;
+	std::vector<std::string> residue_residue_values;
 
 	SetAdjunctOfContacts() : value_present(false), value(0.0), remove(false), multiply_by_area(false)
 	{
@@ -44,6 +45,7 @@ public:
 		value=input.get_value_or_default<double>("value", 0.0);
 		remove=input.get_flag("remove");
 		multiply_by_area=input.get_flag("multiply-by-area");
+		residue_residue_values=input.get_value_vector_or_default<std::string>("residue-residue-values", std::vector<std::string>());
 	}
 
 	void document(CommandDocumentation& doc) const
@@ -53,6 +55,7 @@ public:
 		doc.set_option_decription(CDOD("value", CDOD::DATATYPE_FLOAT, "adjunct value", ""));
 		doc.set_option_decription(CDOD("remove", CDOD::DATATYPE_BOOL, "flag to remove adjunct"));
 		doc.set_option_decription(CDOD("multiply-by-area", CDOD::DATATYPE_BOOL, "flag to assign value multiplied by area"));
+		doc.set_option_decription(CDOD("residue-residue-values", CDOD::DATATYPE_STRING_ARRAY, "residue-residue IDs and values", ""));
 	}
 
 	Result run(DataManager& data_manager) const
@@ -69,6 +72,11 @@ public:
 			throw std::runtime_error(std::string("Area multiplication option used without providing a value."));
 		}
 
+		if(!residue_residue_values.empty() && residue_residue_values.size()%5!=0)
+		{
+			throw std::runtime_error(std::string("The length of the vector of residue-residue IDs and values must be a multiple of 5 (chain rnum chain rnum value)."));
+		}
+
 		assert_adjunct_name_input(name, false);
 
 		std::set<std::size_t> ids=data_manager.selection_manager().select_contacts(parameters_for_selecting);
@@ -77,22 +85,81 @@ public:
 			throw std::runtime_error(std::string("No contacts selected."));
 		}
 
-		for(std::set<std::size_t>::const_iterator it=ids.begin();it!=ids.end();++it)
+		if(residue_residue_values.empty())
 		{
-			std::map<std::string, double>& contact_adjuncts=data_manager.contact_adjuncts_mutable(*it);
-			if(remove)
+			for(std::set<std::size_t>::const_iterator it=ids.begin();it!=ids.end();++it)
 			{
-				contact_adjuncts.erase(name);
-			}
-			else
-			{
-				if(multiply_by_area)
+				std::map<std::string, double>& contact_adjuncts=data_manager.contact_adjuncts_mutable(*it);
+				if(remove)
 				{
-					contact_adjuncts[name]=value*data_manager.contacts()[*it].value.area;
+					contact_adjuncts.erase(name);
 				}
 				else
 				{
-					contact_adjuncts[name]=value;
+					if(multiply_by_area)
+					{
+						contact_adjuncts[name]=value*data_manager.contacts()[*it].value.area;
+					}
+					else
+					{
+						contact_adjuncts[name]=value;
+					}
+				}
+			}
+		}
+		else
+		{
+			std::map<common::ChainResidueAtomDescriptorsPair, double> rr_adjunct_values;
+			for(std::size_t j=0;j<residue_residue_values.size();j+=5)
+			{
+				common::ChainResidueAtomDescriptor a(residue_residue_values[j+0]);
+				common::ChainResidueAtomDescriptor b(residue_residue_values[j+2]);
+				double rr_value=value;
+				std::string line=residue_residue_values[j+1]+" "+residue_residue_values[j+3]+" "+residue_residue_values[j+4];
+				std::istringstream line_input(line);
+				line_input >> a.resSeq >> b.resSeq >> rr_value;
+				if(line_input.fail())
+				{
+					throw std::runtime_error(std::string("Invalid vector of residue-residue IDs and values."));
+				}
+				else
+				{
+					rr_adjunct_values[common::ChainResidueAtomDescriptorsPair(a, b)]=rr_value;
+				}
+			}
+			for(std::set<std::size_t>::const_iterator it=ids.begin();it!=ids.end();++it)
+			{
+				const Contact& contact=data_manager.contacts()[*it];
+				if(!contact.solvent())
+				{
+					const Atom& atom1=data_manager.atoms()[contact.ids[0]];
+					common::ChainResidueAtomDescriptor crad1(atom1.crad.chainID);
+					crad1.resSeq=atom1.crad.resSeq;
+					const Atom& atom2=data_manager.atoms()[contact.ids[1]];
+					common::ChainResidueAtomDescriptor crad2(atom2.crad.chainID);
+					crad2.resSeq=atom2.crad.resSeq;
+					const common::ChainResidueAtomDescriptorsPair crads(crad1, crad2);
+					std::map<common::ChainResidueAtomDescriptorsPair, double>::const_iterator jt=rr_adjunct_values.find(crads);
+					if(jt!=rr_adjunct_values.end())
+					{
+						const double rr_value=jt->second;
+						std::map<std::string, double>& contact_adjuncts=data_manager.contact_adjuncts_mutable(*it);
+						if(remove)
+						{
+							contact_adjuncts.erase(name);
+						}
+						else
+						{
+							if(multiply_by_area)
+							{
+								contact_adjuncts[name]=rr_value*contact.value.area;
+							}
+							else
+							{
+								contact_adjuncts[name]=rr_value;
+							}
+						}
+					}
 				}
 			}
 		}
