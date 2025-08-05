bd2c3be5fefd82f6f7520a17671c4ae9780ee710
klap <klap@klap-machine>
2025-08-05 14:13:10 +0200

    Moved out the sihsolvexpand metaapp.

--- expansion_lt/metaapps/sihsolvexpand/build.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $0)"
-
-# building with OpenMP and with CPU architecture matching
-# g++ -std=c++14 -Ofast -march=native -fopenmp -o ./sihsolvexpand ./sihsolvexpand.cpp -I ../../src/
-
-# building without OpenMP in a more portable way
-g++ -std=c++14 -O3 -o ./sihsolvexpand ./sihsolvexpand.cpp -I ../../src/
