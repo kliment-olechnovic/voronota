76afc3b235a37223d96906eacd7b0277c6b98f97
kliment-olechnovic <kliment.olechnovic@gmail.com>
2026-02-12 09:40:41 +0200

    Updated tests of lt-cadscore swig-python bindings.

--- expansion_lt_cadscore/swig/tests/input/run_cadscorelt_python_v03.py
+++ expansion_lt_cadscore/swig/tests/input/run_cadscorelt_python_v03.py
@@ -2,13 +2,13 @@ import cadscorelt
 import gemmi
 
 structure_target = gemmi.read_structure("./input/data/protein_homodimer1/target.pdb")
-#structure_model1 = gemmi.read_structure("./input/data/protein_homodimer1/model1.pdb")
+structure_model1 = gemmi.read_structure("./input/data/protein_homodimer1/model1.pdb")
 structure_model2 = gemmi.read_structure("./input/data/protein_homodimer1/model2.pdb")
 
 cs = cadscorelt.CADScore.init(subselect_contacts="[-inter-chain]", remap_chains=True)
 
 cs.add_structure_from_gemmi(structure_target[0], "target")
-#cs.add_structure_from_gemmi(structure_model1[0], "model1")
+cs.add_structure_from_gemmi(structure_model1[0], "model1")
 cs.add_structure_from_gemmi(structure_model2[0], "model2")
 
 df_global_scores = cs.get_all_cadscores_residue_residue_summarized_globally().to_pandas()
